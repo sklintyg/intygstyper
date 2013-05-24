e9357bd767cfcec4e98ae03418739bfd5ff2bf7e
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-05-24 15:50:33 +0200

    removing reference to deleted CustomJacksonJaxbJsonProvider

--- rli/src/main/java/se/inera/certificate/modules/rli/rest/RliModuleApi.java
+++ rli/src/main/java/se/inera/certificate/modules/rli/rest/RliModuleApi.java
@@ -1,7 +1,7 @@
 package se.inera.certificate.modules.rli.rest;
 
 import se.inera.certificate.integration.ModuleRestApi;
-import se.inera.certificate.integration.v1.LakarutlatandeType;
+import se.inera.certificate.integration.v1.Lakarutlatande;
 import se.inera.certificate.model.Ovrigt;
 import se.inera.certificate.model.Valideringsresultat;
 
@@ -11,14 +11,14 @@ import se.inera.certificate.model.Valideringsresultat;
 public class RliModuleApi implements ModuleRestApi {
 
     @Override
-    public Ovrigt extract(LakarutlatandeType lakarutlatandeType) {
+    public Ovrigt extract(Lakarutlatande lakarutlatande) {
         Ovrigt ovrigt = new Ovrigt();
 
         return ovrigt;
     }
 
     @Override
-    public Valideringsresultat validate(LakarutlatandeType lakarutlatandeType) {
+    public Valideringsresultat validate(Lakarutlatande lakarutlatande) {
         return new Valideringsresultat(null);
     }
 
