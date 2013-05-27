506a2006f8d6e9303cbd93adf4500119f10dd987
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-05-27 10:02:14 +0200

    module-specific Reselakarintyg.xsd with mapped resmal and resenar

--- rli/src/main/java/se/inera/certificate/modules/rli/rest/RliModuleApi.java
+++ rli/src/main/java/se/inera/certificate/modules/rli/rest/RliModuleApi.java
@@ -1,26 +1,47 @@
 package se.inera.certificate.modules.rli.rest;
 
-import se.inera.certificate.integration.ModuleRestApi;
 import se.inera.certificate.integration.v1.Lakarutlatande;
 import se.inera.certificate.model.Ovrigt;
 import se.inera.certificate.model.Valideringsresultat;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author andreaskaltenbach
  */
-public class RliModuleApi implements ModuleRestApi {
+public class RliModuleApi {
 
-    @Override
-    public Ovrigt extract(Lakarutlatande lakarutlatande) {
+    @POST
+    @Path( "/extension" )
+    @Consumes( MediaType.APPLICATION_XML )
+    @Produces(MediaType.APPLICATION_JSON)
+    public Ovrigt extract(Lakarutlatande intyg) {
         Ovrigt ovrigt = new Ovrigt();
 
         return ovrigt;
     }
 
-    @Override
-    public Valideringsresultat validate(Lakarutlatande lakarutlatande) {
-        return new Valideringsresultat(null);
+    @POST
+    @Path( "/valid" )
+    @Consumes( MediaType.APPLICATION_XML )
+    @Produces( MediaType.APPLICATION_JSON )
+    public Valideringsresultat validate(Lakarutlatande intyg) {
+        List<String> errors = new ArrayList<>();
+
+        if (intyg.getResenar() == null) {
+            errors.add("Resenär is empty");
+        }
+
+        if (intyg.getResmal() == null) {
+            errors.add("Resmal is empty");
+        }
+
+        return new Valideringsresultat(errors);
     }
-
-
 }
