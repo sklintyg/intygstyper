bbd14f4fa69947ab2c17abae32c8c3bcd007cd02
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-06-07 11:34:26 +0200

    return FK7263 as PDF (PDF is not yet filled with certificate information)

--- fk7263/src/main/java/se/inera/certificate/modules/fk7263/rest/Fk7263ModuleApi.java
+++ fk7263/src/main/java/se/inera/certificate/modules/fk7263/rest/Fk7263ModuleApi.java
@@ -1,6 +1,9 @@
 package se.inera.certificate.modules.fk7263.rest;
 
 import com.google.common.base.Joiner;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
 import se.inera.certificate.integration.v1.Lakarutlatande;
 import se.inera.certificate.modules.fk7263.validator.LakarutlatandeValidator;
 
@@ -10,6 +13,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -18,16 +23,16 @@ import java.util.List;
 public class Fk7263ModuleApi {
 
     @POST
-    @Path( "/extension" )
-    @Consumes( MediaType.APPLICATION_XML )
+    @Path("/extension")
+    @Consumes(MediaType.APPLICATION_XML)
     @Produces(MediaType.APPLICATION_JSON)
     public Object extract(Lakarutlatande lakarutlatande) {
         return "{}";
     }
 
     @POST
-    @Path( "/valid" )
-    @Consumes( MediaType.APPLICATION_XML )
+    @Path("/valid")
+    @Consumes(MediaType.APPLICATION_XML)
     @Produces(MediaType.TEXT_PLAIN)
     public Response validate(Lakarutlatande lakarutlatande) {
 
@@ -35,10 +40,25 @@ public class Fk7263ModuleApi {
 
         if (validationErrors.isEmpty()) {
             return Response.ok().build();
-        }
-        else {
+        } else {
             String response = Joiner.on(",").join(validationErrors);
             return Response.status(Response.Status.BAD_REQUEST).entity(response).build();
         }
+    }
+
+    @POST
+    @Path( "/pdf" )
+    @Consumes( MediaType.APPLICATION_JSON )
+    @Produces( "application/pdf" )
+    public byte[] pdf(se.inera.certificate.model.Lakarutlatande intyg) throws IOException, DocumentException {
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        PdfReader template = new PdfReader("pdf/RFV7263_008_J_003_statisk.pdf");
+        PdfStamper pdfStamper = new PdfStamper(template, out);
+
+        pdfStamper.close();
+
+        return out.toByteArray();
     }
 }
