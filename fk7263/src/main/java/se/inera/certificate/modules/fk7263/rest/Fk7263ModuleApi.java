7ca0167a4d40e55b1663d9e2231b10f2c40002c2
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-06-12 15:05:41 +0200

    PDF filename contains personnummer and from/to date

--- fk7263/src/main/java/se/inera/certificate/modules/fk7263/rest/Fk7263ModuleApi.java
+++ fk7263/src/main/java/se/inera/certificate/modules/fk7263/rest/Fk7263ModuleApi.java
@@ -11,6 +11,8 @@ import java.util.List;
 
 import com.google.common.base.Joiner;
 import com.itextpdf.text.DocumentException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.inera.certificate.integration.v1.Lakarutlatande;
 import se.inera.certificate.modules.fk7263.model.Fk7263Intyg;
 import se.inera.certificate.modules.fk7263.pdf.PdfGenerator;
@@ -20,6 +22,8 @@ import se.inera.certificate.modules.fk7263.validator.LakarutlatandeValidator;
  * @author andreaskaltenbach
  */
 public class Fk7263ModuleApi {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Fk7263ModuleApi.class);
 
     @POST
     @Path("/extension")
@@ -46,10 +50,20 @@ public class Fk7263ModuleApi {
     }
 
     @POST
-    @Path( "/pdf" )
-    @Consumes( MediaType.APPLICATION_JSON )
-    @Produces( "application/pdf" )
-    public byte[] pdf(Fk7263Intyg intyg) throws IOException, DocumentException {
-        return new PdfGenerator(intyg).getBytes();
+    @Path("/pdf")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces("application/pdf")
+    public Response pdf(Fk7263Intyg intyg) {
+
+        try {
+            byte[] generatedPdf = new PdfGenerator(intyg).getBytes();
+            return Response.ok(generatedPdf).build();
+        } catch (IOException e) {
+            LOG.error("Failed to generate PDF for certificate #" + intyg.getId(), e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        } catch (DocumentException e) {
+            LOG.error("Failed to generate PDF for certificate #" + intyg.getId(), e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
