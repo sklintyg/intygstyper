32944ba8c2eccad844a2ec4c01ef06b18d0fdbff
Benny Samuelsson <benny.samuelsson@usify.se>
2013-06-25 15:42:37 +0200

    Added dynamic title to FK7263

--- fk7263/src/main/webapp/js/view/fk7263-app.js
+++ fk7263/src/main/webapp/js/view/fk7263-app.js
@@ -9,23 +9,28 @@ var FK7263App = angular.module('FK7263ViewCertApp', [ 'ui.bootstrap', 'services.
         [ '$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
             $routeProvider.when('/view', {
                 templateUrl : MODULE_CONFIG.MODULE_CONTEXT_PATH + '/views/view-cert.html',
-                controller : 'ViewCertCtrl'
+                controller : 'ViewCertCtrl',
+	            title : 'Läkarintyg FK7263'
             // }).when('/recipients', {
             // templateUrl : MODULE_CONFIG.MODULE_CONTEXT_PATH +
             // '/views/recipients.html',
             // controller : 'SentCertWizardCtrl'
             }).when('/statushistory', {
                 templateUrl : MODULE_CONFIG.MODULE_CONTEXT_PATH + '/views/status-history.html',
-                controller : 'ViewCertCtrl'
+                controller : 'ViewCertCtrl',
+			    title : 'Alla intygets händelser'
             }).when('/summary', {
                 templateUrl : MODULE_CONFIG.MODULE_CONTEXT_PATH + '/views/send-summary.html',
-                controller : 'SentCertWizardCtrl'
+                controller : 'SentCertWizardCtrl',
+			    title : 'Kontrollera och skicka intyget'
             }).when('/sent', {
                 templateUrl : MODULE_CONFIG.MODULE_CONTEXT_PATH + '/views/sent-cert.html',
-                controller : 'SentCertWizardCtrl'
+                controller : 'SentCertWizardCtrl',
+	            title : 'Intyget har skickats'
             }).when('/fel/:errorCode', {
                 templateUrl : MODULE_CONFIG.MODULE_CONTEXT_PATH + '/views/error.html',
-                controller : 'ErrorCtrl'
+                controller : 'ErrorCtrl',
+	            title : 'Fel'
             }).otherwise({
                 redirectTo : '/view'
             });
@@ -33,10 +38,16 @@ var FK7263App = angular.module('FK7263ViewCertApp', [ 'ui.bootstrap', 'services.
             $httpProvider.interceptors.push('httpRequestInterceptorCacheBuster');
         } ]);
 
-FK7263App.run([ '$rootScope', 'messageService', function($rootScope, messageService) {
+FK7263App.run([ '$rootScope', '$route','messageService', function($rootScope, $route, messageService) {
     $rootScope.lang = 'sv';
     $rootScope.DEFAULT_LANG = 'sv';
     $rootScope.MODULE_CONFIG = MODULE_CONFIG;
     messageService.addResources(commonMessageResources);
     messageService.addResources(fk7263Messages);
+
+	// Update page title
+	$rootScope.page_title = 'Titel';
+    $rootScope.$on('$routeChangeSuccess', function() {
+      $rootScope.page_title = $route.current.$$route.title + ' | Mina intyg';
+    });
 } ]);
