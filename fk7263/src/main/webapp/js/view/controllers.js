121ed63782d66618ed0f6b58f352f760f2381f5b
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2013-06-28 16:25:48 +0200

    workaround for ng-src behaviour

--- fk7263/src/main/webapp/js/view/controllers.js
+++ fk7263/src/main/webapp/js/view/controllers.js
@@ -5,27 +5,27 @@ angular.module('controllers.fk7263', []);
 angular.module('controllers.fk7263').controller('ViewCertCtrl',
         [ '$scope', '$filter', '$location', '$rootScope', 'certService', function ViewCertCtrl($scope, $filter, $location, $rootScope, certService) {
             $scope.cert = {};
-
+            
             $scope.doneLoading = false;
 
             $scope.send = function() {
                 $location.path("/summary");
             };
 
-            $scope.visibleStatuses = ['SENT'];
+            $scope.visibleStatuses = [ 'SENT' ];
 
             $scope.filterStatuses = function(statuses) {
                 var result = [];
-                for(var i=0; i<statuses.length; i++) {
-                    if($scope.userVisibleStatusFilter(statuses[i])) {
-                    result.push(statuses[i]);
+                for ( var i = 0; i < statuses.length; i++) {
+                    if ($scope.userVisibleStatusFilter(statuses[i])) {
+                        result.push(statuses[i]);
                     }
                 }
                 return result;
             }
 
             $scope.userVisibleStatusFilter = function(status) {
-                for(var i=0; i<$scope.visibleStatuses.length;i++) {
+                for ( var i = 0; i < $scope.visibleStatuses.length; i++) {
                     if (status.type == $scope.visibleStatuses[i]) {
                         return true;
                     }
@@ -41,17 +41,17 @@ angular.module('controllers.fk7263').controller('ViewCertCtrl',
                 $location.path("/view");
             }
 
-            //Convenience methods to simplify template markup
+            // Convenience methods to simplify template markup
             $scope.smittskydd = function() {
                 if (angular.isObject($scope.cert.aktiviteter)) {
                     // check if some activity has the trigger aktivitetskod
-                    for(var i=0; i<$scope.cert.aktiviteter.length;i++) {
+                    for ( var i = 0; i < $scope.cert.aktiviteter.length; i++) {
                         if ($scope.cert.aktiviteter[i].aktivitetskod == "AVSTANGNING_ENLIGT_SM_L_PGA_SMITTA") {
                             return "yes";
                         }
                     }
                 }
-                //in all other cases
+                // in all other cases
                 return "no";
             }
             // expose calculated static link for pdf download
@@ -61,6 +61,10 @@ angular.module('controllers.fk7263').controller('ViewCertCtrl',
                 $scope.doneLoading = true;
                 if (result != null) {
                     result.filteredStatuses = $scope.filterStatuses(result.status);
+                    // added calculated img src for certificate, beacuse for
+                    // some reason ng-src with "/img/{{cert.typ }}.png" would
+                    // evaluate first to "img/.png" before correct value = 404
+                    result.typ_image = "/img/" + result.typ + ".png";
                     $scope.cert = result;
                     $rootScope.cert = result;
                 } else {
@@ -69,7 +73,7 @@ angular.module('controllers.fk7263').controller('ViewCertCtrl',
                 }
             });
 
-	        $scope.pagefocus = true;
+            $scope.pagefocus = true;
         } ]);
 
 angular.module('controllers.fk7263').controller('SentCertWizardCtrl',
@@ -87,24 +91,27 @@ angular.module('controllers.fk7263').controller('SentCertWizardCtrl',
 
             // Initialize recipient handling, default to FK
             $scope.selectedRecipientId = $rootScope.selectedRecipientId || "FK";
-            //set selected recipeintID in rootscope to preserve state between controller instance invocations
+            // set selected recipeintID in rootscope to preserve state between
+            // controller instance invocations
             $rootScope.selectedRecipientId = $scope.selectedRecipientId;
             $scope.recipientList = [ {
                 "id" : "FK",
                 "recipientName" : "Försäkringskassan"
-            }];
+            } ];
 
-            $scope.getRecipientName = function (id) {
-                for(var i=0;i<$scope.recipientList.length;i++) {
+            $scope.getRecipientName = function(id) {
+                for ( var i = 0; i < $scope.recipientList.length; i++) {
                     var recipient = $scope.recipientList[i];
                     if (recipient.id == id) {
                         return recipient.recipientName;
                     }
-                };
+                }
+                ;
             }
 
             $scope.confirmRecipientSelection = function() {
-                // now we have a recipient selected, set the selection in rootscope
+                // now we have a recipient selected, set the selection in
+                // rootscope
                 $rootScope.selectedRecipientId = $scope.selectedRecipientId;
                 $location.path("/summary");
             }
@@ -124,25 +131,25 @@ angular.module('controllers.fk7263').controller('SentCertWizardCtrl',
             }
 
             $scope.alreadySentToRecipient = function() {
-                // check if selected recipient exists with SENT status in cert history
-                for(var i=0;i<$scope.cert.status.length;i++) {
+                // check if selected recipient exists with SENT status in cert
+                // history
+                for ( var i = 0; i < $scope.cert.status.length; i++) {
                     if (($scope.cert.status[i].type === "SENT") && ($scope.cert.status[i].target === $scope.selectedRecipientId)) {
                         return true;
                     }
                 }
                 return false;
-            }            
+            }
             $scope.backToViewCertificate = function() {
                 $location.path("/view");
             }
 
-	        $scope.pagefocus = true;
-        } ]);
-
-angular.module('controllers.fk7263').controller('ErrorCtrl',
-        [ '$scope', '$routeParams', function SentCertWizardCtrl($scope, $routeParams) {
-
-            //set a default if no errorCode is given in routeparams
-            $scope.errorCode = $routeParams.errorCode || "generic";
             $scope.pagefocus = true;
         } ]);
+
+angular.module('controllers.fk7263').controller('ErrorCtrl', [ '$scope', '$routeParams', function SentCertWizardCtrl($scope, $routeParams) {
+
+    // set a default if no errorCode is given in routeparams
+    $scope.errorCode = $routeParams.errorCode || "generic";
+    $scope.pagefocus = true;
+} ]);
