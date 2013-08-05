a2f35395fc51db68bb4ef14143a4eecf66965750
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2013-08-05 14:41:52 +0200

    cleanup and adapted send-certificate wizard to new json certificate model structure

--- fk7263/src/main/webapp/intyg/js/controllers.js
+++ fk7263/src/main/webapp/intyg/js/controllers.js
@@ -5,7 +5,7 @@ angular.module('controllers.fk7263', []);
 angular.module('controllers.fk7263').controller('ViewCertCtrl',
         [ '$scope', '$filter', '$location', '$rootScope', 'certService', function ViewCertCtrl($scope, $filter, $location, $rootScope, certService) {
             $scope.cert = {};
-            
+
             $scope.doneLoading = false;
 
             $scope.send = function() {
@@ -44,19 +44,6 @@ angular.module('controllers.fk7263').controller('ViewCertCtrl',
                 $location.path("/view");
             }
 
-            // Convenience methods to simplify template markup
-//            $scope.smittskydd = function() {
-//                if (angular.isObject($scope.cert.aktiviteter)) {
-//                    // check if some activity has the trigger aktivitetskod
-//                    for ( var i = 0; i < $scope.cert.aktiviteter.length; i++) {
-//                        if ($scope.cert.aktiviteter[i].aktivitetskod == "AVSTANGNING_ENLIGT_SM_L_PGA_SMITTA") {
-//                            return "yes";
-//                        }
-//                    }
-//                }
-//                // in all other cases
-//                return "no";
-//            }
 
             // expose calculated static link for pdf download
             $scope.downloadAsPdfLink = $scope.MODULE_CONFIG.MI_COMMON_API_CONTEXT_PATH + $scope.MODULE_CONFIG.CERT_ID_PARAMETER + "/pdf";
@@ -70,23 +57,31 @@ angular.module('controllers.fk7263').controller('ViewCertCtrl',
                     // evaluate first to "img/.png" before correct value = 404
                     result.typ_image = "/img/" + result.typ.code + ".png";
 
-	                // setup which fields contain values. those which do not will get an unfilled style to make them less visually significant
-	                // TODO: Implement cases for when fields are filled or not. change true to an expression checking for the condition. empty strings? a certain value?
-	                result.filledFields = {};
-	                result.filledFields.smittskydd = true;
-	                result.filledFields.diagnosis = true; //example: result.bedomtTillstand.beskrivning != '' || result.bedomtTillstand.tillstandskod != '';
-	                result.filledFields.progressofdesease = true;
-	                result.filledFields.disabilities = true;
-	                result.filledFields.basedon = true;
-	                result.filledFields.limitation = true;
-	                result.filledFields.recommendations = true;
-	                result.filledFields.plannedtreatment = true;
-	                result.filledFields.workrehab = true;
-	                result.filledFields.patientworkcapacity = true;
-	                result.filledFields.patientworkcapacityjudgement = true;
-	                result.filledFields.prognosis = true;
-	                result.filledFields.othertransport = true;
-	                result.filledFields.fkcontact = true;
+                    // setup which fields contain values. those which do not
+                    // will get an unfilled style to make them less visually
+                    // significant
+                    // TODO: Implement cases for when fields are filled or not.
+                    // change true to an expression checking for the condition.
+                    // empty strings? a certain value?
+                    result.filledFields = {};
+                    result.filledFields.smittskydd = true;
+                    result.filledFields.diagnosis = true; // example:
+                    // result.bedomtTillstand.beskrivning
+                    // != '' ||
+                    // result.bedomtTillstand.tillstandskod
+                    // != '';
+                    result.filledFields.progressofdesease = true;
+                    result.filledFields.disabilities = true;
+                    result.filledFields.basedon = true;
+                    result.filledFields.limitation = true;
+                    result.filledFields.recommendations = true;
+                    result.filledFields.plannedtreatment = true;
+                    result.filledFields.workrehab = true;
+                    result.filledFields.patientworkcapacity = true;
+                    result.filledFields.patientworkcapacityjudgement = true;
+                    result.filledFields.prognosis = true;
+                    result.filledFields.othertransport = true;
+                    result.filledFields.fkcontact = true;
 
                     $scope.cert = result;
                     $rootScope.cert = result;
@@ -156,9 +151,11 @@ angular.module('controllers.fk7263').controller('SentCertWizardCtrl',
             $scope.alreadySentToRecipient = function() {
                 // check if selected recipient exists with SENT status in cert
                 // history
-                for ( var i = 0; i < $scope.cert.status.length; i++) {
-                    if (($scope.cert.status[i].type === "SENT") && ($scope.cert.status[i].target === $scope.selectedRecipientId)) {
-                        return true;
+                if (angular.isObject($scope.cert.status)) {
+                    for ( var i = 0; i < $scope.cert.status.length; i++) {
+                        if (($scope.cert.status[i].type === "SENT") && ($scope.cert.status[i].target === $scope.selectedRecipientId)) {
+                            return true;
+                        }
                     }
                 }
                 return false;
