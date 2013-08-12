d7ab0f3bdc82026e6a74759c08e7bf7dd8911570
Gustav Norbäcker <gustav.norbacker@r2m.se>
2013-08-12 09:06:38 +0200

    Added RLI extension to common external model.

---
+++ rli/src/main/java/se/inera/certificate/modules/rli/model/external/Arrangemang.java
@@ -0,0 +1,69 @@
+package se.inera.certificate.modules.rli.model.external;
+
+import org.joda.time.Partial;
+
+import se.inera.certificate.common.v1.PartialDateInterval;
+import se.inera.certificate.model.Kod;
+
+public class Arrangemang {
+
+	private Kod bokningsreferens;
+
+	private Partial bokningsdatum;
+
+	private PartialDateInterval arrangemangstid;
+
+	private Partial avbestallningsdatum;
+
+	private Kod arrangemangstyp;
+
+	private String plats;
+
+	public Kod getBokningsreferens() {
+		return bokningsreferens;
+	}
+
+	public void setBokningsreferens(Kod bokningsreferens) {
+		this.bokningsreferens = bokningsreferens;
+	}
+
+	public Partial getBokningsdatum() {
+		return bokningsdatum;
+	}
+
+	public void setBokningsdatum(Partial bokningsdatum) {
+		this.bokningsdatum = bokningsdatum;
+	}
+
+	public PartialDateInterval getArrangemangstid() {
+		return arrangemangstid;
+	}
+
+	public void setArrangemangstid(PartialDateInterval arrangemangstid) {
+		this.arrangemangstid = arrangemangstid;
+	}
+
+	public Partial getAvbestallningsdatum() {
+		return avbestallningsdatum;
+	}
+
+	public void setAvbestallningsdatum(Partial avbestallningsdatum) {
+		this.avbestallningsdatum = avbestallningsdatum;
+	}
+
+	public Kod getArrangemangstyp() {
+		return arrangemangstyp;
+	}
+
+	public void setArrangemangstyp(Kod arrangemangstyp) {
+		this.arrangemangstyp = arrangemangstyp;
+	}
+
+	public String getPlats() {
+		return plats;
+	}
+
+	public void setPlats(String plats) {
+		this.plats = plats;
+	}
+}
---
+++ rli/src/main/java/se/inera/certificate/modules/rli/model/external/Utlatande.java
@@ -0,0 +1,148 @@
+package se.inera.certificate.modules.rli.model.external;
+
+import java.util.List;
+
+import org.joda.time.LocalDateTime;
+
+import se.inera.certificate.model.Aktivitet;
+import se.inera.certificate.model.HosPersonal;
+import se.inera.certificate.model.Id;
+import se.inera.certificate.model.Kod;
+import se.inera.certificate.model.Observation;
+import se.inera.certificate.model.Patient;
+import se.inera.certificate.model.Referens;
+import se.inera.certificate.model.Status;
+import se.inera.certificate.model.Vardkontakt;
+
+public class Utlatande {
+
+	private Id id;
+
+	private Kod typ;
+
+	private List<String> kommentars;
+
+	private LocalDateTime signeringsDatum;
+
+	private LocalDateTime skickatDatum;
+
+	private Patient patient;
+
+	private HosPersonal skapadAv;
+
+	private List<Aktivitet> aktiviteter;
+
+	private List<Observation> observations;
+
+	private List<Vardkontakt> vardkontakter;
+
+	private List<Referens> referenser;
+
+	private Arrangemang arrangemang;
+
+	private List<Status> status;
+
+	public Id getId() {
+		return id;
+	}
+
+	public void setId(Id id) {
+		this.id = id;
+	}
+
+	public Kod getTyp() {
+		return typ;
+	}
+
+	public void setTyp(Kod typ) {
+		this.typ = typ;
+	}
+
+	public List<String> getKommentars() {
+		return kommentars;
+	}
+
+	public void setKommentars(List<String> kommentars) {
+		this.kommentars = kommentars;
+	}
+
+	public LocalDateTime getSigneringsDatum() {
+		return signeringsDatum;
+	}
+
+	public void setSigneringsDatum(LocalDateTime signeringsDatum) {
+		this.signeringsDatum = signeringsDatum;
+	}
+
+	public LocalDateTime getSkickatDatum() {
+		return skickatDatum;
+	}
+
+	public void setSkickatDatum(LocalDateTime skickatDatum) {
+		this.skickatDatum = skickatDatum;
+	}
+
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+
+	public HosPersonal getSkapadAv() {
+		return skapadAv;
+	}
+
+	public void setSkapadAv(HosPersonal skapadAv) {
+		this.skapadAv = skapadAv;
+	}
+
+	public List<Aktivitet> getAktiviteter() {
+		return aktiviteter;
+	}
+
+	public void setAktiviteter(List<Aktivitet> aktiviteter) {
+		this.aktiviteter = aktiviteter;
+	}
+
+	public List<Observation> getObservations() {
+		return observations;
+	}
+
+	public void setObservations(List<Observation> observations) {
+		this.observations = observations;
+	}
+
+	public List<Vardkontakt> getVardkontakter() {
+		return vardkontakter;
+	}
+
+	public void setVardkontakter(List<Vardkontakt> vardkontakter) {
+		this.vardkontakter = vardkontakter;
+	}
+
+	public List<Referens> getReferenser() {
+		return referenser;
+	}
+
+	public void setReferenser(List<Referens> referenser) {
+		this.referenser = referenser;
+	}
+
+	public Arrangemang getArrangemang() {
+		return arrangemang;
+	}
+
+	public void setArrangemang(Arrangemang arrangemang) {
+		this.arrangemang = arrangemang;
+	}
+
+	public List<Status> getStatus() {
+		return status;
+	}
+
+	public void setStatus(List<Status> status) {
+		this.status = status;
+	}
+}
