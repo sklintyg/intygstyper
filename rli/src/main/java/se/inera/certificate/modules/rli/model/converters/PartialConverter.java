b3582b3d57b016b0c2f2cb26d99960213ba4c86e
Gustav Norbäcker <gustav.norbacker@r2m.se>
2013-08-19 10:36:53 +0200

    Improved performance of PartialConverter

--- rli/src/main/java/se/inera/certificate/modules/rli/model/converters/PartialConverter.java
+++ rli/src/main/java/se/inera/certificate/modules/rli/model/converters/PartialConverter.java
@@ -23,6 +23,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.joda.time.DateTimeFieldType;
@@ -38,7 +39,16 @@ import org.joda.time.format.DateTimeFormatter;
  */
 public final class PartialConverter {
 
-    private static final String DATE_STR_REGEX = "([0-9]{4})(?:\\-([0-9]{2})(?:\\-([0-9]{2}))?)?";
+    private static final Pattern DATE_STR_REGEX = Pattern.compile("([0-9]{4})(?:\\-([0-9]{2})(?:\\-([0-9]{2}))?)?");
+
+    private static final DateTimeFormatter YEAR_PATTERN = DateTimeFormat.forPattern("yyyy");
+
+    private static final DateTimeFormatter YEAR_MONTH_PATTERN = DateTimeFormat.forPattern("yyyy-MM");
+
+    private static final DateTimeFormatter YEAR_MONTH_DAY_PATTERN = DateTimeFormat.forPattern("yyyy-MM-dd");
+
+    private static final DateTimeFieldType[] PARTIAL_FIELDS = new DateTimeFieldType[] { DateTimeFieldType.year(),
+            DateTimeFieldType.monthOfYear(), DateTimeFieldType.dayOfMonth() };
 
     private PartialConverter() {
 
@@ -52,31 +62,19 @@ public final class PartialConverter {
      * @return A String
      */
     public static String partialToString(Partial partial) {
-
-        if (partial == null) {
-            return null;
-        }
-
-        StringBuilder datePattern = new StringBuilder();
-
-        // Year
         if (partial.isSupported(DateTimeFieldType.year())) {
-            datePattern.append("yyyy");
+            if (partial.isSupported(DateTimeFieldType.monthOfYear())) {
+                if (partial.isSupported(DateTimeFieldType.dayOfMonth())) {
+                    return partial.toString(YEAR_MONTH_DAY_PATTERN);
+                }
+
+                return partial.toString(YEAR_MONTH_PATTERN);
+            }
+
+            return partial.toString(YEAR_PATTERN);
         }
 
-        // Month
-        if (partial.isSupported(DateTimeFieldType.monthOfYear())) {
-            datePattern.append("-MM");
-        }
-
-        // Day
-        if (partial.isSupported(DateTimeFieldType.dayOfMonth())) {
-            datePattern.append("-dd");
-        }
-
-        DateTimeFormatter dfmt = DateTimeFormat.forPattern(datePattern.toString());
-
-        return partial.toString(dfmt);
+        return null;
     }
 
     /**
@@ -100,29 +98,16 @@ public final class PartialConverter {
             return null;
         }
 
-        DateTimeFieldType[] dateFields;
-
-        if (dateValues.length == 1) {
-            dateFields = new DateTimeFieldType[] { DateTimeFieldType.year() };
-            return new Partial(dateFields, dateValues);
-        } else if (dateValues.length == 2) {
-            dateFields = new DateTimeFieldType[] { DateTimeFieldType.year(), DateTimeFieldType.monthOfYear() };
-            return new Partial(dateFields, dateValues);
-        } else if (dateValues.length == 3) {
-            dateFields = new DateTimeFieldType[] { DateTimeFieldType.year(), DateTimeFieldType.monthOfYear(),
-                    DateTimeFieldType.dayOfMonth() };
-            return new Partial(dateFields, dateValues);
-        }
-
-        return null;
+        DateTimeFieldType[] dateFields = new DateTimeFieldType[dateValues.length];
+        System.arraycopy(PARTIAL_FIELDS, 0, dateFields, 0, dateValues.length);
+        return new Partial(dateFields, dateValues);
     }
 
     private static int[] extractDateValuesFromDateStr(String dateStrs) {
 
         List<Integer> dateValueList = new ArrayList<Integer>();
 
-        Pattern regEx = Pattern.compile(DATE_STR_REGEX);
-        Matcher matcher = regEx.matcher(dateStrs);
+        Matcher matcher = DATE_STR_REGEX.matcher(dateStrs);
 
         String dateValueStr;
 
@@ -138,12 +123,6 @@ public final class PartialConverter {
             dateValueList.add(NumberUtils.createInteger(dateValueStr));
         }
 
-        int[] dateValueArr = new int[dateValueList.size()];
-
-        for (int i = 0; i < dateValueArr.length; i++) {
-            dateValueArr[i] = dateValueList.get(i);
-        }
-
-        return dateValueArr;
+        return ArrayUtils.toPrimitive(dateValueList.toArray(new Integer[0]));
     }
 }
