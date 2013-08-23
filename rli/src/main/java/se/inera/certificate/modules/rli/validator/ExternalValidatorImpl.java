42305d38a732b4829ba5cc9747212c342e74b68a
erik forsgren <erik.forsgren@r2m.se>
2013-08-23 09:06:36 +0200

    Added utility script for creating JSON files for testing purposes
    - Added UtilScripts (only for local usage)
    - Added junit test for ExternalValidator
    - Added commentaries to ExternalValidatorImpl

--- rli/src/main/java/se/inera/certificate/modules/rli/validator/ExternalValidatorImpl.java
+++ rli/src/main/java/se/inera/certificate/modules/rli/validator/ExternalValidatorImpl.java
@@ -16,6 +16,7 @@ import se.inera.certificate.modules.rli.model.external.common.Rekommendation;
 import se.inera.certificate.validate.IdValidator;
 import se.inera.certificate.validate.SimpleIdValidatorBuilder;
 
+import se.inera.certificate.modules.rli.model.codes.AktivitetsKod;
 /**
  * @author erik
  * 
@@ -37,9 +38,9 @@ public class ExternalValidatorImpl implements ExternalValidator {
 	 */
 	@Override
 	public List<String> validate(Utlatande utlatande) {
-		
+
 		List<String> validationErrors = new ArrayList<String>();
-		
+
 		validateUtlatande(utlatande, validationErrors);
 		validatePatient(utlatande, validationErrors);
 		validateSkapasAv(utlatande, validationErrors);
@@ -47,116 +48,172 @@ public class ExternalValidatorImpl implements ExternalValidator {
 		validateAktiviteter(utlatande, validationErrors);
 		validateObservationer(utlatande, validationErrors);
 		validateArrangemang(utlatande, validationErrors);
-		
+
 		return validationErrors;
 	}
-	
-	private void validateUtlatande(Utlatande utlatande, List<String> validationErrors){
-		
-		if (utlatande.getId() == null){
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * Validates that required attributes connected with the actual class
+	 * Utlatande are present
+	 * 
+	 */
+	private void validateUtlatande(Utlatande utlatande,
+			List<String> validationErrors) {
+
+		if (utlatande.getId() == null) {
 			validationErrors.add("No Utlatande ID found");
 		}
-		
-		if (utlatande.getTyp() == null){
+
+		if (utlatande.getTyp() == null) {
 			validationErrors.add("No Utlatandetyp found");
 		}
-		
-		if (utlatande.getSigneringsdatum() == null){
+
+		if (utlatande.getSigneringsdatum() == null) {
 			validationErrors.add("No signeringsdatum found");
 		}
-		
+
 	}
 
-	private void validateArrangemang(Utlatande utlatande, List<String> validationErrors) {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * Validates Arrangemang in Utlatande First makes sure Arrangemang is not
+	 * null, if so, the required attributes are validated in turn and any errors
+	 * are added to validation error list
+	 * 
+	 */
+	private void validateArrangemang(Utlatande utlatande,
+			List<String> validationErrors) {
 		Arrangemang arrangemang = utlatande.getArrangemang();
-		
+
 		if (arrangemang == null) {
 			validationErrors.add("Arrangemang was null");
-		}
-		else {
-		
-			if (arrangemang.getBokningsdatum() == null){
+		} else {
+
+			if (arrangemang.getBokningsdatum() == null) {
 				validationErrors.add("No bokningsdatum found in arrangemang");
 			}
-			
-			if (arrangemang.getArrangemangstid() == null){
+
+			if (arrangemang.getArrangemangstid() == null) {
 				validationErrors.add("No arrangemangstid found in arrangemang");
-			}
-			
-			if (arrangemang.getPlats() == null){
+			} 
+
+			if (arrangemang.getPlats() == null) {
 				validationErrors.add("No plats found in arrangemang");
 			}
 		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * Validates Arrangemang in Utlatande First makes sure Arrangemang is not
+	 * null, if so, the required attributes are validated in turn and any errors
+	 * are added to validation error list
+	 * 
+	 */
 	private void validateSkapasAv(Utlatande utlatande, List<String> validationErrors) {
 		HosPersonal skapatAv = utlatande.getSkapadAv();
-		
+
 		if (skapatAv == null) {
 			validationErrors.add("Skapat av was null");
-		}
-		else {
+		} else {
 
-			if (skapatAv.getPersonalId() == null){
+			if (skapatAv.getPersonalId() == null) {
 				validationErrors.add("No personalId found in Skapas av");
 			}
-			
-			if (skapatAv.getFullstandigtNamn() == null){
+
+			if (skapatAv.getFullstandigtNamn() == null) {
 				validationErrors.add("No fullstandigt namn found in Skapas av");
 			}
 		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * Make sure Utlatande contains 1..* Observation
+	 * 
+	 */
 	private void validateObservationer(Utlatande utlatande, List<String> validationErrors) {
 		List<Observation> observationer = utlatande.getObservationer();
-		
+
 		if (observationer.isEmpty()) {
 			validationErrors.add("No observations found");
-		}
-		else {
-			for (Observation obs : observationer){
-				if (obs.getObservationsperiod() == null){
-					validationErrors.add("No observationsperiod found in: " + obs.getObservationskod().getCode() + "\n");
+		} else {
+			for (Observation obs : observationer) {
+				if (obs.getObservationsperiod() == null) {
+					validationErrors.add("No observationsperiod found in: "
+							+ obs.getObservationskod().getCode() + "\n");
 				}
 			}
-			
+
 		}
-		
+
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * Validate Aktiviteter,
+	 */
 	private void validateAktiviteter(Utlatande utlatande, List<String> validationErrors) {
 		List<Aktivitet> aktiviteter = utlatande.getAktiviteter();
-		
+
 		if (aktiviteter.isEmpty()) {
 			validationErrors.add("No aktiviteter found");
-		}
-		else {
-			for (Aktivitet akt : aktiviteter){
-				if (akt.getAktivitetskod() == null){
-					validationErrors.add("No aktivitetskod found in: " + akt + "\n");
+		} else {
+			/*
+			 * Make sure Utlatande contains 1..2 Aktiviteter and nothing
+			 * else
+			 */
+			if (aktiviteter.size() < 1 && aktiviteter.size() > 2) {
+				validationErrors
+						.add("Utlatande does not contain 1 or 2 Aktiviteter");
+			} else {
+				/*
+				 * Make sure all Aktiviteter contains the required attribute Aktivitetskod
+				 */
+				for (Aktivitet akt : aktiviteter) {
+					if (akt.getAktivitetskod() == null || 
+							akt.getAktivitetskod().getCodeSystem().equals(AktivitetsKod.KLINISK_UNDERSOKNING)
+									|| akt.getAktivitetskod().getCodeSystem().equals(AktivitetsKod.OMVARDNADSATGARD)) {
+						validationErrors.add("No valid aktivitetskod found in: "+ akt + "\n");
+					}
 				}
 			}
 		}
 	}
-
+	/*
+	 * (non-Javadoc)
+	 * Make sure Utlatande contains 1..* Rekommendation
+	 */
 	private void validateRekommendationer(Utlatande utlatande, List<String> validationErrors) {
 		List<Rekommendation> rekommendationer = utlatande.getRekommendationer();
 		if (rekommendationer.isEmpty()) {
-			validationErrors.add("No rekommendationer found");
-		}
-		else {
-			for (Rekommendation rek : rekommendationer){
-				if (rek.getRekommendationskod() == null){
-					validationErrors.add("No rekommendationskod found in: " + rek.getRekommendationskod().getCode()+ "\n");
-				}				
-				if (rek.getSjukdomskannedom() == null){
-					validationErrors.add("No sjukdomskannedom found in: " + rek.getRekommendationskod().getCode()+ "\n");
+			validationErrors.add("No Rekommendation found");
+		} else {
+			for (Rekommendation rek : rekommendationer) {
+				if (rek.getRekommendationskod() == null) {
+					validationErrors.add("No rekommendationskod found in: "
+							+ rek.getRekommendationskod().getCode() + "\n");
+				}
+				if (rek.getSjukdomskannedom() == null) {
+					validationErrors.add("No sjukdomskannedom found in: "
+							+ rek.getRekommendationskod().getCode() + "\n");
 				}
 			}
 		}
 	}
-
+	
+	/* (non-Javadoc)
+	 * 
+	 * Make sure Utlatande contains 1 Patient
+	 */
 	private void validatePatient(Utlatande utlatande, List<String> validationErrors) {
+	
 		Patient patient = utlatande.getPatient();
 
 		if (patient == null) {
@@ -164,6 +221,16 @@ public class ExternalValidatorImpl implements ExternalValidator {
 		}
 		else {
 			validationErrors.addAll(idValidator.validate(patient.getPersonId()));
+			
+			/*
+			 * Make sure Fornamn and Efternamn contains at least one item
+			 */
+			if (patient.getFornamns().isEmpty()){
+				validationErrors.add("At least one Fornamn must be provided for Patient");
+			}
+			if (patient.getEfternamns().isEmpty()){
+				validationErrors.add("At least one Efternamn must be provided for Patient");
+			}
 		}
 	}
 }
---
+++ rli/src/test/java/se/inera/certificate/modules/rli/model/validator/ExternalValidatorTest.java
@@ -0,0 +1,49 @@
+package se.inera.certificate.modules.rli.model.validator;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import se.inera.certificate.integration.json.CustomObjectMapper;
+import se.inera.certificate.modules.rli.model.external.Utlatande;
+import se.inera.certificate.modules.rli.validator.ExternalValidatorImpl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ExternalValidatorTest {
+	
+	private ExternalValidatorImpl validator;
+
+	@Before
+	public void setUp() throws Exception {
+		validator = new ExternalValidatorImpl();
+	}
+
+	private Utlatande buildTestUtlatande(String filename){
+		ObjectMapper mapper = new CustomObjectMapper();
+		Utlatande utlatande = new Utlatande();
+		
+		try{ utlatande = mapper.readValue(new InputStreamReader(
+				new ClassPathResource(filename).getInputStream()), Utlatande.class);
+		} catch(IOException ioe){
+			ioe.printStackTrace();
+		}
+		return utlatande;
+	}
+	@Test
+	public void testValidate() {
+		Utlatande utlatande  = buildTestUtlatande("rli-example-1-external.json");
+		List<String> validationErrors = validator.validate(utlatande);
+		
+		assertEquals("Kunde inte tolka personummer '191212121212' på formatet 'yyyyMMdd-nnnn'", validationErrors.get(0));
+		
+		
+	}
+
+}
