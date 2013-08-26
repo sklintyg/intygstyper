4e542a54ea2bc6e573ef60a4dc4b5e047e3d6dd8
Johan Lindholm <johan.lindholm@usify.se>
2013-08-26 16:15:34 +0200

    Copy changes from Lena to modules.
    (cherry picked from commit 09bdf0c0e996ad50ba88e467bb5634529450d590)
    Signed-off-by: Johan Lindholm <johan.lindholm@usify.se>

--- fk7263/src/main/webapp/webcert/js/view/messages.js
+++ fk7263/src/main/webapp/webcert/js/view/messages.js
@@ -1,8 +1,8 @@
 'use strict';
 var fk7263Messages = {
     "sv": {
-        "view.label.pagetitle": "Intyg FK7263",
-        "view.label.pagedescription" : "Författarstöd - intyg FK7263.",
+        "view.label.pagetitle": "Intyg FK 7263",
+        "view.label.pagedescription" : "Författarstöd - intyg FK 7263.",
         "view.label.header1":"Diagnos",
         "view.label.field1":"ICD 10",
         "view.label.field2":"Diagnos",
