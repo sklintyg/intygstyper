a1b7c498990806cc9e3906700f82b270902a7423
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2013-08-28 10:29:58 +0200

    updated module api to handle different transport formats when unmarshalling (cherry picked from commit aa0418ebc88bd3ae6da8fbd90791be173f19418e)
    Signed-off-by: Par Wenaker <par.wenaker@callistaenterprise.se>

--- fk7263/src/main/java/se/inera/certificate/modules/fk7263/rest/Fk7263ModuleApi.java
+++ fk7263/src/main/java/se/inera/certificate/modules/fk7263/rest/Fk7263ModuleApi.java
@@ -1,6 +1,7 @@
 package se.inera.certificate.modules.fk7263.rest;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
@@ -11,13 +12,18 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import se.inera.certificate.fk7263.model.v1.Utlatande;
 import se.inera.certificate.model.util.Strings;
 import se.inera.certificate.modules.fk7263.model.Fk7263Intyg;
 import se.inera.certificate.modules.fk7263.model.converter.ExternalToTransportFk7263LegacyConverter;
+import se.inera.certificate.modules.fk7263.model.converter.LegacyTransportJaxbToGenericTransportJaxbConverter;
 import se.inera.certificate.modules.fk7263.model.converter.TransportToExternalConverter;
 import se.inera.certificate.modules.fk7263.model.external.Fk7263CertificateContentHolder;
 import se.inera.certificate.modules.fk7263.model.external.Fk7263Utlatande;
@@ -28,27 +34,41 @@ import se.inera.ifv.insuranceprocess.healthreporting.registermedicalcertificate.
 import com.itextpdf.text.DocumentException;
 
 /**
- * @author andreaskaltenbach
+ * @author andreaskaltenbach, marced
  */
 public class Fk7263ModuleApi {
 
     private static final Logger LOG = LoggerFactory.getLogger(Fk7263ModuleApi.class);
 
+    private static final Unmarshaller unmarshaller;
+
+    // Create unmarshaller for the transport format(s) this module can handle
+    static {
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(Utlatande.class, RegisterMedicalCertificate.class);
+            unmarshaller = jaxbContext.createUnmarshaller();
+        } catch (JAXBException e) {
+            throw new RuntimeException("Failed to create JAXB context", e);
+        }
+    }
+
     /**
-     * @param utlatande
+     * @param transportXml
      * @return
      */
     @POST
     @Path("/unmarshall")
     @Consumes(MediaType.APPLICATION_XML)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response unmarshall(se.inera.certificate.fk7263.model.v1.Utlatande fk7263UtlatandeJaxb) {
-        // TODO: Schema validation according to fk7263_intyg.xsd
+    public Response unmarshall(String transportXml) {
 
-        Fk7263Utlatande externalModel = TransportToExternalConverter.convert(fk7263UtlatandeJaxb);
+        Object jaxbObject = unmarshallTransportXML(transportXml);
+
+        Fk7263Utlatande externalModel = convertTransportJaxbToModel(jaxbObject);
 
         Fk7263Intyg internalModel = new Fk7263Intyg(externalModel);
 
+        // validate
         List<String> validationErrors = new UtlatandeValidator(internalModel).validate();
 
         if (validationErrors.isEmpty()) {
@@ -56,6 +76,42 @@ public class Fk7263ModuleApi {
         } else {
             String response = Strings.join(",", validationErrors);
             return Response.status(Response.Status.BAD_REQUEST).entity(response).build();
+        }
+    }
+
+    /**
+     * Converts different types of transportJaxb object to the external module model format.
+     * @param jaxbObject
+     * @return Fk7263Utlatande
+     */
+    private Fk7263Utlatande convertTransportJaxbToModel(Object jaxbObject) {
+        if (jaxbObject instanceof Utlatande) {
+            return convertToModel((Utlatande) jaxbObject);
+        } else if (jaxbObject instanceof RegisterMedicalCertificate) {
+            LOG.debug("Converting legacy transport -> generic transport format");
+            RegisterMedicalCertificate legacyJaxb = (RegisterMedicalCertificate) jaxbObject;
+            Utlatande genericUtlatande = LegacyTransportJaxbToGenericTransportJaxbConverter.convert(legacyJaxb.getLakarutlatande());
+            return convertToModel((Utlatande) genericUtlatande);
+        }
+        return null;
+    }
+
+    private Fk7263Utlatande convertToModel(Utlatande genericUtlatande) {
+        LOG.debug("Converting " + genericUtlatande.getClass().getCanonicalName() + " to externalModuleFormat");
+        return TransportToExternalConverter.convert(genericUtlatande);
+    }
+
+    /**
+     * Unmarshal xml string into jaxb
+     * @param transportXml
+     * @return jaxbObject if unmarshalling was successful
+     */
+    private Object unmarshallTransportXML(String transportXml) {
+        try {
+            return unmarshaller.unmarshal(new StringReader(transportXml));
+        } catch (JAXBException e) {
+            LOG.error("Failed to unmarshal transportXml", e);
+            throw new RuntimeException(e);
         }
     }
 
@@ -89,7 +145,7 @@ public class Fk7263ModuleApi {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces("application/pdf")
     public Response pdf(Fk7263CertificateContentHolder contentHolder) {
-        // create the internal model that pdf generator exspects
+        // create the internal model that pdf generator expects
         Fk7263Intyg intyg = new Fk7263Intyg(contentHolder.getCertificateContent());
         try {
             byte[] generatedPdf = new PdfGenerator(intyg).getBytes();
