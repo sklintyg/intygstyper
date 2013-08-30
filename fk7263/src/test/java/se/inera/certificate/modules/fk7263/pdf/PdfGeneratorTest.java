e4d60b2e5bd5da99f1c2f692758570c38340446f
Pierre Sandboge <pierre.sandboge@callistaenterprise.se>
2013-08-30 15:10:47 +0200

    INTYG-368 Replaced FK7263 v8 with v9, and changed associated tests. Updated corresponding text for "arbetslöshet" in citizen web. (cherry picked from commit 8902e78ac38c47dcc9203ce01690c3e5589ddbf3)
    Signed-off-by: Par Wenaker <par.wenaker@callistaenterprise.se>

--- fk7263/src/test/java/se/inera/certificate/modules/fk7263/pdf/PdfGeneratorTest.java
+++ fk7263/src/test/java/se/inera/certificate/modules/fk7263/pdf/PdfGeneratorTest.java
@@ -1,14 +1,17 @@
 package se.inera.certificate.modules.fk7263.pdf;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.AcroFields;
 import com.itextpdf.text.pdf.PdfReader;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
 import se.inera.certificate.integration.json.CustomObjectMapper;
@@ -46,6 +49,33 @@ public class PdfGeneratorTest {
             assertEquals("Value for field " + fieldKey + " is not the expected",
                     expectedFields.getField(fieldKey), generatedFields.getField(fieldKey));
         }
+    }
+
+    /**
+     * This test creates a new document to compare against. The new document ends up in the project root.
+     *
+     * @throws IOException
+     * @throws DocumentException
+     */
+    @Ignore
+    @Test
+    public void createTestPdf() throws IOException, DocumentException {
+
+        Fk7263Intyg intyg = new CustomObjectMapper().readValue(fk7263_json, Fk7263Intyg.class);
+
+        // generate PDF
+        byte[] generatorResult = new PdfGenerator(intyg).getBytes();
+
+        File newTestPdf = new File("utlatande.pdf");
+        boolean created = newTestPdf.createNewFile();
+        if (created) {
+            FileOutputStream out = new FileOutputStream(newTestPdf);
+            out.write(generatorResult);
+            out.close();
+        } else {
+            fail();
+        }
+
     }
 
     private AcroFields readExpectedFields() throws IOException {
