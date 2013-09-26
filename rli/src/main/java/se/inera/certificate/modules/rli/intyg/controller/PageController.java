3d50b35ad5f495486cac1e9083863a9634b9aead
Gustav Norbäcker <gustav.norbacker@r2m.se>
2013-09-26 14:07:39 +0200

    Moved the PageController for MI to a better place.

--- rli/src/main/java/se/inera/certificate/modules/rli/intyg/controller/PageController.java
+++ rli/src/main/java/se/inera/certificate/modules/rli/intyg/controller/PageController.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package se.inera.certificate.modules.rli.controller;
+package se.inera.certificate.modules.rli.intyg.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
