5958096a5fd8e3f791cb07c2b8f578f3bf0364c2
Bjorn Beskow <bb@callistaenterprise.se>
2013-11-11 18:50:34 +0100

    Tagit bort eli from release-branch.

--- rli/src/main/java/se/inera/certificate/modules/rli/model/converters/UndersokningPopulator.java
+++
@@ -1,9 +0,0 @@
-package se.inera.certificate.modules.rli.model.converters;
-
-import se.inera.certificate.modules.rli.model.internal.Undersokning;
-
-public interface UndersokningPopulator {
-
-    Undersokning createAndPopulateUndersokning(se.inera.certificate.modules.rli.model.external.Utlatande extUtlatande);
-
-}
