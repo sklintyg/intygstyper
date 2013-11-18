8438363bc085e5dd32945d9878473cc49bd7d1fb
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2013-11-18 12:59:23 +0100

    made a directive to encapsulate generic datepicker-popup state toggling

--- fk7263/src/main/webapp/webcert/js/view/directives.js
+++ fk7263/src/main/webapp/webcert/js/view/directives.js
@@ -1,53 +1,73 @@
 /**
- * Directives used only in module app pages. 
+ * Directives used only in module app pages.
  */
 angular.module('wc.fk7263.directives', []);
 
 /**
- * Scrolls viewport to attached element if 'scrollHereIf' attribute evaluates as true
+ * Scrolls viewport to attached element if 'scrollHereIf' attribute evaluates as
+ * true
  */
-angular.module('wc.fk7263.directives').directive('scrollHereIf', function () {
-    return function (scope, element, attributes) {
-      setTimeout(function () {
-        if (scope.$eval(attributes.scrollHereIf)) {
-          window.scrollTo(0, element[0].offsetTop - 100)
-        }
-      });
+angular.module('wc.fk7263.directives').directive('scrollHereIf', function() {
+    return function(scope, element, attributes) {
+        setTimeout(function() {
+            if (scope.$eval(attributes.scrollHereIf)) {
+                window.scrollTo(0, element[0].offsetTop - 100)
+            }
+        });
     }
-  });
+});
 
-angular.module('wc.fk7263.directives').directive("wcCertField", ['$rootScope', function($rootScope) {
-  return {
-      restrict : "A",
-      transclude : true,
-      replace : true,
-      scope : {
-        fieldLabel: "@",
-        fieldNumber: "@",
-        filled: "=?"
-      },
-      template :
-      		'<div class="body-row clearfix">'
-      			+'<h4 class="cert-field-number"><span message key="view.label.field"></span> {{fieldNumber}}</h4>'
-      			+'<h3 class="title" ng-class="{ \'unfilled\' : !filled}"><span message key="{{ fieldLabel }}"></span> <span class="cert-field-blank" ng-hide="filled"><span message key="view.label.blank"></span></span></h3>'
-      			+'<span class="text" ng-show="filled">'
-      			+'<span ng-transclude></span>'
-      			+'</span>'
-         +'</div>'
+angular.module('wc.fk7263.directives').directive(
+        "wcDatePickerField",
+        [
+                '$rootScope','$timeout',
+                function($rootScope, $timeout) {
+                    return {
+                        restrict : "A",
+                        replace : true,
+                        scope : {
+                            targetModel : "=",
+                            domId:"@",
+                            onChange: "&"
+                        },
+                        controller : function($scope, $element, $attrs) {
+                            // Expose "now" as a model property for the template
+                            // to render as todays date
+                            $scope.isOpen = false;
+                            $scope.toggleOpen = function() {
+                                $timeout(function() {
+                                    $scope.isOpen = !$scope.isOpen;
+                                });
+                            }
 
+                        },
+                        template : '<span>'
+                                   + '<input id="{{domId}}" type="text" datepicker-popup ng-model="targetModel" is-open="isOpen" ng-change="onChange()">'
+                                   + '<button id="{{domId}}-toggle" class="btn" ng-click="toggleOpen()" ng-disabled="isOpen"><i class="icon-calendar"></i></button>'
+                                   +'<span>'
+                    }
+                } ]);
 
-/*
-	//TODO: fix this to use default value for filled if it is omitted (it is already optional but default value is undefined =?)
-	compile: function(element,attrs)
-    {
-	    var filled = attrs.filled || true;
-	    var htmlText = '<div class="body-row">'
-	               +'   <h3 class="title" ng-class="{ \'unfilled\' : !'+filled+'}"><span message key="{{ title }}"></span> <span ng-hide="'+filled+'"><span message key="view.label.blank"></span></span></h3>'
-	               +'   <span class="text" ng-show="'+filled+'">'
-	               +'       <span ng-transclude></span>'
-	               +'   </span>'
-	               +'</div>';
-	    element.replaceWith(htmlText);
-    }*/
-  }
-} ]);
+angular
+        .module('wc.fk7263.directives')
+        .directive(
+                "wcCertField",
+                [
+                        '$rootScope',
+                        function($rootScope) {
+                            return {
+                                restrict : "A",
+                                transclude : true,
+                                replace : true,
+                                scope : {
+                                    fieldLabel : "@",
+                                    fieldNumber : "@",
+                                    filled : "=?"
+                                },
+                                template : '<div class="body-row clearfix">'
+                                        + '<h4 class="cert-field-number"><span message key="view.label.field"></span> {{fieldNumber}}</h4>'
+                                        + '<h3 class="title" ng-class="{ \'unfilled\' : !filled}"><span message key="{{ fieldLabel }}"></span> <span class="cert-field-blank" ng-hide="filled"><span message key="view.label.blank"></span></span></h3>'
+                                        + '<span class="text" ng-show="filled">' + '<span ng-transclude></span>' + '</span>' + '</div>'
+
+                            }
+                        } ]);
