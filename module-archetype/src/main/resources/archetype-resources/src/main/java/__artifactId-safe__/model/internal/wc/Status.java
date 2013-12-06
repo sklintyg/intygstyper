3d0415f3fd39f57b6f3b5092f0193f88424e3533
Gustav Norbäcker <gustav.norbacker@r2m.se>
2013-12-06 14:49:10 +0100

    Added support for artifactId containing '-'

---
+++ module-archetype/src/main/resources/archetype-resources/src/main/java/__artifactId-safe__/model/internal/wc/Status.java
@@ -0,0 +1,62 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/**
+ * Copyright (C) 2013 Inera AB (http://www.inera.se)
+ *
+ * This file is part of Inera Certificate Modules (http://code.google.com/p/inera-certificate-modules).
+ *
+ * Inera Certificate Modules is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Inera Certificate Modules is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ${package}.${artifactId-safe}.model.internal.wc;
+
+import org.joda.time.LocalDateTime;
+
+public class Status {
+
+    private String type;
+
+    private String target;
+
+    private LocalDateTime timestamp;
+
+    public Status() {
+
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+}
---
+++ module-archetype/src/main/resources/archetype-resources/src/test/java/__artifactId-safe__/model/converter/ExternalToInternalConverterTest.java
@@ -0,0 +1,59 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/**
+ * Copyright (C) 2013 Inera AB (http://www.inera.se)
+ *
+ * This file is part of Inera Certificate Modules (http://code.google.com/p/inera-certificate-modules).
+ *
+ * Inera Certificate Modules is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Inera Certificate Modules is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ${package}.${artifactId-safe}.model.converter;
+
+import static org.unitils.reflectionassert.ReflectionAssert.assertLenientEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ${package}.${artifactId-safe}.rest.dto.CertificateContentHolder;
+import ${package}.${artifactId-safe}.utils.Scenario;
+import ${package}.${artifactId-safe}.utils.ScenarioFinder;
+
+/**
+ * Unit test for the ExternalToInteralConverter. This test is Spring configured.
+ * 
+ * @author Niklas Pettersson, R2M
+ * 
+ */
+public class ExternalToInternalConverterTest {
+
+    private static ExternalToInternalConverter converter;
+
+    @Before
+    public void setUp() throws Exception {
+        converter = new ExternalToInternalConverter();
+    }
+
+    @Test
+    public void testConvertUtlatande() throws Exception {
+        for (Scenario scenario : ScenarioFinder.getExternalScenarios("valid-*")) {
+            CertificateContentHolder extUtlatande = scenario.asExternalModelWithHolder();
+
+            ${package}.${artifactId-safe}.model.internal.mi.Utlatande actual = converter.convert(extUtlatande);
+
+            ${package}.${artifactId-safe}.model.internal.mi.Utlatande expected = scenario.asInternalMIModel();
+            assertLenientEquals("Error in scenario " + scenario.getName(), expected, actual);
+        }
+    }
+}
