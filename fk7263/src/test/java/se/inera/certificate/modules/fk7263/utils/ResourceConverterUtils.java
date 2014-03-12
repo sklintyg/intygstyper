37829528343851b9de40d0062a87c48d3639bef1
Erik Forsgren <erik.forsgren@r2m.se>
2014-03-12 15:57:12 +0100

    Added test utils as implemented in TS-bas etc.
    - Added the scenario methodology
    - Added unitils dependency to test scope
    - Added new assert method ModelAssert for deep reflection assertion on json-structs etc.

---
+++ fk7263/src/test/java/se/inera/certificate/modules/fk7263/utils/ResourceConverterUtils.java
@@ -0,0 +1,52 @@
+package se.inera.certificate.modules.fk7263.utils;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.bind.JAXB;
+
+import se.inera.certificate.integration.json.CustomObjectMapper;
+import se.inera.certificate.integration.rest.dto.CertificateContentMeta;
+import se.inera.certificate.modules.fk7263.rest.dto.CertificateContentHolder;
+import se.inera.certificate.fk7263.model.v1.Utlatande;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ResourceConverterUtils {
+
+    private static final ObjectMapper OBJECT_MAPPER = new CustomObjectMapper();
+
+    public static Utlatande toTransport(File resource) throws IOException {
+        return JAXB.unmarshal(resource, Utlatande.class);
+    }
+
+    public static se.inera.certificate.modules.fk7263.model.external.Fk7263Utlatande toExternal(File resource)
+            throws IOException {
+        return OBJECT_MAPPER.readValue(resource, se.inera.certificate.modules.fk7263.model.external.Fk7263Utlatande.class);
+    }
+
+    public static CertificateContentHolder toExternalWithHolder(File resource) throws IOException {
+        return wrapExternalWithHolder(toExternal(resource));
+    }
+
+    public static CertificateContentHolder wrapExternalWithHolder(
+            se.inera.certificate.modules.fk7263.model.external.Fk7263Utlatande externalModel) throws IOException {
+        CertificateContentHolder result = new CertificateContentHolder();
+
+        result.setCertificateContent(externalModel);
+
+        CertificateContentMeta meta = new CertificateContentMeta();
+        meta.setId(result.getCertificateContent().getId().getRoot());
+        meta.setType(result.getCertificateContent().getTyp().getCode());
+        meta.setPatientId(result.getCertificateContent().getPatient().getId().getExtension());
+
+        result.setCertificateContentMeta(meta);
+
+        return result;
+    }
+
+    public static se.inera.certificate.modules.fk7263.model.internal.Fk7263Intyg toInternal(File resource)
+            throws IOException {
+        return OBJECT_MAPPER.readValue(resource, se.inera.certificate.modules.fk7263.model.internal.Fk7263Intyg.class);
+    }
+}
---
+++ fk7263/src/test/java/se/inera/certificate/modules/fk7263/utils/Scenario.java
@@ -0,0 +1,60 @@
+package se.inera.certificate.modules.fk7263.utils;
+
+import se.inera.certificate.modules.fk7263.rest.dto.CertificateContentHolder;
+import se.inera.certificate.fk7263.model.v1.Utlatande;
+
+/**
+ * Defines a scenario that can be tested. The following models (as POJOs) can be extracted from a scenario:
+ * <ul>
+ * <li>Transport model
+ * <li>Export model (with and without a {@link CertificateContentHolder})
+ * <li>Internal model
+ * </ul>
+ * 
+ * @see ScenarioFinder
+ */
+public interface Scenario {
+
+    /**
+     * Returns the name of the scenario. Useful for assertion messages.
+     * 
+     * @return The scenario name.
+     */
+    String getName();
+
+    /**
+     * Returns the scenario as a transport model.
+     * 
+     * @return The scenario as a transport model.
+     * @throws ScenarioNotFoundException
+     *             if the scenario wasn't found.
+     */
+    Utlatande asTransportModel() throws ScenarioNotFoundException;
+
+    /**
+     * Returns the scenario as a external model.
+     * 
+     * @return The scenario as a external model.
+     * @throws ScenarioNotFoundException
+     *             if the scenario wasn't found.
+     */
+    se.inera.certificate.modules.fk7263.model.external.Fk7263Utlatande asExternalModel() throws ScenarioNotFoundException;
+
+    /**
+     * Returns the scenario as a external model, wrapped in a {@link CertificateContentHolder}.
+     * 
+     * @return The scenario as a wrapped external model.
+     * @throws ScenarioNotFoundException
+     *             if the scenario wasn't found.
+     */
+    CertificateContentHolder asExternalModelWithHolder() throws ScenarioNotFoundException;
+
+    /**
+     * Returns the scenario as a internal Mina Intyg model.
+     * 
+     * @return The scenario as a internal Mina Intyg model.
+     * @throws ScenarioNotFoundException
+     *             if the scenario wasn't found.
+     */
+    se.inera.certificate.modules.fk7263.model.internal.Fk7263Intyg asInternalModel() throws ScenarioNotFoundException;
+}
