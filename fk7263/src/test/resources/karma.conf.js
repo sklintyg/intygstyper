9e55c3a01081bd335b5d15c7273a08d2f560d6f4
Magnus Larsson <magnus@magnuss-mbp.svr-domain.local>
2014-03-12 13:42:33 +0100

    Format date from date picker as String instead of Date.

---
+++ fk7263/src/test/resources/karma.conf.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/* global module */
+module.exports = function (config) {
+    var WEB_CERT_WEBAPP = "../../../../../inera-webcert/webcert/src/main/webapp/js/";
+    config.set({
+
+        autoWatch : true,
+
+        basePath : '../../main/webapp',
+
+        //browsers: ['Chrome', 'ChromeCanary', 'Firefox', 'PhantomJS'],
+        browsers : ['PhantomJS'],
+
+        files : [
+            WEB_CERT_WEBAPP + 'vendor/angular/1.1.5/angular.js', WEB_CERT_WEBAPP + 'vendor/angular/1.1.5/angular-mocks.js', 'webcert/js/view/fk7263-app.js',
+            'webcert/js/**/*.js', '../../test/js/webcert/**/*.js'
+        ],
+
+        frameworks : ['jasmine'],
+
+        junitReporter : {
+            outputFile : '../../../../target/surefire-reports/karma-test-results.xml'
+        },
+
+        plugins : [
+            'karma-jasmine', 'karma-junit-reporter', 'karma-chrome-launcher', 'karma-firefox-launcher',
+            'karma-phantomjs-launcher'
+        ],
+
+        reporters : ['progress', 'junit']
+    });
+};
