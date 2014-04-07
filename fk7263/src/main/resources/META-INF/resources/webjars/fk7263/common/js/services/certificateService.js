dd408fedbdd525bba64c2be411352ba517430d81
Benny Samuelsson <benny.samuelsson@usify.se>
2014-04-07 14:08:21 +0200

    Formatted file

--- fk7263/src/main/resources/META-INF/resources/webjars/fk7263/common/js/services/certificateService.js
+++ fk7263/src/main/resources/META-INF/resources/webjars/fk7263/common/js/services/certificateService.js
@@ -5,24 +5,25 @@ define([
     return ['$http', '$log',
         function ($http, $log) {
 
-	        /*
-	         * Load certificate details from the server.
-	         */
-	        function _getCertificate (id, onSuccess, onError) {
-	            $log.debug('_getCertificate id:' + id);
-	            var restPath = '/moduleapi/intyg/signed/' + id;
-	            $http.get(restPath).success(function (data) {
-	                $log.debug('_getCertificate data:' + data);
-	                onSuccess(data);
-	            }).error(function (data, status) {
-	                $log.error('error ' + status);
-	                onError(data);
-	            });
-	        }
+            /*
+             * Load certificate details from the server.
+             */
+            function _getCertificate(id, onSuccess, onError) {
+                $log.debug('_getCertificate id:' + id);
+                var restPath = '/moduleapi/intyg/signed/' + id;
+                $http.get(restPath).success(function (data) {
+                    $log.debug('_getCertificate data:' + data);
+                    onSuccess(data);
+                }).error(function (data, status) {
+                    $log.error('error ' + status);
+                    onError(data);
+                });
+            }
+
             /**
              * Get a certificate draft with the specified id from the server.
              */
-            function _getDraft (id, onSuccess, onError) {
+            function _getDraft(id, onSuccess, onError) {
                 $log.debug('_getDraft id: ' + id);
                 var restPath = '/moduleapi/intyg/draft/' + id;
                 $http.get(restPath).
@@ -39,7 +40,7 @@ define([
             /**
              * Saves a certificate draft to the server.
              */
-            function _saveDraft (id, cert, onSuccess, onError) {
+            function _saveDraft(id, cert, onSuccess, onError) {
                 $log.debug('_saveDraft id: ' + id);
                 var restPath = '/moduleapi/intyg/draft/' + id;
                 $http.put(restPath, cert).
@@ -56,7 +57,7 @@ define([
             /**
              * Discards a certificate draft and removes it from the server.
              */
-            function _discardDraft (id, onSuccess, onError) {
+            function _discardDraft(id, onSuccess, onError) {
                 $log.debug('_discardDraft id: ' + id);
                 var restPath = '/moduleapi/intyg/draft/' + id;
                 $http.remove(restPath).
@@ -72,10 +73,10 @@ define([
 
             // Return public API for the service
             return {
-            	getCertificate : _getCertificate,
-                getDraft : _getDraft,
-                saveDraft : _saveDraft,
-                discardDraft : _discardDraft
+                getCertificate: _getCertificate,
+                getDraft: _getDraft,
+                saveDraft: _saveDraft,
+                discardDraft: _discardDraft
             };
         }];
 });
