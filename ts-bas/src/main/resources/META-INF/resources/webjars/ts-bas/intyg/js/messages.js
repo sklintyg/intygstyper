78fd1e9af30d0776e7fcfa1c22537bad46ee8af6
Erik Forsgren <erik.forsgren@r2m.se>
2014-04-10 10:28:42 +0200

    Städade bort RLI-grejer från Ts-Bas intygs-vy

--- ts-bas/src/main/resources/META-INF/resources/webjars/ts-bas/intyg/js/messages.js
+++ ts-bas/src/main/resources/META-INF/resources/webjars/ts-bas/intyg/js/messages.js
@@ -23,7 +23,39 @@ define(
 
 			return {
 				"sv" : {
-					"view.label.certtitle" : "Läkarintyg vid avbeställd resa",
+		            "ts.label.intygavser" : "Intyget avser",
+		            "ts.label.identitet" : "Identiteten är styrkt genom",
+		            "ts.label.synfunktioner" : "1. Synfunktioner",
+		            "ts.label.horselbalans" : "2. Hörsel och balanssinne",
+		            "ts.label.funktionsnedsattning" : "3. Rörelseorganens funktioner",
+		            "ts.label.hjartkarl" : "4. Hjärt- och kärlsjukdomar",
+		            "ts.label.diabetes" : "5. Diabetes",
+		            "ts.label.neurologi" : "6. Neurologiska sjukdomar",
+		            "ts.label.medvetandestorning" : "7. Epilepsi, epileptiskt anfall och annan medvetandestörning",
+		            "ts.label.njurar" : "8. Njursjukdomar",
+		            "ts.label.kognitivt" : "9. Demens och andra kognitiva störningar",
+		            "ts.label.somnvakenhet" : "10. Sömn- och vakenhetsstörningar",
+		            "ts.label.narkotikalakemedel" : "11. Alkohol, narkotika och läkemedel",
+		            "ts.label.psykiskt" : "12. Psykiska sjukdomar och störningar",
+		            "ts.label.utvecklingsstorning" : "13. ADHD, autismspektrumtillstånd och likartade tillstånd samt psykisk utvecklingsstörning",
+		            "ts.label.sjukhusvard" : "14. Sjukhusvård",
+		            "ts.label.medicinering" : "15. Övrig medicinering",
+		            "ts.label.ovrigkommentar" : "16. Övrig kommentar",
+		            "ts.label.bedomning" : "Bedömning",
+		            "ts.label.unit" : "Vårdenhet",
+
+		            "ts.label.korkort.c1" : "C1",
+		            "ts.label.korkort.c1e" : "C1E",
+		            "ts.label.korkort.c" : "C",
+		            "ts.label.korkort.ce" : "CE",
+		            "ts.label.korkort.d1" : "D1",
+		            "ts.label.korkort.d1e" : "D1E",
+		            "ts.label.korkort.d" : "D",
+		            "ts.label.korkort.de" : "DE",
+		            "ts.label.korkort.taxi" : "Taxi",
+		            "ts.label.korkort.annat" : "Annat",
+					
+		            "view.label.certtitle" : "Läkarintyg Transportstyrelsen Bas",
 					"view.label.pagetitle" : "Granska och skicka",
 					"view.label.pagedescription" : "Här kan du titta på ditt intyg och skicka det. Om du upptäcker några felaktigheter så hör av dig enheten som utfärdade ditt intyg. Du kan även öppna intyget som en PDF och skriva ut och spara.",
 					"view.label.patientname" : "Patientens namn:",
@@ -44,8 +76,8 @@ define(
 					"view.label.latestevent.noevents" : "Inga händelser",
 					"view.label.latestevent.showall" : "Visa alla händelser",
 					"history.label.pagetitle" : "Intygets alla händelser",
-					"rli.status.sent" : "Skickat till",
-					"rli.target.mi" : "Försäkringsbolaget"
+					"ts.status.sent" : "Skickat till",
+					"ts.target.mi" : "Försäkringsbolaget"
 
 				},
 				"en" : {
