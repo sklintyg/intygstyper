933e690647a10de8772706036b898b69572f6be5
Joakim Kemeny <joakim@kemeny.se>
2014-04-28 13:25:51 +0200

    Kör JSHint och CSSLint som en del av bygget.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,56 @@
+/* global module */
 module.exports = function(grunt) {
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.initConfig({
-        jshint: {
-            files: ['Gruntfile.js', 'src/main/javascript/*.js','src/test/javascript/*.js']
-        }
-    });
-    grunt.registerTask('default', ['jshint']);
+	'use strict';
+
+	grunt.loadNpmTasks('grunt-contrib-csslint');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+
+	grunt.initConfig({
+
+		csslint : {
+			dev : {
+				options : {
+					csslintrc : 'src/main/resources/.csslintrc',
+					force : true
+				},
+				src : [ '*/src/main/resources/META-INF/resources/webjars/**/*.css' ]
+			},
+			build : {
+				options : {
+					csslintrc : 'src/main/resources/.csslintrc',
+					force : true,
+					formatters : [ {
+						id : 'checkstyle-xml',
+						dest : 'target/checkstyle-csslint-result.xml'
+					} ],
+					absoluteFilePathsForFormatters : true
+				},
+				src : [ '*/src/main/resources/META-INF/resources/webjars/**/*.css' ]
+			}
+		},
+
+		jshint : {
+			dev : {
+				options : {
+					jshintrc : 'src/main/resources/.jshintrc',
+					force : true
+				},
+				src : [ 'Gruntfile.js', '*/src/main/resources/META-INF/resources/webjars/**/*.js',
+						'*/src/test/js/**/*.js' ]
+			},
+			build : {
+				options : {
+					jshintrc : 'src/main/resources/.jshintrc',
+					force : true,
+					reporter : 'checkstyle',
+					reporterOutput : 'target/checkstyle-jshint-result.xml'
+				},
+				src : [ 'Gruntfile.js', '*/src/main/resources/META-INF/resources/webjars/**/*.js',
+						'*/src/test/js/**/*.js' ]
+			}
+		}
+	});
+
+	grunt.registerTask('dev', [ 'csslint:dev', 'jshint:dev' ]);
+	grunt.registerTask('default', [ 'csslint:build', 'jshint:build' ]);
 };
