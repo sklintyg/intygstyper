02b47518fe4c19dfbfb01a4325ac12a19563fa0a
Joakim Kemeny <joakim@kemeny.se>
2014-05-02 11:15:15 +0200

    Förenklade byggfilerna och anpassade dem så att de matchar dokumentationen.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,36 @@
 /* global module */
 module.exports = function(grunt) {
-	'use strict';
+    'use strict';
 
-	grunt.loadNpmTasks('grunt-contrib-csslint');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
-	grunt.initConfig({
+    grunt.initConfig({
 
-		csslint : {
-			dev : {
-				options : {
-					csslintrc : 'src/main/resources/.csslintrc',
-					force : true
-				},
-				src : [ '*/src/main/resources/META-INF/resources/webjars/**/*.css' ]
-			},
-			build : {
-				options : {
-					csslintrc : 'src/main/resources/.csslintrc',
-					force : true,
-					formatters : [ {
-						id : 'checkstyle-xml',
-						dest : 'target/checkstyle-csslint-result.xml'
-					} ],
-					absoluteFilePathsForFormatters : true
-				},
-				src : [ '*/src/main/resources/META-INF/resources/webjars/**/*.css' ]
-			}
-		},
+        csslint: {
+            dev: {
+                options: {
+                    csslintrc: 'src/main/resources/.csslintrc',
+                    force: true
+                },
+                src: [ '*/src/main/resources/META-INF/resources/webjars/**/*.css' ]
+            }
+        },
 
-		jshint : {
-			dev : {
-				options : {
-					jshintrc : 'src/main/resources/.jshintrc',
-					force : true
-				},
-				src : [ 'Gruntfile.js', '*/src/main/resources/META-INF/resources/webjars/**/*.js',
-						'*/src/test/js/**/*.js' ]
-			},
-			build : {
-				options : {
-					jshintrc : 'src/main/resources/.jshintrc',
-					force : true,
-					reporter : 'checkstyle',
-					reporterOutput : 'target/checkstyle-jshint-result.xml'
-				},
-				src : [ 'Gruntfile.js', '*/src/main/resources/META-INF/resources/webjars/**/*.js',
-						'*/src/test/js/**/*.js' ]
-			}
-		}
-	});
+        jshint: {
+            dev: {
+                options: {
+                    jshintrc: 'src/main/resources/.jshintrc',
+                    force: true
+                },
+                src: [
+                    'Gruntfile.js',
+                    '*/src/main/resources/META-INF/resources/webjars/**/*.js',
+                    '*/src/test/**/*.js'
+                ]
+            }
+        }
+    });
 
-	grunt.registerTask('dev', [ 'csslint:dev', 'jshint:dev' ]);
-	grunt.registerTask('default', [ 'csslint:build', 'jshint:build' ]);
+    grunt.registerTask('default', [ 'jshint', 'csslint' ]);
 };
