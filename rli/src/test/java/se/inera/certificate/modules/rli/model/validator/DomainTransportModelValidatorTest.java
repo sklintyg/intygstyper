77a39fbdf071ad2f1e7fb30ddb0c73db7e3eaad0
Gustav Norbäcker <gustav.norbacker@r2m.se>
2014-05-12 16:22:20 +0200

    INTYG-1121 Test: validerar intyg enl. domänmodell.
    Lade till unittest för alla intygstyper som försäkrar att testdata
    validerar enligt domänmodellen.

---
+++ rli/src/test/java/se/inera/certificate/modules/rli/model/validator/DomainTransportModelValidatorTest.java
@@ -0,0 +1,70 @@
+package se.inera.certificate.modules.rli.model.validator;
+
+import static org.junit.Assert.fail;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import javax.xml.XMLConstants;
+import javax.xml.bind.JAXB;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.xml.sax.SAXException;
+
+import se.inera.certificate.modules.rli.utils.Scenario;
+import se.inera.certificate.modules.rli.utils.ScenarioFinder;
+
+public class DomainTransportModelValidatorTest {
+
+    private static final String COMMON_UTLATANDE_SCHEMA = "/schemas/clinicalprocess_healthcond_certificate/core_components/clinicalprocess_healthcond_certificate_0.9.xsd";
+
+    private static final String COMMON_UTLATANDE_ISO_SCHEMA = "/schemas/clinicalprocess_healthcond_certificate/core_components/iso_dt_subset_1.0.xsd";
+
+    private static Schema commonSchema;
+
+    @BeforeClass
+    public static void initCommonSchema() throws Exception {
+        Source isoSchemaFile = new StreamSource(new ClassPathResource(COMMON_UTLATANDE_ISO_SCHEMA).getInputStream());
+        Source utlatandeSchemaFile = new StreamSource(new ClassPathResource(COMMON_UTLATANDE_SCHEMA).getInputStream());
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        commonSchema = schemaFactory.newSchema(new Source[] { isoSchemaFile, utlatandeSchemaFile });
+    }
+
+    @Test
+    public void testValidateTransportXmlAgainstDomainModel() throws Exception {
+        // Check that valid scenarios validates against the common domain model
+        for (Scenario scenario : ScenarioFinder.getTransportScenarios("valid-*")) {
+            validateUtlatande(scenario);
+        }
+
+        // Also check that invalid scenarios doesn't validate
+        for (Scenario scenario : ScenarioFinder.getTransportScenarios("invalid-*")) {
+            try {
+                validateUtlatande(scenario);
+                fail("Expected schema validation error");
+            } catch (Exception ignore) {
+            }
+        }
+    }
+
+    private void validateUtlatande(Scenario scenario) {
+        try {
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            JAXB.marshal(scenario.asTransportModel(), output);
+
+            Validator validator = commonSchema.newValidator();
+            validator.validate(new StreamSource(new ByteArrayInputStream(output.toByteArray())));
+
+        } catch (Exception e) {
+            throw new RuntimeException(String.format("Error in scenario %s: %s", scenario.getName(), e.getMessage()));
+        }
+    }
+}
