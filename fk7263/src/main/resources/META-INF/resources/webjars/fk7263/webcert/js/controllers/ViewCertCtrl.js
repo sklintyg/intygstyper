131c391b6d678a1423a1f0cedc79e1bf70cc72d6
Benny Samuelsson <benny.samuelsson@usify.se>
2014-06-04 14:24:52 +0200

    Lade till kopiering på visasidan för fk-intyget

--- fk7263/src/main/resources/META-INF/resources/webjars/fk7263/webcert/js/controllers/ViewCertCtrl.js
+++ fk7263/src/main/resources/META-INF/resources/webjars/fk7263/webcert/js/controllers/ViewCertCtrl.js
@@ -1,20 +1,33 @@
 define([
     'angular',
     'webjars/common/webcert/js/services/CertificateService',
-    'webjars/common/webcert/js/services/ManageCertView'
-], function(angular, CertificateService, ManageCertView) {
+    'webjars/common/webcert/js/services/ManageCertView',
+    'services/ManageCertificate'
+], function(angular, CertificateService, ManageCertView, ManageCertificate) {
     'use strict';
 
     var moduleName = 'fk7263.ViewCertCtrl';
 
-    angular.module(moduleName, []).
-        controller(moduleName, ['$log', '$rootScope', '$routeParams', '$scope', CertificateService, ManageCertView,
-            function($log, $rootScope, $routeParams, $scope, CertificateService, ManageCertView) {
+    angular.module(moduleName, [ManageCertView, ManageCertificate]).
+        controller(moduleName, ['$log', '$rootScope', '$routeParams', '$scope', '$cookieStore', CertificateService, ManageCertView, ManageCertificate,
+            function($log, $rootScope, $routeParams, $scope, $cookieStore, CertificateService, ManageCertView, ManageCertificate) {
 
+                // Copy dialog setup
+                var COPY_DIALOG_COOKIE = 'wc.dontShowCopyDialog';
+                var copyDialog = {
+                    isOpen: false
+                };
+                $scope.dialog = {
+                    acceptprogressdone: true,
+                    focus: false,
+                    errormessageid: 'error.failedtocopyintyg',
+                    showerror: false,
+                    dontShowCopyInfo: $cookieStore.get(COPY_DIALOG_COOKIE)
+                };
+
+                // Page setup
                 $scope.cert = {};
                 $scope.cert.filledAlways = true;
-
-                // init state
                 $scope.widgetState = {
                     doneLoading: false,
                     activeErrorMessageKey: null,
@@ -49,6 +62,20 @@ define([
                     $log.debug('Got error while loading cert');
                     $log.debug(error.message);
                 });
+
+                /**
+                 * Private
+                 */
+
+
+                /**
+                 * Exposed functions
+                 * @param cert
+                 */
+
+                $scope.copy = function(cert) {
+                    copyDialog = ManageCertificate.copy($scope, cert, COPY_DIALOG_COOKIE);
+                };
             }
         ]);
 
