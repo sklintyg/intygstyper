48c3db9c7a32fe812cbda0ee3f0128e8343a3c82
Gustav Norbäcker <gustav.norbacker@r2m.se>
2014-08-19 15:31:21 +0200

    Dokumenterade SchemaValidatorBuilder.

--- module-support/src/main/java/se/inera/certificate/xml/SchemaValidatorBuilder.java
+++ module-support/src/main/java/se/inera/certificate/xml/SchemaValidatorBuilder.java
@@ -19,6 +19,21 @@ import org.w3c.dom.ls.LSInput;
 import org.w3c.dom.ls.LSResourceResolver;
 import org.xml.sax.SAXException;
 
+/**
+ * Utility that aids in creating an XML validator from complex XSD files (which include and/or import other XSD files).
+ * Create an instance of the builder and register all resources (XSD files) that the builder needs to know about. When a
+ * {@link Schema} is generated the root XSD must be specified.
+ * <p>
+ * Example:
+ * 
+ * <pre>
+ * SchemaValidatorBuilder schemaValidatorBuilder = new SchemaValidatorBuilder();
+ * Source rootSource = schemaValidatorBuilder.registerResource(&quot;root-schema.xsd&quot;);
+ * schemaValidatorBuilder.registerResource(&quot;included-schema.xsd&quot;);
+ * schemaValidatorBuilder.registerResource(&quot;imported-schema.xsd&quot;);
+ * Schema schema = schemaValidatorBuilder.build(rootSource);
+ * </pre>
+ */
 public class SchemaValidatorBuilder {
 
     private final List<Source> sources;
@@ -27,14 +42,38 @@ public class SchemaValidatorBuilder {
         this.sources = new ArrayList<>();
     }
 
+    /**
+     * Register an resource that can be fount on the classpath.
+     * 
+     * @param classPathResouce
+     *            The XSD classpath resource path.
+     * 
+     * @return The {@link Source} representation of the resource. Useful when calling {@link #build(Source)} if the
+     *         specified resource is the root source.
+     * 
+     * @throws IOException
+     *             if the resource could not be found.
+     */
     public Source registerResource(String classPathResouce) throws IOException {
         Source source = new StreamSource(new ClassPathResource(classPathResouce).getInputStream());
+        // We define the systemId as the last path segment of the classPathResource (the filename).
         source.setSystemId(getLastPathSegment(classPathResouce));
         sources.add(source);
 
         return source;
     }
 
+    /**
+     * Builds a {@link Schema} that can be used to validate XML agains the complex XSD schema.
+     * 
+     * @param rootSource
+     *            The XSD that is the root schema (including and importing other schemas).
+     * 
+     * @return A schema used for validation.
+     * 
+     * @throws SAXException
+     *             If the schema could not be generated.
+     */
     public Schema build(Source rootSource) throws SAXException {
         SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
         schemaFactory.setResourceResolver(new ResourceResolver());
@@ -49,6 +88,11 @@ public class SchemaValidatorBuilder {
         return path;
     }
 
+    /**
+     * A simple {@link LSResourceResolver} implementation that resolves resources based on the sysmteId against those
+     * registered in the builder. <br>
+     * NOTE: The resources are returned as input streams, meaning they can only be read once.
+     */
     private class ResourceResolver implements LSResourceResolver {
 
         @Override
