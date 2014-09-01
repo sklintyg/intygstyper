e7d5fc8cc49bc550d2fb3a040bc4df197bd44f90
Gustav Norbäcker <gustav.norbacker@r2m.se>
2014-09-01 11:56:54 +0200

    INTYG-1070 Dokumenterade certificate state enum.

--- module-support/src/main/java/se/inera/certificate/model/CertificateState.java
+++ module-support/src/main/java/se/inera/certificate/model/CertificateState.java
@@ -4,12 +4,27 @@ package se.inera.certificate.model;
  * @author andreaskaltenbach
  */
 public enum CertificateState {
+    /** This code is unused */
     UNHANDLED,
+
+    /** The intyg is arhcived in Mina Intyg ('arkiverad') */
     DELETED,
+
+    /** The intyg is no longer archived in Mina Intyg */
     RESTORED,
+
+    /** The intyg is 'makulerat' */
     CANCELLED,
+
+    /** The intyg is sent to a recipient */
     SENT,
+
+    /** The intyg was stored in Intygstjänsten */
     RECEIVED,
+
+    /** This code is unused (except for some tests) */
     IN_PROGRESS,
+
+    /** This code is unused (except for some tests) */
     PROCESSED
 }
