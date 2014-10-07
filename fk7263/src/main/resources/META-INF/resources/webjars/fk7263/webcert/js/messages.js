f6c7ad41aff11234f9bc4a1915c18beb6c6edfec
Joakim Kemeny <joakim@kemeny.se>
2014-10-07 10:52:02 +0200

    WEBCERT-1053 Uppdaterat texten som vissas som kvittens när användaren har återtagit ett intyg.

--- fk7263/src/main/resources/META-INF/resources/webjars/fk7263/webcert/js/messages.js
+++ fk7263/src/main/resources/META-INF/resources/webjars/fk7263/webcert/js/messages.js
@@ -61,7 +61,7 @@ var fk7263Messages = {
         'fk7263.label.rekommendationer.fk': 'Rekommendationer till Försäkringskassan',
         'fk7263.label.fk-kontakt': 'Kontakt',
         'fk7263.label.vardenhet.adress': 'Vårdenhetens kontaktuppgifter',
-        'fk7263.label.makulera.confirmation': 'Läkarintyg FK 7263 utställt på ${namn} - ${personnummer} har makulerats. Intyget kan inte längre visas av patienten.',
+        'fk7263.label.makulera.confirmation': 'Läkarintyg FK 7263 utställd på ${namn} – ${personnummer} är återtaget. Mottagare av intyget är notifierade om detta.',
 
         'fk7263.vardkontakt.undersokning': 'Min undersökning av patienten den ',
         'fk7263.vardkontakt.telefonkontakt': 'Min telefonkontakt med patienten den ',
