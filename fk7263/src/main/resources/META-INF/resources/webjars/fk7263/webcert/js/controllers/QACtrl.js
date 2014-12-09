ed495abc7a46c0ab65ecfd1c2d44e632c1f2f5cf
stephenwhite <stephen.white@mac.com>
2014-12-09 14:44:40 +0100

    [webcert-928] - addition of update all unhandled

--- fk7263/src/main/resources/META-INF/resources/webjars/fk7263/webcert/js/controllers/QACtrl.js
+++ fk7263/src/main/resources/META-INF/resources/webjars/fk7263/webcert/js/controllers/QACtrl.js
@@ -1,8 +1,8 @@
 angular.module('fk7263').controller('fk7263.QACtrl',
-    [ '$log', '$rootScope', '$routeParams', '$scope', '$timeout', '$window', 'common.dialogService',
+    [ '$log', '$rootScope', '$routeParams', '$scope', '$timeout', '$window', '$filter', 'common.dialogService',
         'fk7263.fragaSvarService', 'common.fragaSvarCommonService', 'common.ManageCertView', 'common.statService',
         'common.User',
-        function($log, $rootScope, $routeParams, $scope, $timeout, $window, dialogService, fragaSvarService,
+        function($log, $rootScope, $routeParams, $scope, $timeout, $window, $filter, dialogService, fragaSvarService,
             fragaSvarCommonService, ManageCertView, statService, User) {
             'use strict';
 
@@ -183,6 +183,27 @@ angular.module('fk7263').controller('fk7263.QACtrl',
                     });
             };
 
+            $scope.updateAllAsHandled = function(){
+                angular.forEach($scope.qaList, function(qa) {
+                    if(fragaSvarCommonService.isUnhandled(qa)){
+                        $scope.updateAsHandled(qa);
+                    }
+                });
+            };
+
+            $scope.hasUnhandledQas = function(){
+                if(!$scope.qaList || $scope.qaList.length === 0){
+                    return false;
+                }
+                for (var i = 0, len = $scope.qaList.length; i < len; i++) {
+                    var qa = $scope.qaList[i];
+                    if(fragaSvarCommonService.isUnhandled(qa)){
+                        return true;
+                    }
+                }
+                return false;
+            };
+
             $scope.updateAsHandled = function(qa) {
                 $log.debug('updateAsHandled:' + qa);
                 qa.updateHandledStateInProgress = true;
@@ -288,4 +309,18 @@ angular.module('fk7263').controller('fk7263.QACtrl',
                 $scope.newQuestion.chosenTopic = $scope.newQuestion.topics[0]; // 'Välj ämne' is default
             };
             $scope.initQuestionForm();
+
+            // listeners - interscope communication
+            var unbindMarkAllAsHandledEvent = $scope.$on('markAllAsHandledEvent', function($event) {
+                $scope.updateAllAsHandled();
+            });
+
+            $scope.$on('$destroy', unbindMarkAllAsHandledEvent);
+
+            var unbindHasUnhandledQasEvent = $scope.$on('hasUnhandledQasEvent', function($event, deferred) {
+                deferred.resolve($scope.hasUnhandledQas());
+            });
+
+            $scope.$on('$destroy', unbindHasUnhandledQasEvent);
+
         }]);
