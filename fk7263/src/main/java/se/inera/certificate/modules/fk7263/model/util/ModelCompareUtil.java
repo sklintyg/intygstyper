c71cf64f2f2578c68cacbe7ab7b889d8173c64b8
Erik Forsgren <erik.forsgren@r2m.se>
2015-01-13 13:14:52 +0100

    Implementerat metod som kollar om modell-förändringar skett mellan två givna tillstånd
    - util-klassen ModelCompareUtil innehåller jämförelse-logiken

---
+++ fk7263/src/main/java/se/inera/certificate/modules/fk7263/model/util/ModelCompareUtil.java
@@ -0,0 +1,85 @@
+package se.inera.certificate.modules.fk7263.model.util;
+
+import java.util.Arrays;
+
+import org.apache.commons.lang3.StringUtils;
+
+import se.inera.certificate.model.InternalLocalDateInterval;
+import se.inera.certificate.modules.fk7263.model.internal.Utlatande;
+
+/**
+ * Util for checking a models consistency in different states.
+ * @author erik
+ *
+ */
+public class ModelCompareUtil {
+
+    /**
+     * Check if two models differ.
+     * @param oldUtlatande
+     * @param newUtlatande
+     * @return true if they do, false otherwise
+     */
+    public static boolean modelDiffers(Utlatande oldUtlatande, Utlatande newUtlatande) {
+        return diagnoseDiffers(oldUtlatande, newUtlatande)
+        || sjukskrivningsperiodDiffers(oldUtlatande, newUtlatande)
+        || sjukskrivningsgradDiffers(oldUtlatande, newUtlatande);
+    }
+    
+    private static boolean sjukskrivningsgradDiffers(Utlatande oldUtlatande, Utlatande newUtlatande) {
+        int[] oldSjukskrivningsgrad = makeIntMatrix(oldUtlatande);
+        int[] newSjukskrivningsgrad = makeIntMatrix(newUtlatande);
+        return Arrays.equals(oldSjukskrivningsgrad,newSjukskrivningsgrad) ? false : true;
+    }
+
+    private static int[] makeIntMatrix(Utlatande source) {
+        int[] matrix = new int[4];
+        matrix[0] = source.getNedsattMed100() != null ? 1 : 0; 
+        matrix[1] = source.getNedsattMed75() != null ? 1 : 0; 
+        matrix[2] = source.getNedsattMed50() != null ? 1 : 0; 
+        matrix[3] = source.getNedsattMed25() != null ? 1 : 0; 
+        return matrix;
+    }
+
+    private static boolean sjukskrivningsperiodDiffers(Utlatande oldUtlatande, Utlatande newUtlatande) {
+        return checkPerioderDiffers(oldUtlatande.getNedsattMed100(), newUtlatande.getNedsattMed100())
+            || checkPerioderDiffers(oldUtlatande.getNedsattMed75(),newUtlatande.getNedsattMed75())
+            || checkPerioderDiffers(oldUtlatande.getNedsattMed50(),newUtlatande.getNedsattMed50())
+            || checkPerioderDiffers(oldUtlatande.getNedsattMed25(),newUtlatande.getNedsattMed25());
+        
+    }
+
+    private static boolean checkPerioderDiffers(InternalLocalDateInterval oldPeriod, InternalLocalDateInterval newPeriod) {
+        if (oldPeriod != null && newPeriod != null) {
+            return oldPeriod.equals(newPeriod) ? false : true;
+        } else if ((oldPeriod == null && newPeriod != null) || (oldPeriod != null && newPeriod == null)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean diagnoseDiffers(Utlatande oldUtlatande, Utlatande newUtlatande) {
+        boolean differs = false;
+        String[] diagnoskoderOld = {oldUtlatande.getDiagnosKod(), oldUtlatande.getDiagnosKod2(), oldUtlatande.getDiagnosKod3()}; 
+        String[] diagnoskoderNew = {newUtlatande.getDiagnosKod(), newUtlatande.getDiagnosKod2(), newUtlatande.getDiagnosKod3()}; 
+
+        String[] diagnosbeskrivningOld = {oldUtlatande.getDiagnosBeskrivning1(), oldUtlatande.getDiagnosBeskrivning2(), oldUtlatande.getDiagnosBeskrivning3()}; 
+        String[] diagnosbeskrivningNew = {newUtlatande.getDiagnosBeskrivning1(), newUtlatande.getDiagnosBeskrivning2(), newUtlatande.getDiagnosBeskrivning3()}; 
+
+        differs = checkStringArray(diagnoskoderOld, diagnoskoderNew) || checkStringArray(diagnosbeskrivningOld, diagnosbeskrivningNew);
+
+        return differs;
+    }
+
+    private static boolean checkStringArray(String[] oldArray, String[] newArray) {
+        for (int i = 0; i < oldArray.length; i++) {
+            if ((!StringUtils.isEmpty(oldArray[i]) && StringUtils.isEmpty(newArray[i])) || (StringUtils.isEmpty(oldArray[i]) && !StringUtils.isEmpty(newArray[i]))) {
+                return true;
+            } else if (oldArray[i] != null && newArray[i] != null) {
+                return oldArray[i].equals(newArray[i]) ? false : true;
+            } 
+        }
+        return false;
+    }
+}
--- fk7263/src/test/java/se/inera/certificate/modules/fk7263/rest/Fk7263ModuleApiTest.java
+++ fk7263/src/test/java/se/inera/certificate/modules/fk7263/rest/Fk7263ModuleApiTest.java
@@ -1,10 +1,13 @@
 package se.inera.certificate.modules.fk7263.rest;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.verify;
 
 import java.io.IOException;
+import java.io.StringWriter;
 
 import org.apache.commons.io.FileUtils;
 import org.joda.time.LocalDateTime;
@@ -19,6 +22,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.w3.wsaddressing10.AttributedURIType;
 
 import se.inera.certificate.integration.json.CustomObjectMapper;
+import se.inera.certificate.model.InternalLocalDateInterval;
 import se.inera.certificate.modules.fk7263.model.internal.Utlatande;
 import se.inera.certificate.modules.fk7263.utils.ResourceConverterUtils;
 import se.inera.certificate.modules.support.api.dto.CreateNewDraftHolder;
@@ -112,6 +116,83 @@ public class Fk7263ModuleApiTest {
         Mockito.when(registerMedicalCertificateClient.registerMedicalCertificate(Mockito.any(AttributedURIType.class), Mockito.any(RegisterMedicalCertificateType.class))).thenReturn(response);
         fk7263ModuleApi.sendCertificateToRecipient(internalModel, "logicalAddress");
         verify(registerMedicalCertificateClient).registerMedicalCertificate(Mockito.eq(address), Mockito.any(RegisterMedicalCertificateType.class));
+    }
+
+    @Test
+    public void testModelIsNotChanged() throws Exception {
+        String utlatandeOldString = toJsonString(new CustomObjectMapper().readValue(new ClassPathResource(
+                "Fk7263ModuleApiTest/utlatande.json").getFile(), Utlatande.class));
+        String utlatandeNewString = utlatandeOldString;
+        assertFalse(fk7263ModuleApi.isModelChanged(utlatandeOldString, utlatandeNewString));
+
+    }
+
+    @Test
+    public void testModelIsChangedNedsattningsgradNull() throws Exception {
+        Utlatande utlatandeOld = new CustomObjectMapper().readValue(new ClassPathResource(
+                "Fk7263ModuleApiTest/utlatande.json").getFile(), Utlatande.class);
+        Utlatande utlatandeNew = new CustomObjectMapper().readValue(new ClassPathResource(
+                "Fk7263ModuleApiTest/utlatande.json").getFile(), Utlatande.class);
+        utlatandeNew.setNedsattMed100(null);
+
+        String utlatandeOldString = toJsonString(utlatandeOld);
+        String utlatandeNewString = toJsonString(utlatandeNew);
+        assertTrue(fk7263ModuleApi.isModelChanged(utlatandeOldString, utlatandeNewString));
+    }
+
+    @Test
+    public void testModelIsChangedNedsattningsgradDate() throws Exception {
+        Utlatande utlatandeOld = new CustomObjectMapper().readValue(new ClassPathResource(
+                "Fk7263ModuleApiTest/utlatande.json").getFile(), Utlatande.class);
+        Utlatande utlatandeNew = new CustomObjectMapper().readValue(new ClassPathResource(
+                "Fk7263ModuleApiTest/utlatande.json").getFile(), Utlatande.class);
+
+        // Change the date and ensure this is recognized as a change in the model
+        utlatandeNew.setNedsattMed100(new InternalLocalDateInterval("2011-03-03", "2011-04-04"));
+        
+        String utlatandeOldString = toJsonString(utlatandeOld);
+        String utlatandeNewString = toJsonString(utlatandeNew);
+        assertTrue(fk7263ModuleApi.isModelChanged(utlatandeOldString, utlatandeNewString));
+    }
+
+    @Test
+    public void testModelIsChangedDiagnoskod() throws Exception {
+        Utlatande utlatandeOld = new CustomObjectMapper().readValue(new ClassPathResource(
+                "Fk7263ModuleApiTest/utlatande.json").getFile(), Utlatande.class);
+        Utlatande utlatandeNew = new CustomObjectMapper().readValue(new ClassPathResource(
+                "Fk7263ModuleApiTest/utlatande.json").getFile(), Utlatande.class);
+        
+        // Mess with the diagnose and make sure the change registers
+        utlatandeNew.setDiagnosKod("BLAH");
+        
+        String utlatandeOldString = toJsonString(utlatandeOld);
+        String utlatandeNewString = toJsonString(utlatandeNew);
+        assertTrue(fk7263ModuleApi.isModelChanged(utlatandeOldString, utlatandeNewString));
+    }
+
+    @Test
+    public void testModelIsChangedDiagnosbeskrivning() throws Exception {
+        Utlatande utlatandeOld = new CustomObjectMapper().readValue(new ClassPathResource(
+                "Fk7263ModuleApiTest/utlatande.json").getFile(), Utlatande.class);
+        Utlatande utlatandeNew = new CustomObjectMapper().readValue(new ClassPathResource(
+                "Fk7263ModuleApiTest/utlatande.json").getFile(), Utlatande.class);
+        
+        // Mess with the diagnose and make sure the change registers
+        utlatandeNew.setDiagnosBeskrivning1("BLAH");
+        
+        String utlatandeOldString = toJsonString(utlatandeOld);
+        String utlatandeNewString = toJsonString(utlatandeNew);
+        assertTrue(fk7263ModuleApi.isModelChanged(utlatandeOldString, utlatandeNewString));
+    }
+
+    private String toJsonString(Utlatande utlatande) throws ModuleException {
+        StringWriter writer = new StringWriter();
+        try {
+            objectMapper.writeValue(writer, utlatande);
+        } catch (IOException e) {
+            throw new ModuleException("Failed to serialize internal model", e);
+        }
+        return writer.toString();
     }
 
     private CreateNewDraftHolder createNewDraftHolder() {
