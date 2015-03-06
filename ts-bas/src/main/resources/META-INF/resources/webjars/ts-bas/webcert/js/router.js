2df48798258c3c7f1e09a496b01ce410379b28f2
stephenwhite <stephen.white@mac.com>
2015-03-06 15:47:22 +0100

    [webcert-844] - rebase from dev

--- ts-bas/src/main/resources/META-INF/resources/webjars/ts-bas/webcert/js/router.js
+++ ts-bas/src/main/resources/META-INF/resources/webjars/ts-bas/webcert/js/router.js
@@ -4,7 +4,7 @@
 angular.module('ts-bas.router', ['ui.router']).config(function($stateProvider) {
     'use strict';
     $stateProvider.
-        state('ts-bas-edit', {
+        state('ts-bas.edit', {
             url : '/ts-bas/edit/:certificateId',
             templateUrl: '/web/webjars/ts-bas/webcert/views/edit-cert.html',
             controller: 'ts-bas.EditCertCtrl'
