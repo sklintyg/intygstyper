4b76472cd4f502769338fa33a32dcbf55ed389f5
stephenwhite <stephen.white@mac.com>
2015-03-06 09:20:17 +0100

    [webcert-844] - name changes on state names '-' is the dot '.' delimeter, ui.router uses the dot to denote child states ..

--- ts-diabetes/src/main/resources/META-INF/resources/webjars/ts-diabetes/webcert/js/router.js
+++ ts-diabetes/src/main/resources/META-INF/resources/webjars/ts-diabetes/webcert/js/router.js
@@ -4,7 +4,7 @@
 angular.module('ts-diabetes.router', ['ui.router']).config(function($stateProvider) {
     'use strict';
     $stateProvider.
-        state('ts-diabetes.edit', {
+        state('ts-diabetes-edit', {
             url: '/ts-diabetes/edit/:certificateId',
             templateUrl: '/web/webjars/ts-diabetes/webcert/views/edit-cert.html',
             controller: 'ts-diabetes.EditCertCtrl'
