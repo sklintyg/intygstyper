1543e1dbc875d605ce102b857663c5abb72466f2
Jesper Holmberg <jesper.holmberg@callistaenterprise.se>
2015-03-31 16:34:24 +0200

    Första version av XSL transformer

---
+++ ts-bas/src/test/java/se/inera/certificate/modules/ts_bas/transformation/TSBasTransformerTest.java
@@ -0,0 +1,63 @@
+package se.inera.certificate.modules.ts_bas.transformation;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.fail;
+
+import java.io.ByteArrayInputStream;
+import java.net.URL;
+import java.util.List;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+
+import org.junit.Test;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+
+public class TSBasTransformerTest {
+
+    @Test
+    public void testTransformation() throws Exception {
+        List<String> testFiles = asList("ts-bas-max.xml", "valid-diabetes-typ2-kost.xml",
+                "valid-korrigerad-synskarpa.xml", "valid-maximal.xml", "valid-minimal.xml",
+                "valid-persontransport.xml", "valid-sjukhusvard.xml", "valid-utan-korrigerad-synskarpa.xml");
+
+        XslTransformer transformer = new XslTransformer("xsl/transform-ts-bas.xsl");
+
+        for (String xmlFile : testFiles) {
+            String xmlContents = Resources.toString(getResource("scenarios/transport/" + xmlFile), Charsets.UTF_8);
+
+            if (!validateAgainstXSD(new StreamSource(new ByteArrayInputStream(xmlContents.getBytes(Charsets.UTF_8))),
+                    "intygstjanster-services/core-components/se_intygstjanster_services_1.0.xsd")) {
+                fail();
+            }
+
+            String result = transformer.transform(xmlContents);
+
+            if (!validateAgainstXSD(new StreamSource(new ByteArrayInputStream(result.getBytes(Charsets.UTF_8))),
+                    "clinicalprocess-healthcond-certificate/specializations/TS-Bas/ts-bas_model.xsd")) {
+                fail();
+            }
+        }
+    }
+
+    private static boolean validateAgainstXSD(StreamSource xml, String xsdPath) {
+        try {
+            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = factory.newSchema(Thread.currentThread().getContextClassLoader().getResource(xsdPath));
+            schema.newValidator().validate(xml);
+            return true;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+
+    private static URL getResource(String href) {
+        return Thread.currentThread().getContextClassLoader().getResource(href);
+    }
+
+}
