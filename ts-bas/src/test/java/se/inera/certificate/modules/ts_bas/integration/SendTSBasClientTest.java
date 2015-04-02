3b917fa3b5b9e2ceb81acff94fa79b39ae9f78d8
Jesper Holmberg <jesper.holmberg@callistaenterprise.se>
2015-04-02 13:28:34 +0200

    WEBCERT-1651: använd den publicerade katalogstrukturen som utgångspunkt för våra egna scheman

---
+++ ts-bas/src/test/java/se/inera/certificate/modules/ts_bas/integration/SendTSBasClientTest.java
@@ -0,0 +1,15 @@
+package se.inera.certificate.modules.ts_bas.integration;
+
+import junit.framework.TestCase;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class SendTSBasClientTest extends TestCase {
+
+    @Ignore
+    public void testSendStuff() {
+        SendTSBasClient client = new SendTSBasClient();
+        client.sendStuff(new byte[] {});
+    }
+
+}
