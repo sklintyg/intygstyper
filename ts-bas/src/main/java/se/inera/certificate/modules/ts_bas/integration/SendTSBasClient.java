4bec967b1df4168d2773e892097c9f4140f6a282
Erik Forsgren <erik.forsgren@r2m.se>
2015-04-09 10:42:26 +0200

    Fixat med namespaces i XSLT i ts-bas

--- ts-bas/src/main/java/se/inera/certificate/modules/ts_bas/integration/SendTSBasClient.java
+++ ts-bas/src/main/java/se/inera/certificate/modules/ts_bas/integration/SendTSBasClient.java
@@ -2,20 +2,31 @@ package se.inera.certificate.modules.ts_bas.integration;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPBody;
 import javax.xml.soap.SOAPException;
 import javax.xml.soap.SOAPMessage;
 import javax.xml.ws.Dispatch;
 import javax.xml.ws.Service;
 import javax.xml.ws.soap.SOAPBinding;
 
-import com.google.common.base.Charsets;
-import com.google.common.base.Throwables;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
 import se.inera.certificate.clinicalprocess.healthcond.certificate.registerCertificate.v1.RegisterCertificateResponderService;
 
+import com.google.common.base.Throwables;
+
 public class SendTSBasClient {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SendTSBasClient.class);
 
     private static final QName PORT_NAME =
             new QName("urn:riv:clinicalprocess:healthcond:certificate:RegisterCertificate:1:rivtabp21", "RegisterCertificateResponderPort");
@@ -23,12 +34,14 @@ public class SendTSBasClient {
     private final RegisterCertificateResponderService service;
 
     private final MessageFactory messageFactory;
+    private final DocumentBuilderFactory builderFactory;
 
     public SendTSBasClient(String url) {
         service = new RegisterCertificateResponderService();
         service.addPort(PORT_NAME, SOAPBinding.SOAP11HTTP_BINDING, url);
         try {
             messageFactory = MessageFactory.newInstance();
+            builderFactory = DocumentBuilderFactory.newInstance();
         } catch (SOAPException e) {
             throw Throwables.propagate(e);
         }
@@ -36,13 +49,23 @@ public class SendTSBasClient {
 
     public SOAPMessage registerCertificate(String message) {
         try {
-            SOAPMessage soapReq1 = messageFactory.createMessage(null, new ByteArrayInputStream(message.getBytes(Charsets.UTF_8)));
+            LOGGER.debug("Creating SoapMessage in sendTsBasClient");
+            SOAPMessage soapReq1 = messageFactory.createMessage();
+
+            // Create a Document from the byte stream
+            InputStream is = new ByteArrayInputStream(message.getBytes());
+            builderFactory.setNamespaceAware(true);
+            Document doc = builderFactory.newDocumentBuilder().parse(is); 
+            
+            // Add content to soap message body
+            SOAPBody body = soapReq1.getSOAPBody();
+            body.addDocument(doc);
 
             Dispatch<SOAPMessage> dispSOAPMsg = service.createDispatch(PORT_NAME, SOAPMessage.class, Service.Mode.MESSAGE);
 
             return dispSOAPMsg.invoke(soapReq1);
 
-        } catch (SOAPException | IOException e) {
+        } catch (SOAPException | SAXException | IOException | ParserConfigurationException e) {
             throw Throwables.propagate(e);
         }
     }
--- ts-bas/src/test/java/se/inera/certificate/modules/ts_bas/transformation/TSBasTransformerTest.java
+++ ts-bas/src/test/java/se/inera/certificate/modules/ts_bas/transformation/TSBasTransformerTest.java
@@ -7,21 +7,63 @@ import java.io.ByteArrayInputStream;
 import java.net.URL;
 import java.util.List;
 
-import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import se.inera.certificate.xml.SchemaValidatorBuilder;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 
 public class TSBasTransformerTest {
 
+    private static final String COMMON_UTLATANDE_SCHEMA = "/intygstjanster-services/core-components/se_intygstjanster_services_1.0.xsd";
+
+    private static final String COMMON_UTLATANDE_TYPES_SCHEMA = "/intygstjanster-services/core-components/se_intygstjanster_services_types_1.0.xsd";
+
+    private static final String COMMON_REGISTER_SCHEMA = "/interactions/RegisterTSBasInteraction/RegisterTSBasResponder_1.0.xsd";
+
+    private static final String CLINICAL_TS_BAS_SCHEMA = "specializations/TS-Bas/ts-bas_model.xsd";
+
+    private static final String CLINICAL_CORE_SCHEMA = "core_components/clinicalprocess_healthcond_certificate_1.0.xsd";
+
+    private static final String CLINICAL_TYPES_SCHEMA = "core_components/clinicalprocess_healthcond_certificate_types_1.0.xsd";
+
+    private static final String CLINIAL_ISO_SCHEMA = "/insuranceprocess-healthreporting/core-components/iso_dt_subset_1.0.xsd";
+
+    private static final String CLINIAL_REGISTER_SCHEMA = "interactions/RegisterCertificateInteraction/RegisterCertificateResponder_1.0.xsd";
+
+    private static Schema intygstjansterSchema;
+
+    private static Schema clinicalSchema;
+
+    @BeforeClass
+    public static void initIntygstjansterSchema() throws Exception {
+        SchemaValidatorBuilder schemaValidatorBuilder = new SchemaValidatorBuilder();
+        Source rootSource = schemaValidatorBuilder.registerResource(COMMON_REGISTER_SCHEMA);
+        schemaValidatorBuilder.registerResource(COMMON_UTLATANDE_SCHEMA);
+        schemaValidatorBuilder.registerResource(COMMON_UTLATANDE_TYPES_SCHEMA);
+        intygstjansterSchema = schemaValidatorBuilder.build(rootSource);
+    }
+
+    @BeforeClass
+    public static void initClinicalSchema() throws Exception {
+        SchemaValidatorBuilder schemaValidatorBuilder = new SchemaValidatorBuilder();
+        Source rootSource = schemaValidatorBuilder.registerResource(CLINIAL_REGISTER_SCHEMA);
+        schemaValidatorBuilder.registerResource(CLINICAL_CORE_SCHEMA);
+        schemaValidatorBuilder.registerResource(CLINICAL_TYPES_SCHEMA);
+        schemaValidatorBuilder.registerResource(CLINICAL_TS_BAS_SCHEMA);
+        schemaValidatorBuilder.registerResource(CLINIAL_ISO_SCHEMA);
+        clinicalSchema = schemaValidatorBuilder.build(rootSource);
+    }
+
     @Test
     public void testTransformation() throws Exception {
-        List<String> testFiles = asList("ts-bas-max.xml", "valid-diabetes-typ2-kost.xml",
+        List<String> testFiles = asList("xsl.xml", "ts-bas-max.xml", "valid-diabetes-typ2-kost.xml",
                 "valid-korrigerad-synskarpa.xml", "valid-maximal.xml", "valid-minimal.xml",
                 "valid-persontransport.xml", "valid-sjukhusvard.xml", "valid-utan-korrigerad-synskarpa.xml");
 
@@ -30,24 +72,33 @@ public class TSBasTransformerTest {
         for (String xmlFile : testFiles) {
             String xmlContents = Resources.toString(getResource("scenarios/transport/" + xmlFile), Charsets.UTF_8);
 
-            if (!validateAgainstXSD(xmlContents, "intygstjanster-services/core-components/se_intygstjanster_services_1.0.xsd")) {
+            if (!validateIntygstjansterXSD(xmlContents)) {
                 fail();
             }
 
             String result = transformer.transform(xmlContents);
 
-            if (!validateAgainstXSD(result, "specializations/TS-Bas/ts-bas_model.xsd")) {
+            if (!validateClinicalXSD(result)) {
                 fail();
             }
         }
     }
 
-    private static boolean validateAgainstXSD(String xml, String xsdPath) {
+    private static boolean validateIntygstjansterXSD(String xml) {
         StreamSource xmlSource = new StreamSource(new ByteArrayInputStream(xml.getBytes(Charsets.UTF_8)));
         try {
-            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            Schema schema = factory.newSchema(Thread.currentThread().getContextClassLoader().getResource(xsdPath));
-            schema.newValidator().validate(xmlSource);
+            intygstjansterSchema.newValidator().validate(xmlSource);
+            return true;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+
+    private static boolean validateClinicalXSD(String xml) {
+        StreamSource xmlSource = new StreamSource(new ByteArrayInputStream(xml.getBytes(Charsets.UTF_8)));
+        try {
+            clinicalSchema.newValidator().validate(xmlSource);
             return true;
         } catch (Exception ex) {
             ex.printStackTrace();
