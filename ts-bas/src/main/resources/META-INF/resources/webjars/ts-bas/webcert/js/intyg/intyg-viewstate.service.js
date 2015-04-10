fe74b6623cae68e332cf3b425626543d2c4f6e45
Benny Samuelsson <benny.samuelsson@usify.se>
2015-04-10 17:07:23 +0200

    ts-bas intygController now uses its own viewstate and common viewstate. files are also renamed and moved to intyg.

---
+++ ts-bas/src/main/resources/META-INF/resources/webjars/ts-bas/webcert/js/intyg/intyg-viewstate.service.js
@@ -0,0 +1,7 @@
+angular.module('ts-bas').service('ts-bas.IntygController.ViewStateService',
+    ['$log', 'common.IntygViewStateService',
+        function($log, CommonViewState) {
+            'use strict';
+
+            this.common = CommonViewState;
+        }]);
