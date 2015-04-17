e757413063a038251c5aef8fe62898f035a83cba
Benny Samuelsson <benny.samuelsson@usify.se>
2015-04-17 15:44:13 +0200

    Fixade jasmine dependency test för ts-diabetes.

--- ts-diabetes/src/test/js/webcert/controllers/EditCertCtrlSpec.js
+++ ts-diabetes/src/test/js/webcert/controllers/EditCertCtrlSpec.js
@@ -14,6 +14,7 @@ describe('ts-diabetes.EditCertCtrl', function() {
         $provide.value('common.UserModel', UserModel);
         $provide.value('common.wcFocus', wcFocus);
         $provide.value('common.intygNotifyService', intygNotifyService);
+        $provide.value('common.DateUtilsService', {});
         $provide.value('common.IntygEditViewStateService',{intyg:{}});
     }));
 
