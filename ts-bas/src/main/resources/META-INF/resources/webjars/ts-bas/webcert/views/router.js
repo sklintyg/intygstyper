60c68f6005ce5eb37fb4a1ec11d841d98318628e
stephenwhite <stephen.white@callistaenterprise.se>
2015-05-20 14:11:08 +0200

    [webcert-1757] - first cut, split of utkast.html

--- ts-bas/src/main/resources/META-INF/resources/webjars/ts-bas/webcert/views/router.js
+++ ts-bas/src/main/resources/META-INF/resources/webjars/ts-bas/webcert/views/router.js
@@ -15,13 +15,83 @@ angular.module('ts-bas').config(function($stateProvider) {
                     templateUrl: '/web/webjars/common/webcert/utkast/utkast-header/utkast-header.html',
                     controller: 'common.UtkastHeader'
                 },
+
+                'patient@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/patient.html'
+                },
+                'intention@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/intention.html'
+                },
+                'identity@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/identity.html'
+                },
+                'alert@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/alert.html'
+                },
+
+                'form1@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/form1.html'
+                },
+                'form2@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/form2.html'
+                },
+                'form3@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/form3.html'
+                },
+                'form4@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/form4.html'
+                },
+                'form5@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/form5.html'
+                },
+                'form6@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/form6.html'
+                },
+                'form7@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/form7.html'
+                },
+                'form8@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/form8.html'
+                },
+                'form9@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/form9.html'
+                },
+                'form10@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/form10.html'
+                },
+                'form11@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/form11.html'
+                },
+                'form12@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/form12.html'
+                },
+                'form13@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/form13.html'
+                },
+                'form14@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/form14.html'
+                },
+                'form15@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/form15.html'
+                },
+                'form16@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/form16.html'
+                },
+                'form17@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/form17.html'
+                },
+                'form18@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/form18.html'
+                },
+                'form19@ts-bas-edit' : {
+                    templateUrl: '/web/webjars/ts-bas/webcert/views/utkast/form/form19.html'
+                },
+
                 'footer@ts-bas-edit' : {
                     templateUrl: '/web/webjars/common/webcert/utkast/utkast-footer/utkast-footer.html',
                     controller: 'common.UtkastFooter'
-                },
-                'form1@ts-bas-edit' : {
-                    templateUrl: '/web/webjars/common/webcert/utkast/utkast/form/form1.html'
                 }
+
             }
         });
 });
