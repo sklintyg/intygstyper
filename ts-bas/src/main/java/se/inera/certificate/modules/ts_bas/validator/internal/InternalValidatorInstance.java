bc06e74291df2e0dabff79acdc033d4a80122d3e
Benny Samuelsson <benny.samuelsson@usify.se>
2015-06-04 16:40:21 +0200

    WEBCERT-1872. Extra nullcheckar.

--- ts-bas/src/main/java/se/inera/certificate/modules/ts_bas/validator/internal/InternalValidatorInstance.java
+++ ts-bas/src/main/java/se/inera/certificate/modules/ts_bas/validator/internal/InternalValidatorInstance.java
@@ -3,6 +3,7 @@ package se.inera.certificate.modules.ts_bas.validator.internal;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.BooleanUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -382,7 +383,7 @@ public class InternalValidatorInstance {
             addValidationError("narkotikaLakemedel.teckenMissbruk", ValidationMessageType.EMPTY, "ts-bas.validation.narkotikaLakemedel.teckenmissbruk.missing");
         }
 
-        if (narkotikaLakemedel.getTeckenMissbruk() || narkotikaLakemedel.getForemalForVardinsats()) {
+        if (BooleanUtils.isTrue(narkotikaLakemedel.getTeckenMissbruk()) || BooleanUtils.isTrue(narkotikaLakemedel.getForemalForVardinsats())) {
             if (narkotikaLakemedel.getProvtagningBehovs() == null) {
                 addValidationError("narkotikaLakemedel.provtagningBehovs", ValidationMessageType.EMPTY, "ts-bas.validation.narkotikalakemedel.provtagning-behovs.missing");
             }
