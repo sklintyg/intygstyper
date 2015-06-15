a4351390191b5a27e840412be725aabe0b6b1c63
Erik Lupander <erik.lupander@callistaenterprise.se>
2015-06-15 12:39:37 +0200

    WEBCERT-1314 : Fixed merge conflict that had retained the ManageCertView injection

--- fk7263/src/main/resources/META-INF/resources/webjars/fk7263/webcert/views/intyg/fragasvar/fragasvar.controller.js
+++ fk7263/src/main/resources/META-INF/resources/webjars/fk7263/webcert/views/intyg/fragasvar/fragasvar.controller.js
@@ -1,9 +1,9 @@
 angular.module('fk7263').controller('fk7263.QACtrl',
     [ '$log', '$rootScope', '$stateParams', '$scope', '$timeout', '$window', '$filter', 'common.dialogService',
-        'fk7263.fragaSvarService', 'common.fragaSvarCommonService','common.ManageCertView','common.statService',
+        'fk7263.fragaSvarService', 'common.fragaSvarCommonService', 'common.statService',
         'common.UserModel', 'fk7263.QACtrl.Helper', 'common.IntygViewStateService',
         function($log, $rootScope, $stateParams, $scope, $timeout, $window, $filter, dialogService, fragaSvarService,
-            fragaSvarCommonService, ManageCertView, statService, UserModel, qaHelper, CommonViewState) {
+            fragaSvarCommonService, statService, UserModel, qaHelper, CommonViewState) {
             'use strict';
 
             // init state
