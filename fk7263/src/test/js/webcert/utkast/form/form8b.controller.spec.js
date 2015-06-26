f381f1cad85a02c04078c238108d9376242e2335
Per Beijer <per.beijer@usify.se>
2015-06-26 13:39:34 +0200

    WEBCERT-1935 Uppdaterade 8b Jasminetestet för att ta med hela flödet av datepickern.

--- fk7263/src/test/js/webcert/utkast/form/form8b.controller.spec.js
+++ fk7263/src/test/js/webcert/utkast/form/form8b.controller.spec.js
@@ -7,21 +7,40 @@ describe('fk7263.EditCertCtrl.Form8bCtrl', function() {
     var model;
     var viewState;
 
-    beforeEach(angular.mock.module('common', 'fk7263', 'htmlTemplates', function() {
+    beforeEach(angular.mock.module('common', 'fk7263', 'htmlTemplates', function($provide) {
+        // Decorators that update form input names and interpolates them. Needed for datepicker directives templates dynamic name attributes
+        $provide.decorator('ngModelDirective', function($delegate) {
+            var ngModel = $delegate[0], controller = ngModel.controller;
+            ngModel.controller = ['$scope', '$element', '$attrs', '$injector', function(scope, element, attrs, $injector) {
+                var $interpolate = $injector.get('$interpolate');
+                attrs.$set('name', $interpolate(attrs.name || '')(scope));
+                $injector.invoke(controller, this, {
+                    '$scope': scope,
+                    '$element': element,
+                    '$attrs': attrs
+                });
+            }];
+            return $delegate;
+        });
+        $provide.decorator('formDirective', function($delegate) {
+            var form = $delegate[0], controller = form.controller;
+            form.controller = ['$scope', '$element', '$attrs', '$injector', function(scope, element, attrs, $injector) {
+                var $interpolate = $injector.get('$interpolate');
+                attrs.$set('name', $interpolate(attrs.name || attrs.ngForm || '')(scope));
+                $injector.invoke(controller, this, {
+                    '$scope': scope,
+                    '$element': element,
+                    '$attrs': attrs
+                });
+            }];
+            return $delegate;
+        });
     }));
 
     describe('#Intygdata gets done loading after controller is initialized', function() {
 
         beforeEach(angular.mock.inject([
-            '$compile',
-            '$templateCache',
-            '$controller',
-            '$rootScope',
-            '$http',
-            '$httpBackend',
-            '$log',
-            'fk7263.Domain.IntygModel',
-            'fk7263.EditCertCtrl.ViewStateService',
+            '$compile', '$templateCache', '$controller', '$rootScope', '$http', '$httpBackend', '$log', 'fk7263.Domain.IntygModel', 'fk7263.EditCertCtrl.ViewStateService',
             function( $compile, $templateCache, $controller, _$rootScope_, _$http_, _$httpBackend_, _$log_, _model_, _viewState_) {
                 $rootScope = _$rootScope_;
                 $scope = $rootScope.$new();
@@ -31,11 +50,17 @@ describe('fk7263.EditCertCtrl.Form8bCtrl', function() {
                 viewState = _viewState_;
                 viewState.intygModel = model;
 
-                $scope.form8b = {};
-                $scope.form8b.nedsattMed100from = { $parsers: [] };
-                $scope.form8b.nedsattMed100tom = { $parsers: [] };
-                $controller('fk7263.EditCert.Form8bCtrl' , { $scope: $scope, $log : $log, model : model, viewState : viewState });
+                var viewHtml = $templateCache.get('/web/webjars/fk7263/webcert/views/utkast/form/form8b.html');
+                var element = $compile(viewHtml)($scope);
+                $controller('fk7263.EditCert.Form8bCtrl',  {
+                    $scope: $scope,
+                    $log : $log,
+                    model : model,
+                    viewState : viewState,
+                    $element: element
+                });
                 $scope.$digest();
+
                 viewState.common.doneLoading = true;
                 $scope.$digest();
             }]));
@@ -44,31 +69,19 @@ describe('fk7263.EditCertCtrl.Form8bCtrl', function() {
             // ----- act
             $scope.$apply();
 
-            $scope.form8b.nedsattMed100from.$viewValue = Date(2015,5,26);
-
-            // Simulate datepicker parser
-            angular.forEach($scope.form8b.nedsattMed100from.$parsers, function(k) {
-                $scope.form8b.nedsattMed100from.$modelValue = k($scope.form8b.nedsattMed100from.$viewValue);
-            });
+            $scope.form8b.nedsattMed100from.$setViewValue(Date(2015,5,26));
             $scope.$apply();
 
             // ----- assert
             expect(model.nedsattMed100.from).toBe('2015-06-26');
         });
+
     });
 
     describe('#Intygdata gets done loading before controller is initialized', function() {
 
         beforeEach(angular.mock.inject([
-            '$compile',
-            '$templateCache',
-            '$controller',
-            '$rootScope',
-            '$http',
-            '$httpBackend',
-            '$log',
-            'fk7263.Domain.IntygModel',
-            'fk7263.EditCertCtrl.ViewStateService',
+            '$compile', '$templateCache', '$controller', '$rootScope', '$http', '$httpBackend', '$log', 'fk7263.Domain.IntygModel', 'fk7263.EditCertCtrl.ViewStateService',
             function( $compile, $templateCache, $controller, _$rootScope_, _$http_, _$httpBackend_, _$log_, _model_, _viewState_) {
                 $rootScope = _$rootScope_;
                 $scope = $rootScope.$new();
@@ -78,12 +91,18 @@ describe('fk7263.EditCertCtrl.Form8bCtrl', function() {
                 viewState = _viewState_;
                 viewState.intygModel = model;
 
-                $scope.form8b = {};
-                $scope.form8b.nedsattMed100from = { $parsers: [] };
-                $scope.form8b.nedsattMed100tom = { $parsers: [] };
                 viewState.common.doneLoading = true;
                 $scope.$digest();
-                $controller('fk7263.EditCert.Form8bCtrl' , { $scope: $scope, $log : $log, model : model, viewState : viewState });
+
+                var viewHtml = $templateCache.get('/web/webjars/fk7263/webcert/views/utkast/form/form8b.html');
+                var element = $compile(viewHtml)($scope);
+                $controller('fk7263.EditCert.Form8bCtrl',  {
+                    $scope: $scope,
+                    $log : $log,
+                    model : model,
+                    viewState : viewState,
+                    $element: element
+                });
                 $scope.$digest();
             }]));
 
@@ -91,12 +110,7 @@ describe('fk7263.EditCertCtrl.Form8bCtrl', function() {
             // ----- act
             $scope.$apply();
 
-            $scope.form8b.nedsattMed100from.$viewValue = Date(2015,5,26);
-
-            // Simulate datepicker parser
-            angular.forEach($scope.form8b.nedsattMed100from.$parsers, function(k) {
-                $scope.form8b.nedsattMed100from.$modelValue = k($scope.form8b.nedsattMed100from.$viewValue);
-            });
+            $scope.form8b.nedsattMed100from.$setViewValue(Date(2015,5,26));
             $scope.$apply();
 
             // ----- assert
