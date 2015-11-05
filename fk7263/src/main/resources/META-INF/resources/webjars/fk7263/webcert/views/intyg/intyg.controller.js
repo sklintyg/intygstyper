64a3f3f649362284161897497260f5be253beedf
Per Beijer <per.beijer@usify.se>
2015-11-05 10:18:55 +0100

    WEBCERT-2106: Uppgraderat till angularjs 1.4.7

--- fk7263/src/main/resources/META-INF/resources/webjars/fk7263/webcert/views/intyg/intyg.controller.js
+++ fk7263/src/main/resources/META-INF/resources/webjars/fk7263/webcert/views/intyg/intyg.controller.js
@@ -1,7 +1,7 @@
 angular.module('fk7263').controller('fk7263.ViewCertCtrl',
-    [ '$log', '$rootScope', '$stateParams', '$scope', '$cookieStore','common.IntygService','common.IntygProxy',
+    [ '$log', '$rootScope', '$stateParams', '$scope', 'common.IntygService','common.IntygProxy',
         'common.messageService', 'common.UserModel', 'fk7263.IntygController.ViewStateService',
-        function($log, $rootScope, $stateParams, $scope, $cookieStore, IntygService, IntygProxy,
+        function($log, $rootScope, $stateParams, $scope, IntygService, IntygProxy,
             messageService, UserModel, ViewState) {
             'use strict';
 
