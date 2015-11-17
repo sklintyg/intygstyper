bb3c3b53e7ba669473326aeacb82d95f7c02eca2
Rasmus Lundberg-Åblad <rasmus.ablad@usify.se>
2015-11-17 10:00:24 +0100

    [SITANP] Fixed duplication of changes text values from nedsättningar array.

--- sjukersattning/src/main/resources/META-INF/resources/webjars/sjukersattning/webcert/views/utkast/form/form5.controller.js
+++ sjukersattning/src/main/resources/META-INF/resources/webjars/sjukersattning/webcert/views/utkast/form/form5.controller.js
@@ -1,16 +1,29 @@
 angular.module('sjukersattning').controller('sjukersattning.EditCert.Form5Ctrl',
-    ['$scope', '$log', 'sjukersattning.EditCertCtrl.ViewStateService',
-        function($scope, $log, viewState) {
+    ['$scope', '$log', 'sjukersattning.EditCertCtrl.ViewStateService', '$filter',
+        function($scope, $log, viewState, $filter) {
             'use strict';
             var model = viewState.intygModel;
             $scope.model = model;
             $scope.viewState = viewState;
 
+            $scope.selectedNedsattningar = [
+                { 'id': 1, selected: false, text : null } ,
+                { 'id': 2, selected: false, text : null },
+                { 'id': 3, selected: false, text : null },
+                { 'id': 4, selected: false, text : null },
+                { 'id': 5, selected: false, text : null },
+                { 'id': 6, selected: false, text : null },
+                { 'id': 7, selected: false, text : null },
+                { 'id': 8, selected: false, text : null }
+            ];
+
             function onPageLoad(){
 
                // alert('funktionsnedsattningar length' + model.funktionsnedsattningar.length);
                 if(model.funktionsnedsattningar.length < 1){ // do nothing let viewState options list dictate data
+                    clearViewModel();
                     $scope.funktionsnedsattningOptions = viewState.funktionsnedsattningOptions;
+
                 } else { // set data first, then update from backend on relevant data
                     $scope.funktionsnedsattningOptions = viewState.funktionsnedsattningOptions; // 1. load
                     for(var i = 0; i < model.funktionsnedsattningar.length; i++) { // 2. update
@@ -22,54 +35,65 @@ angular.module('sjukersattning').controller('sjukersattning.EditCert.Form5Ctrl',
                 }
             }
 
-           /* $scope.$watch('viewState.common.doneLoading', function(newVal, oldVal) {
-                if (newVal === oldVal) {
-                    return;
-                }
-
-                // only do this once the page is loaded and changes come from the gui!
-                if (viewState.common.doneLoading) {
-                    // Remove defaults not applicable when smittskydd is active
-                    if (newVal === true) {
-                        model.updateToAttic(model.properties.form5);
-                        model.clear(model.properties.form5);
-                    } else {
-                        model.restoreFromAttic(model.properties.form5);
-                    }
-                }
-            }); */
-
-            $scope.setFocus = function(id, state){
+            $scope.setFocus = function(id, state ,text){
                 var element = window.document.getElementById(id);
                 if(element) {
                     //var initialClasses = element.className;
+                    var toUpdate = $scope.selectedNedsattningar[id];
                     if (!state) {
                         element.blur();
+
+                        if (toUpdate) {
+                            toUpdate.selected = false;
+                            toUpdate.text = text;
+                            // set attic?
+                        }
+
                     } else{
                         element.focus();
-                        // set model here
-
-
-
-                        //model.funktionsnedsattning.push
+                        toUpdate.selected= true;
+                        toUpdate.text = text;
+                        // restore attic?
                     }
-
-
                 }
             };
 
-            $scope.setFunktionsNedsattningar = function(id, text){
+            $scope.updateViewModelNedsattningar = function(id, text) {
+                var toUpdate = $scope.selectedNedsattningar[id];
+                if (!toUpdate) {
+                   return;
+                } else{
+                    toUpdate.text = text;
 
-                    var match = model.funktionsnedsattningar[id];
+                        setFunktionsNedsattningar(id,text);
 
-                    if( model.funktionsnedsattningar[id] === undefined ) {
-                         var obj = { 'funktionsomrade': id, 'beskrivning' : text };
+
+                }
+            }
+
+            function setFunktionsNedsattningar(funktionsomrade, beskrivning){
+                    //var match = model.funktionsnedsattningar[id];
+                    var match = $filter('filter')(model.funktionsnedsattningar, {funktionsomrade:funktionsomrade})[0];
+                    console.log('match', match);
+
+                    if( !match ) {
+                         var obj = { 'funktionsomrade': funktionsomrade, 'beskrivning' : beskrivning };
                          model.funktionsnedsattningar.push(obj);
-                    } else if (match) {
-                         match.funktionsomrade = id;
-                         match.beskrivning = text;
+                        console.log('model.funktionsnedsattningar new*: ' + JSON.stringify(model.funktionsnedsattningar));
+                    } else {
+                         match.funktionsomrade = funktionsomrade;
+                         match.beskrivning = beskrivning;
+                        console.log('model.funktionsnedsattningar old*: ' + JSON.stringify(model.funktionsnedsattningar));
                     }
-            };
+                    //console.log('model.funktionsnedsattningar: ' + JSON.stringify(model.funktionsnedsattningar));
+            }
+
+            function clearViewModel(){
+                for(var i = 0; i < viewState.funktionsnedsattningOptions.length; i++){
+                    viewState.funktionsnedsattningOptions[i].text = '';
+                    viewState.funktionsnedsattningOptions[i].selected = false;
+                }
+            }
 
             onPageLoad();
         }]);
