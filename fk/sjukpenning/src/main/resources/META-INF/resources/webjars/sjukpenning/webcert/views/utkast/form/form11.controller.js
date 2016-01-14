24bc97f4b4982e46258ef15f74e0607e72365ec0
Rasmus Lundberg-Åblad <rasmus.ablad@usify.se>
2016-01-14 11:15:55 +0100

    SIT-23: Clean-up, Removed Sjukpenning as part of clean up.

--- fk/sjukpenning/src/main/resources/META-INF/resources/webjars/sjukpenning/webcert/views/utkast/form/form11.controller.js
+++
@@ -1,9 +0,0 @@
-angular.module('sjukpenning').controller('sjukpenning.EditCert.Form11Ctrl',
-    ['$scope', '$log', 'sjukpenning.EditCertCtrl.ViewStateService',
-        function($scope, $log, viewState) {
-            'use strict';
-            var model;
-            model = viewState.intygModel;
-            $scope.model = model;
-            $scope.viewState = viewState;
-        }]);
