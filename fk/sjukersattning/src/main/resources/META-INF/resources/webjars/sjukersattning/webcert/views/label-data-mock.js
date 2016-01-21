12040c0f4c571e747be44278f2be58e08cca1419
Benny Samuelsson <benny.samuelsson@usify.se>
2016-01-21 09:03:01 +0100

    SIT-30: Uppdaterad labelfil med alla IDn

--- fk/sjukersattning/src/main/resources/META-INF/resources/webjars/sjukersattning/webcert/views/label-data-mock.js
+++ fk/sjukersattning/src/main/resources/META-INF/resources/webjars/sjukersattning/webcert/views/label-data-mock.js
@@ -5,78 +5,96 @@ var sjukersattningDynamicLabelsMock = {
         'text': [
             // KAT 1
             {
-                'id': 'KAT_1.RBK', 'text': 'Grund för medicinskt underlag'
+                'id': 'KAT_1.RBK',
+                'text': 'Grund för medicinskt underlag'
             },
             {
-                'id': 'KAT_1.HLP', 'text': 'Hjälp Grund för medicinskt underlag'
+                'id': 'KAT_1.HLP',
+                'text': 'Hjälp Grund för medicinskt underlag'
             },
             {
-                'id': 'FRG_1.RBK', 'text': 'I relation till vilken sysselsättning gör du den medicinska bedömningen?'
+                'id': 'FRG_1.RBK',
+                'text': 'Grund för medicinskt underlag (MU)'
             },
             {
-                'id': 'FRG_1.HLP', 'text': 'Om du kryssar i flera val är det viktigt att du tydliggör under fältet Övrigt om sjukskrivningens omfattning eller period skiljer sig åt mellan de två sysselsättningarna.'
+                'id': 'FRG_1.HLP',
+                'text': 'Om du kryssar i flera val är det viktigt att du tydliggör under fältet Övrigt om sjukskrivningens omfattning eller period skiljer sig åt mellan de två sysselsättningarna.'
             },
             {
-                'id': 'DFR_1.1.RBK', text: 'Typ av grund för MU'
+                'id': 'DFR_1.1.RBK',
+                'text': 'Typ av grund för MU'
             },
             {
-                'id': 'DFR_1.1.HLP', text: 'DFR_1.1.HLP'
+                'id': 'DFR_1.1.HLP',
+                'text': 'DFR_1.1.HLP'
             },
             {
-                'id': 'DFR_1.2.RBK', 'text': 'Ange yrke och nuvarande arbetsuppgifter'
+                'id': 'DFR_1.2.RBK',
+                'text': 'Datum som grund för MU'
             },
             {
-                'id': 'DFR_1.2.HLP', text: 'DFR_1.2.HLP'
+                'id': 'DFR_1.2.HLP',
+                'text': 'DFR_1.2.HLP'
             },
             {
-                'id': 'DFR_1.3.RBK', 'text': 'Vilken annan grund finns för MU'
+                'id': 'DFR_1.3.RBK',
+                'text': 'Vilken annan grund finns för MU'
             },
             {
-                'id': 'DFR_1.3.HLP', text: 'DFR_1.3.HLP'
+                'id': 'DFR_1.3.HLP',
+                'text': 'DFR_1.3.HLP'
             },
             {
-                'id': 'DFR_2.1.RBK', text: 'DFR_2.1.RBK'
+                'id': 'DFR_2.1.RBK',
+                'text': 'Datum för kännedom om patienten'
             },
             {
-                'id': 'DFR_2.1.HLP', text: 'DFR_2.1.HLP'
+                'id': 'DFR_2.1.HLP',
+                'text': 'DFR_2.1.HLP'
             },
 
             // KAT 2
             {
                 'id': 'KAT_2.RBK',
-                'text': '2. Intyg är baserat på'
+                'text': 'Andra medicinska utredningar och underlag'
             },
             {
                 'id': 'KAT_2.HLP',
                 'text': '<p>Om underlaget är baserat utifrån annan anledning, ange det under Övrigt, se kategori 8.</p>'
             },
             {
-                'id': 'DFR_3.1.RBK', 'text': 'Diagnoskod ICD-10-SE'
+                'id': 'DFR_3.1.RBK',
+                'text': 'Finns det andra medicinska utredningar eller underlag'
             },
             {
-                'id': 'DFR_4.1.RBK', 'text': 'Diagnoskod ICD-10-SE'
+                'id': 'DFR_4.1.RBK',
+                'text': 'Utrednings- eller underlagstyp?'
             },
             {
-                'id': 'DFR_4.2.RBK', 'text': 'Diagnos'
+                'id': 'DFR_4.2.RBK',
+                'text': 'Datum för utredning eller underlag'
             },
             {
-                'id': 'DFR_4.3.RBK', 'text': 'Diagnos'
+                'id': 'DFR_4.3.RBK',
+                'text': 'Var utredningen kan hämtas in'
             },
             {
-                'id': 'DFR_4.3.HLP', text: 'DFR_4.3.HLP'
+                'id': 'DFR_4.3.HLP',
+                'text': 'DFR_4.3.HLP'
             },
 
             // KAT 3
             {
                 'id': 'KAT_3.RBK',
-                'text': '3. Sjukdom som orsakar nedsatt arbetsförmåga'
+                'text': 'Sjukdomsförlopp'
             },
             {
                 'id': 'KAT_3.HLP',
                 'text': 'Ange samtliga diagnoser som orsakar nedsatt arbetsförmåga. Huvuddiagnosen för arbetsförmågans nedsättning anges först. Symptomdiagnoser kan godkännas en kortare period under medicinskt utredning'
             },
             {
-                'id': 'DFR_5.1.RBK'
+                'id': 'DFR_5.1.RBK',
+                'text': 'Beskrivning av sjukdomsförlopp'
             },
             {
                 'id': 'DFR_5.1.HLP'
@@ -84,34 +102,43 @@ var sjukersattningDynamicLabelsMock = {
 
             // KAT 4
             {
-                'id': 'KAT_4.RBK', 'text': '4. Sjukdomens konsekvens för patienten'
+                'id': 'KAT_4.RBK',
+                'text': 'Diagnos'
             },
             {
                 'id': 'KAT_4.HLP'
             },
             {
-                'id': 'FRG_6.RBK', 'text': 'Din bedömning av patientens behov av sjukskrivning utifrån aktuell sysselsättning'
-            },
-            { 'id': 'FRG_6.HLP' },
-            {
-                'id': 'DFR_6.1.RBK', 'text': 'Välj sjukskrivningsgrad (nedsättning av arbetsförmåga)'
-            },
-            { 'id': 'DFR_6.1.HLP' },
-            {
-                'id': 'DFR_6.2.RBK', 'text': 'Period'
-            },
-            { 'id': 'DFR_6.2.HLP' },
-            {
-                'id': 'KV_FKMU_0003.1', 'text': 'Helt nedsatt'
+                'id': 'FRG_6.RBK',
+                'text': 'Din bedömning av patientens behov av sjukskrivning utifrån aktuell sysselsättning'
             },
             {
-                'id': 'DFR_7.1.RBK'
+                'id': 'FRG_6.HLP'
+            },
+            {
+                'id': 'DFR_6.1.RBK',
+                'text': 'Diagnostext'
+            },
+            {
+                'id': 'DFR_6.1.HLP'
+            },
+            {
+                'id': 'DFR_6.2.RBK',
+                'text': 'Diagnoskod ICD-10'
+            },
+            {
+                'id': 'DFR_6.2.HLP'
+            },
+            {
+                'id': 'DFR_7.1.RBK',
+                'text': 'Diagnosgrund'
             },
             {
                 'id': 'DFR_7.1.HLP'
             },
             {
-                'id': 'DFR_7.2.RBK'
+                'id': 'DFR_7.2.RBK',
+                'text': 'Skäl för ny bedömning av tidigare diagnosgrund?'
             },
             {
                 'id': 'DFR_7.2.HLP'
@@ -119,74 +146,67 @@ var sjukersattningDynamicLabelsMock = {
 
             // KAT 5
             {
-                'id': 'KAT_5.RBK', 'text': '5. Medicinskt behandling'
+                'id': 'KAT_5.RBK',
+                'text': 'Funktionsnedsättning'
             },
             {
                 'id': 'KAT_5.HLP'
             },
             {
-                'id': 'DFR_8.1.RBK'
+                'id': 'DFR_8.1.RBK',
+                'text': 'Beskriv intellektuell funktion'
             },
             {
                 'id': 'DFR_8.1.HLP'
             },
             {
-                'id': 'DFR_9.1.RBK'
+                'id': 'DFR_9.1.RBK',
+                'text': 'Beskriv kommunikation och social interaktion'
             },
             {
                 'id': 'DFR_9.1.HLP'
             },
             {
-                'id': 'DFR_10.1.RBK'
+                'id': 'DFR_10.1.RBK',
+                'text': 'Beskriv uppmärksamhet och koncentration'
             },
             {
                 'id': 'DFR_10.2.HLP'
             },
             {
-                'id': 'FRG_11.RBK'
+                'id': 'DFR_11.1.RBK',
+                'text': 'Beskriv annan psykisk funktion'
             },
             {
-                'id': 'FRG_11.HLP'
+                'id': 'DFR_11.1.HLP',
+                'text': 'Av läkaren observerade nedsättningar (direkt och indirekt) och avvikelse i status, rtg-fynd, lab, kliniskt fysiologiska undersökningar och ev. testresultat. Vid beskrivningen av funktionsnedsättningen vid psykisk sjukdom ingår förutom basalt psykiskt status även den värderade del av anamnes läkaren bedömer vara av sjukligt tillstånd.'
             },
             {
-                'id': 'DFR_11.1.RBK', 'text': 'Undersökningsfynd som är en konsekvens av sjukdomen. (funktionsnedsättning)'
-            },
-            {
-                'id': 'DFR_11.1.HLP', 'text': 'Av läkaren observerade nedsättningar (direkt och indirekt) och avvikelse i status, rtg-fynd, lab, kliniskt fysiologiska undersökningar och ev. testresultat. Vid beskrivningen av funktionsnedsättningen vid psykisk sjukdom ingår förutom basalt psykiskt status även den värderade del av anamnes läkaren bedömer vara av sjukligt tillstånd.'
-            },
-            {
-                'id': 'DFR_12.1.RBK', 'text': 'Pågående medicinska behandlingar/åtgärder och ansvarig vårdenhet.'
+                'id': 'DFR_12.1.RBK',
+                'text': 'Beskriv syn, hörsel och tal'
             },
             {
                 'id': 'DFR_12.1.HLP'
             },
             {
-                'id': 'DFR_13.1.RBK', 'text': 'Pågående medicinska behandlingar/åtgärder och ansvarig vårdenhet.'
+                'id': 'DFR_13.1.RBK',
+                'text': 'Beskriv balans/koordination, motorik och smärta'
             },
             {
                 'id': 'DFR_13.1.HLP'
             },
             {
-                'id': 'FRG_14.RBK',
-                'text': 'Min rekommendation av sjukskrivningsens längd överskrider Socialstyrelsens försäkringsmedicinska beslutsstöd.'
-            },
-            {
-                'id': 'DFR_14.1.RBK'
+                'id': 'DFR_14.1.RBK',
+                'text': 'Beskriv annan kroppslig funktion'
             },
             {
                 'id': 'DFR_14.1.HLP'
-            },
-            {
-                'id': 'DFR_14.2.RBK', 'text': 'Beskriv varför arbetsförmågan bedöms vara nedsatt längre tid än den som det försäkringsmedicinska beslutsstödet anger'
-            },
-            {
-                'id': 'DFR_14.2.HLP'
             },
 
             // KAT 6
             {
                 'id': 'KAT_6.RBK',
-                'text': '6. Bedömning'
+                'text': 'Aktivitetsbegränsning'
             },
             {
                 'id': 'KAT_6.HLP',
@@ -194,7 +214,7 @@ var sjukersattningDynamicLabelsMock = {
             },
             {
                 'id': 'DFR_17.1.RBK',
-                'text': 'Uppskattning av prognos för när patienten bedöms kunna återgå till aktuell sysselsättning helt eller delvis'
+                'text': 'Beskrivning av aktivitetsbegränsningar'
             },
             {
                 'id': 'DFR_17.1.HLP',
@@ -204,34 +224,35 @@ var sjukersattningDynamicLabelsMock = {
             // KAT 7
             {
                 'id': 'KAT_7.RBK',
-                'text': '7. Förslag till åtgärder som skulle kunna underlätta återgång till arbete'
+                'text': 'Medicinska behandlingar/åtgärder'
             },
             {
                 'id': 'KAT_7.HLP'
             },
             {
                 'id': 'DFR_18.1.RBK',
-                'text': 'DFR_18.1.RBK'
+                'text': 'Typ av avslutade med. behandlingar, period och resultat'
             },
             {
                 'id': 'DFR_18.1.HLP'
             },
             {
                 'id': 'DFR_19.1.RBK',
-                'text': 'DFR_19.1.RBK'
+                'text': 'Typ av pågående behandlingar och ansvarig vårdenhet'
             },
             {
                 'id': 'DFR_19.1.HLP'
             },
             {
                 'id': 'DFR_20.1.RBK',
-                'text': 'DFR_20.1.RBK'
+                'text': 'Typ av planerade behandlingar och ansvarig vårdenhet'
             },
             {
                 'id': 'DFR_20.1.HLP'
             },
             {
-                'id': 'DFR_21.1.RBK'
+                'id': 'DFR_21.1.RBK',
+                'text': 'Typ av substansintag'
             },
             {
                 'id': 'DFR_21.1.HLP'
@@ -240,19 +261,21 @@ var sjukersattningDynamicLabelsMock = {
             // KAT 8
             {
                 'id': 'KAT_8.RBK',
-                'text': '8. Övrigt'
+                'text': 'Medicinska förutsättningar för arbete'
             },
             {
                 'id': 'KAT_8.HLP'
             },
             {
-                'id': 'DFR_22.1.RBK'
+                'id': 'DFR_22.1.RBK',
+                'text': 'Beskriv patientens medicinska förutsättningar för arbete'
             },
             {
                 'id': 'DFR_22.1.HLP'
             },
             {
-                'id': 'DFR_23.1.RBK'
+                'id': 'DFR_23.1.RBK',
+                'text': 'Beskrivning förmågor trots begränsningar'
             },
             {
                 'id': 'DFR_23.1.HLP'
@@ -261,13 +284,14 @@ var sjukersattningDynamicLabelsMock = {
             // KAT 9
             {
                 'id': 'KAT_9.RBK',
-                'text': '9. Tilläggsfrågor'
+                'text': 'Övrigt'
             },
             {
                 'id': 'KAT_9.HLP'
             },
             {
-                'id': 'DFR_25.1.RBK'
+                'id': 'DFR_25.1.RBK',
+                'text': 'Typ av övriga upplysningar'
             },
             {
                 'id': 'DFR_25.1.HLP'
@@ -276,68 +300,157 @@ var sjukersattningDynamicLabelsMock = {
             // KAT 10
             {
                 'id': 'KAT_10.RBK',
-                'text': ''
+                'text': 'Kontakt'
             },
             {
                 'id': 'KAT_10.HLP'
             },
             {
-                'id': 'DFR_26.1.RBK'
+                'id': 'DFR_26.1.RBK',
+                'text': 'Kontakt önskas'
             },
             {
                 'id': 'DFR_26.1.HLP'
             },
             {
-                'id': 'DFR_26.2.RBK'
+                'id': 'DFR_26.2.RBK',
+                'text': 'Motivering av kontakt önskas'
             },
             {
                 'id': 'DFR_26.2.HLP'
             },
 
+            // KV_FKMU_0001
             {
-                'id': 'KV_FKMU_0001.1',
+                'id': 'KV_FKMU_0001.1.RBK',
                 'text': 'Min undersökning av patienten'
             },
             {
-                'id': 'KV_FKMU_0001.2',
+                'id': 'KV_FKMU_0001.1.HLP',
+                'text': 'Min undersökning av patienten'
+            },
+/*
+            {
+                'id': 'KV_FKMU_0001.2.RBK',
                 'text': 'Min telefonkontakt med patienten'
             },
             {
-                'id': 'KV_FKMU_0001.3',
+                'id': 'KV_FKMU_0001.2.HLP',
+                'text': 'Min telefonkontakt med patienten'
+            },
+*/
+            {
+                'id': 'KV_FKMU_0001.3.RBK',
                 'text': 'Journaluppgifter från den'
             },
             {
-                'id': 'KV_FKMU_0002.1',
+                'id': 'KV_FKMU_0001.3.HLP',
+                'text': 'Journaluppgifter från den'
+            },
+
+            {
+                'id': 'KV_FKMU_0001.4.RBK',
+                'text': 'Anhörigs beskrivning av patienten'
+            },
+            {
+                'id': 'KV_FKMU_0001.4.HLP',
+                'text': 'Anhörigs beskrivning av patienten'
+            },
+
+            {
+                'id': 'KV_FKMU_0001.5.RBK',
+                'text': 'Annat'
+            },
+            {
+                'id': 'KV_FKMU_0001.5.HLP',
+                'text': 'Annat'
+            },
+
+            {
+                'id': 'KV_FKMU_0005.1.RBK',
+                'text': 'Neuropsykiatriskt utlåtande'
+            },
+            {
+                'id': 'KV_FKMU_0005.2.RBK',
+                'text': 'Underlag från habiliteringen'
+            },
+            {
+                'id': 'KV_FKMU_0005.3.RBK',
+                'text': 'Underlag från arbetsterapeut'
+            },
+            {
+                'id': 'KV_FKMU_0005.4.RBK',
+                'text': 'Underlag från fysioterapeut'
+            },
+            {
+                'id': 'KV_FKMU_0005.5.RBK',
+                'text': 'Underlag från logoped'
+            },
+            {
+                'id': 'KV_FKMU_0005.6.RBK',
+                'text': 'Underlag från psykolog'
+            },
+            {
+                'id': 'KV_FKMU_0005.7.RBK',
+                'text': 'Underlag från företagshälsovård'
+            },
+/*            {
+                'id': 'KV_FKMU_0004.8.RBK',
+                'text': 'Underlag från skolhälsovård'
+            },*/
+            {
+                'id': 'KV_FKMU_0005.9.RBK',
+                'text': 'Utredning av annan specialistklinik'
+            },
+            {
+                'id': 'KV_FKMU_0005.10.RBK',
+                'text': 'Utredning från vårdinrättning utomlands'
+            },
+            {
+                'id': 'KV_FKMU_0005.11.RBK',
+                'text': 'Övrigt'
+            },
+
+            // KV_FKMU_0002
+/*            {
+                'id': 'KV_FKMU_0002.1.RBK',
                 'text': 'Nuvarande arbete'
             },
             {
-                'id': 'KV_FKMU_0002.2',
+                'id': 'KV_FKMU_0002.2.RBK',
                 'text': 'Arbetssökande'
             },
             {
-                'id': 'KV_FKMU_0002.3',
+                'id': 'KV_FKMU_0002.3.RBK',
                 'text': 'Föräldraledighet för vård av barn'
             },
             {
-                'id': 'KV_FKMU_0002.4',
+                'id': 'KV_FKMU_0002.4.RBK',
                 'text': 'Studier'
             },
             {
-                'id': 'KV_FKMU_0002.5',
+                'id': 'KV_FKMU_0002.5.RBK',
                 'text': 'Deltar i arbetsmarknadspolitiskt program'
             },
+
+            // KV_FKMU_0003
             {
-                'id': 'KV_FKMU_0003.2',
+                'id': 'KV_FKMU_0003.1.RBK',
+                'text': 'Helt nedsatt'
+            },
+            {
+                'id': 'KV_FKMU_0003.2.RBK',
                 'text': 'Nedsatt med 3/4'
             },
             {
-                'id': 'KV_FKMU_0003.3',
+                'id': 'KV_FKMU_0003.3.RBK',
                 'text': 'Nedsatt med hälften'
             },
             {
-                'id': 'KV_FKMU_0003.4',
+                'id': 'KV_FKMU_0003.4.RBK',
                 'text': 'Nedsatt med 1/4'
             },
+
             {
                 'id': 'KV_FKMU_0004.1',
                 'text': 'Inte aktuellt'
@@ -382,6 +495,8 @@ var sjukersattningDynamicLabelsMock = {
                 'id': 'KV_FKMU_0004.11',
                 'text': 'Övrigt'
             },
+
+ */
             // KAT 9999 tilläggsfrågor
             {
                 'id': 'KAT_9999.RBK',
