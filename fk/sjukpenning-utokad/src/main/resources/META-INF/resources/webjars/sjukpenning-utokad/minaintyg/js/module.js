f961eed5eac4c081e73c5ca812ce540ac11a9bc3
Per Beijer <per.beijer@usify.se>
2016-02-22 12:32:28 +0100

    INTYG-1982: Visning av sjukpenning-utokad i MI

--- fk/sjukpenning-utokad/src/main/resources/META-INF/resources/webjars/sjukpenning-utokad/minaintyg/js/module.js
+++ fk/sjukpenning-utokad/src/main/resources/META-INF/resources/webjars/sjukpenning-utokad/minaintyg/js/module.js
@@ -6,43 +6,43 @@ angular.module('lisu').config(function($stateProvider) {
 
     $stateProvider.
         state('sjukpenning-utokad-view', {
-            url :'/sjukpenning-utokad/view/:certificateId',
+            url :'/lisu/view/:certificateId',
             templateUrl: '/web/webjars/sjukpenning-utokad/minaintyg/views/view-cert.html',
             controller: 'sjukpenning-utokad.ViewCertCtrl',
             data : { title: 'Läkarintyg sjukersättning', keepInboxTabActive: true }
         }).
         state('sjukpenning-utokad-recipients', {
-            url : '/sjukpenning-utokad/recipients',
+            url : '/lisu/recipients',
             templateUrl: '/web/webjars/sjukpenning-utokad/minaintyg/views/recipients.html',
             controller: 'common.SendCertWizardCtrl',
             data : { title: 'Skicka intyg till mottagare' }
         }).
         state('sjukpenning-utokad-statushistory', {
-            url : '/sjukpenning-utokad/statushistory',
+            url : '/lisu/statushistory',
             templateUrl: '/web/webjars/sjukpenning-utokad/minaintyg/views/status-history.html',
             controller: 'sjukpenning-utokad.ViewCertCtrl',
             data : { title: 'Alla intygets händelser' }
         }).
         state('sjukpenning-utokad-summary', {
-            url : '/sjukpenning-utokad/summary',
+            url : '/lisu/summary',
             templateUrl: '/web/webjars/sjukpenning-utokad/minaintyg/views/send-summary.html',
             controller: 'common.SendCertWizardCtrl',
             data : { title: 'Kontrollera och skicka intyget' }
         }).
         state('sjukpenning-utokad-sent', {
-            url : '/sjukpenning-utokad/sent',
+            url : '/lisu/sent',
             templateUrl: '/web/webjars/sjukpenning-utokad/minaintyg/views/sent-cert.html',
             controller: 'common.SendCertWizardCtrl',
             data : { title: 'Intyget har skickats' }
         }).
         state('sjukpenning-utokad-fel', {
-            url : '/sjukpenning-utokad/fel/:errorCode',
+            url : '/lisu/fel/:errorCode',
             templateUrl: '/web/webjars/sjukpenning-utokad/minaintyg/views/error.html',
             controller: 'sjukpenning-utokad.ErrorCtrl',
             data : { title: 'Fel' }
         }).
         state('sjukpenning-utokad-visafel', {
-            url :'/sjukpenning-utokad/visafel/:errorCode',
+            url :'/lisu/visafel/:errorCode',
             templateUrl: '/web/webjars/sjukpenning-utokad/minaintyg/views/error.html',
             controller: 'sjukpenning-utokad.ErrorCtrl',
             data : { title: 'Fel',
@@ -55,5 +55,5 @@ angular.module('lisu').run(['common.messageService',
     function(messageService) {
         'use strict';
 
-        messageService.addResources(sjukersattningMessages);
+        messageService.addResources(sjukpenningUtokadMessages);
     }]);
