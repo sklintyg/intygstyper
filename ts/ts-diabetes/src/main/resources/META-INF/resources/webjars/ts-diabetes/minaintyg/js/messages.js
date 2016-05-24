db864cc4e7973b6fa2bfe377ad92248369722a51
Per Beijer <per.beijer@usify.se>
2016-05-24 07:01:16 +0200

    INTYG-2495: Dynamiska texter för TS-diabetes

--- ts/ts-diabetes/src/main/resources/META-INF/resources/webjars/ts-diabetes/minaintyg/js/messages.js
+++ ts/ts-diabetes/src/main/resources/META-INF/resources/webjars/ts-diabetes/minaintyg/js/messages.js
@@ -29,40 +29,7 @@ var tsDiabetesMessages = {
         'ts-diabetes.label.ovrigkommentar': 'Övriga upplysningar och kommentarer',
         'ts-diabetes.label.unit': 'Vårdenhet',
 
-        'ts-diabetes.label.korkort.a': 'A',
-        'ts-diabetes.label.korkort.am': 'AM',
-        'ts-diabetes.label.korkort.a1': 'A1',
-        'ts-diabetes.label.korkort.a2': 'A2',
-        'ts-diabetes.label.korkort.c1': 'C1',
-        'ts-diabetes.label.korkort.b': 'B',
-        'ts-diabetes.label.korkort.be': 'BE',
-        'ts-diabetes.label.korkort.c1e': 'C1E',
-        'ts-diabetes.label.korkort.c': 'C',
-        'ts-diabetes.label.korkort.ce': 'CE',
-        'ts-diabetes.label.korkort.d1': 'D1',
-        'ts-diabetes.label.korkort.d1e': 'D1E',
-        'ts-diabetes.label.korkort.d': 'D',
-        'ts-diabetes.label.korkort.de': 'DE',
-        'ts-diabetes.label.korkort.taxi': 'Taxi',
-        'ts-diabetes.label.korkort.traktor': 'Traktor',
-        'ts-diabetes.label.korkort.annat': 'Annat',
-
-        // Identitet styrkt genom
-        'ts-diabetes.label.identitet.id_kort': 'ID-kort',
-        'ts-diabetes.label.identitet.foretag_eller_tjanstekort': 'Företagskort eller tjänstekort.',
-        'ts-diabetes.label.identitet.korkort': 'Svenskt körkort',
-        'ts-diabetes.label.identitet.pers_kannedom': 'Personlig kännedom',
-        'ts-diabetes.label.identitet.forsakran_kap18': 'Försäkran enligt 18 kap 4 §',
-        'ts-diabetes.label.identitet.pass': 'Pass',
-
         'ts-diabetes.label.syn.binokulart': 'Binokulärt',
-        'ts-diabetes.label.syn.diplopi': 'd) Förekommer dubbelseende? ',
-        'ts-diabetes.label.syn.progressivogonsjukdom': ' c) Har patienten någon progressiv ögonsjukdom?',
-
-        'ts-diabetes.label.syn.separat-ogonlakarintyg': 'a) Ögonläkarintyg kommer att skickas in separat',
-        'ts-diabetes.label.syn.synfaltsprovning-utan-anmarkning': 'b) Är synfältsprövning enligt Donders konfrontationsmetod utan anmärkning? ',
-        'ts-diabetes.label.syn.synskarpa': 'c) Värden för synskärpa',
-
         'ts-diabetes.label.syn.hogeroga': 'Höger öga',
         'ts-diabetes.label.syn.vansteroga': 'Vänster öga',
         'ts-diabetes.label.syn.utankorrektion': 'Utan korrektion',
@@ -70,31 +37,8 @@ var tsDiabetesMessages = {
         'ts-diabetes.label.syn.kontaktlins': 'Kontaktlinser',
         'ts-diabetes.label.syn.korrektionsglasens-styrka': 'Korrektionsglasens styrka',
 
-        'ts-diabetes.label.diabetes.diabetestyp': 'Diabetestyp',
-        'ts-diabetes.label.diabetes.diagnostid': 'Diagnosen diabetes ställdes år',
         'ts-diabetes.label.diabetes.diabetestyp.diabetes_typ_1': 'Typ 1',
         'ts-diabetes.label.diabetes.diabetestyp.diabetes_typ_2': 'Typ 2',
-        'ts-diabetes.label.diabetes.behandling': 'Behandling',
-        'ts-diabetes.label.diabetes.behandling.endast-kost': 'Endast kost',
-        'ts-diabetes.label.diabetes.behandling.tabletter': 'Tabletter',
-        'ts-diabetes.label.diabetes.behandling.insulin': 'Insulin',
-        'ts-diabetes.label.diabetes.behandling.insulin-period': 'Insulin sedan år',
-        'ts-diabetes.label.diabetes.behandling.annan': 'Annan behandling',
-
-        'ts-diabetes.label.hypoglykemier.har-patient-kannedom': 'a) Har patienten kunskap om lämpliga åtgärder vid hypoglykemi?',
-        'ts-diabetes.label.hypoglykemier.tecken-nedsatt-hjarnfunktion': 'b) Förekommer hypoglykemier med tecken på nedsatt hjärnfunktion (neuroglukopena symtom) som bedöms kunna innebära en trafiksäkerhetsrisk?',
-        'ts-diabetes.label.hypoglykemier.saknar-formaga-kanna-varningstecken': 'c) Saknar patienten förmåga att känna varningstecken på hypoglykemi',
-
-        'ts-diabetes.label.hypoglykemier.allvarlig-forekomst': 'd) Har patienten haft allvarlig hypoglykemi (som krävt hjälp av annan för att hävas) under det senaste året?',
-        'ts-diabetes.label.hypoglykemier.allvarlig-forekomst.beskrivning': 'Hur många sådana episoder? ',
-
-        'ts-diabetes.label.hypoglykemier.allvarlig-forekomst-trafiken': 'e) Har patienten haft allvarlig hypoglykemi i trafiken under det senaste året?',
-        'ts-diabetes.label.hypoglykemier.allvarlig-forekomst-trafiken.beskrivning': 'Hur många sådana episoder och när inträffade de? ',
-
-        'ts-diabetes.label.hypoglykemier.egenkontroll-blodsocker': 'f) Genomför patienten egenkontroller av blodsocker?',
-
-        'ts-diabetes.label.hypoglykemier.allvarlig-forekomst-vaken-tid': 'g) Har patienten haft allvarlig hypoglykemi (som krävt hjälp av annan för att hävas) under vaken tid det senaste året?',
-        'ts-diabetes.label.hypoglykemier.allvarlig-forekomst-vaken-tid.observationstid': 'När?',
 
         'ts-diabetes.label.true': 'JA',
         'ts-diabetes.label.false': 'NEJ',
@@ -161,17 +105,11 @@ var tsDiabetesMessages = {
         'ts-diabetes.label.contactinfo': 'Namnförtydligande, mottagningens adress och telefon',
         'ts-diabetes.label.ej-angivet': 'Ej angivet',
 
-        'ts-diabetes.label.bedomning-info-alt-1': 'Patienten uppfyller kraven enligt Transportstyrelsens föreskrifter och allmänna råd om medicinska krav för innehav av körkort m.m. (TSFS 2010:125, senast ändrade genom TSFS 2013:2) för:',
         'ts-diabetes.label.bedomning.kan-inte-ta-stallning': 'Kan inte ta ställning',
-        'ts-diabetes.label.bedomning-info-undersokas-med-specialkompetens': 'Patienten bör före ärendets avgörande undersökas av läkare med specialistkompetens i:',
-        'ts-diabetes.label.bedomning.lamplighet-inneha': 'Om patienten söker behörighet C1, C1E, C, CE, D1, D1E, D, DE eller Taxi, är han eller hon lämplig att inneha sådan behörighet med hänsyn till de körningar och arbetsformer som är aktuella vid sådant innehav? (Se 6 kap. 16 §) ',
-        'ts-diabetes.helptext.readpdfhelp': '<b>Läsa en pdf-fil</b><br/>PDF är ett filformat som används för att ett dokument ska se likadant ut i olika datorer. För att kunna öppna pdf-filer behöver du en pdf-läsare exempelvis. <a href="http://get.adobe.com/se/reader/" target="_blank">Adobe Reader</a>.',
+
         // Helptexts
 
-        'ts-diabetes.helptext.synfunktioner.a-c': 'Då någon av a - c besvarats med JA krävs ett läkarintyg från ögonspecialist.',
-        'ts-diabetes.helptext.synfunktioner.8-dioptrier': 'Då högsta styrka i något av glasen överskrider 8 dioptrier besvarats med JA måste Intyg om korrektionsglasens styrka skickas in.',
-        'ts-diabetes.helptext.diabetes.behandling': 'Vid tablett- eller insulinbehandlad diabetes krävs det att ett läkarintyg gällande sjukdomen skickas in.',
-        'ts-diabetes.helptext.narkotika-lakemedel.provtagning': 'Om ja på ovanstående ska resultatet redovisas separat.',
+        'ts-diabetes.helptext.readpdfhelp': '<b>Läsa en pdf-fil</b><br/>PDF är ett filformat som används för att ett dokument ska se likadant ut i olika datorer. För att kunna öppna pdf-filer behöver du en pdf-läsare exempelvis. <a href="http://get.adobe.com/se/reader/" target="_blank">Adobe Reader</a>.',
 
         'ts-diabetes.inbox.complementaryinfo': 'Avser behörighet'
 
--- ts/ts-diabetes/src/main/resources/META-INF/resources/webjars/ts-diabetes/webcert/views/messages.js
+++ ts/ts-diabetes/src/main/resources/META-INF/resources/webjars/ts-diabetes/webcert/views/messages.js
@@ -27,28 +27,7 @@ var tsDiabetesMessages = {
         'ts-diabetes.label.send.body': '',
         'ts-diabetes.label.intygavser': 'Intyget avser',
         'ts-diabetes.label.identitet': 'Identiteten är styrkt genom',
-        'ts-diabetes.label.diabetes': '1. Allmänt',
-        'ts-diabetes.label.hypoglykemier': '2. Hypoglykemier (lågt blodsocker)',
-        'ts-diabetes.label.synintyg': '3. Synintyg',
-        'ts-diabetes.label.bedomning': '4. Bedömning',
         'ts-diabetes.label.vardenhet': 'Vårdenhet',
-        'ts-diabetes.label.korkort.am': 'AM',
-        'ts-diabetes.label.korkort.a1': 'A1',
-        'ts-diabetes.label.korkort.a2': 'A2',
-        'ts-diabetes.label.korkort.a': 'A',
-        'ts-diabetes.label.korkort.b': 'B',
-        'ts-diabetes.label.korkort.be': 'BE',
-        'ts-diabetes.label.korkort.traktor': 'Traktor',
-        'ts-diabetes.label.korkort.c1': 'C1',
-        'ts-diabetes.label.korkort.c1e': 'C1E',
-        'ts-diabetes.label.korkort.c': 'C',
-        'ts-diabetes.label.korkort.ce': 'CE',
-        'ts-diabetes.label.korkort.d1': 'D1',
-        'ts-diabetes.label.korkort.d1e': 'D1E',
-        'ts-diabetes.label.korkort.d': 'D',
-        'ts-diabetes.label.korkort.de': 'DE',
-        'ts-diabetes.label.korkort.taxi': 'Taxi',
-        'ts-diabetes.label.korkort.annat': 'Annat',
         'ts-diabetes.label.makulera.confirmation': 'Transportstyrelsens läkarintyg, diabetes, utställd på ${namn} – ${personnummer} är återtaget. Mottagare av intyget är notifierade om detta.',
         'ts-diabetes.label.spara-utkast': 'Spara',
         'ts-diabetes.label.ta-bort-utkast': 'Ta bort utkast',
@@ -58,31 +37,10 @@ var tsDiabetesMessages = {
         'ts-diabetes.label.status.signed': 'Intyget är signerat och är nu tillgängligt för patienten i webbtjänsten Mina intyg, som nås via 1177.se. Om patienten inte har möjlighet att skicka intyget elektroniskt till Transportstyrelsen kan du skicka intyget direkt till Transportstyrelsen åt patienten. Observera att patientens samtycke då krävs.',
         'ts-diabetes.label.kopiera.text': 'Skapar en kopia av befintligt intyg. Det nya utkastet (kopian) kan ändras och signeras.',
 
-        // Identitet styrkt genom
-        'ts-diabetes.label.identitet.id_kort': 'ID-kort',
-        'ts-diabetes.label.identitet.foretag_eller_tjanstekort': 'Företagskort eller tjänstekort.',
-        'ts-diabetes.label.identitet.korkort': 'Svenskt körkort',
-        'ts-diabetes.label.identitet.pers_kannedom': 'Personlig kännedom',
-        'ts-diabetes.label.identitet.forsakran_kap18': 'Försäkran enligt 18 kap 4 §',
-        'ts-diabetes.label.identitet.pass': 'Pass',
-        'ts-diabetes.helptext.identitet-styrkt-genom.id-kort': '* SIS-märkt ID-kort, svenskt nationellt ID-kort eller ID-kort utfärdat av Skatteverket.',
-        'ts-diabetes.helptext.identitet-styrkt-genom.foretags-tjanstekort': '** SIS-märkt företagskort eller tjänstekort.',
-        'ts-diabetes.helptext.identitet-styrkt-genom.forsakran-enl18kap4': '*** Försäkran enligt 18 kap 4 § i Transportstyrelsens föreskrifter (TSFS 2010:125, senast ändrade genom TSFS 2013:2): Identiteten får fastställas genom att en förälder, annan vårdnadshavare, make, maka eller sambo, registrerad partner eller myndigt barn skriftligen försäkrar att lämnade uppgifter om sökandens identitet är riktiga. Den som lämnar en sådan försäkran ska vara närvarande vid identitetskontrollen och kunna styrka sin egen identitet.',
-        'ts-diabetes.helptext.identitet-styrkt-genom.pass': '**** Svenskt EU-pass, annat EU-pass utfärdade från och med den 1 september 2006, pass utfärdat av Island, Liechtenstein, Norge eller Schweiz från och med den 1 september 2006.',
-
         // Visa ts-diabetes
         'ts-diabetes.label.patient': 'Patientens adressuppgifter',
 
-        'ts-diabetes.label.syn': '3. Synintyg',
-        'ts-diabetes.label.ovrigkommentar': 'Övriga upplysningar och kommentarer',
-
         'ts-diabetes.label.syn.binokulart': 'Binokulärt',
-        'ts-diabetes.label.syn.diplopi': 'd) Förekommer dubbelseende? ',
-        'ts-diabetes.label.syn.progressivogonsjukdom': ' c) Har patienten någon progressiv ögonsjukdom?',
-
-        'ts-diabetes.label.syn.separat-ogonlakarintyg': 'a) Ögonläkarintyg kommer att skickas in separat',
-        'ts-diabetes.label.syn.synfaltsprovning-utan-anmarkning': 'b) Är synfältsprövning enligt Donders konfrontationsmetod utan anmärkning? ',
-        'ts-diabetes.label.syn.synskarpa': 'c) Värden för synskärpa',
 
         'ts-diabetes.label.syn.hogeroga': '',
         'ts-diabetes.label.syn.vansteroga': '',
@@ -90,48 +48,15 @@ var tsDiabetesMessages = {
         'ts-diabetes.label.syn.medkorrektion': '',
         'ts-diabetes.label.syn.kontaktlins': '',
 
-        'ts-diabetes.label.diabetes.vilket-ar': 'Vilket år ställdes diagnosen diabetes?',
-        'ts-diabetes.label.diabetes.diabetestyp': 'Diabetestyp',
         'ts-diabetes.label.diabetes.diabetestyp.diabetes_typ_1': 'Typ 1',
         'ts-diabetes.label.diabetes.diabetestyp.diabetes_typ_2': 'Typ 2',
-        'ts-diabetes.label.diabetes.behandling': 'Behandling',
-        'ts-diabetes.label.diabetes.behandling.endast-kost': 'Endast kost',
-        'ts-diabetes.label.diabetes.behandling.tabletter': 'Tabletter',
-        'ts-diabetes.label.diabetes.behandling.insulin': 'Insulin',
-        'ts-diabetes.label.diabetes.behandling.insulin-period': 'Insulin sedan år',
-        'ts-diabetes.label.diabetes.behandling.annan': 'Annan behandling',
-        'ts-diabetes.label.diabetes.behandling.annan-beskrivning': 'Annan behandling, vilken?',
-
-        'ts-diabetes.label.hypoglykemier.har-patient-kannedom': 'a) Har patienten kunskap om lämpliga åtgärder vid hypoglykemi?',
-        'ts-diabetes.label.hypoglykemier.tecken-nedsatt-hjarnfunktion': 'b) Förekommer hypoglykemier med tecken på nedsatt hjärnfunktion (neuroglukopena symtom) som bedöms kunna innebära en trafiksäkerhetsrisk?',
-        'ts-diabetes.label.hypoglykemier.saknar-formaga-kanna-varningstecken': 'c) Saknar patienten förmåga att känna varningstecken på hypoglykemi ("unawareness")?',
-
-        'ts-diabetes.label.hypoglykemier.allvarlig-forekomst': 'd) Har patienten haft allvarlig hypoglykemi (som krävt hjälp av annan för att hävas) under det senaste året?',
-        'ts-diabetes.label.hypoglykemier.allvarlig-forekomst.beskrivning': 'Hur många sådana episoder? ',
-
-        'ts-diabetes.label.hypoglykemier.allvarlig-forekomst-trafiken': 'e) Har patienten haft allvarlig hypoglykemi i trafiken under det senaste året?',
-        'ts-diabetes.label.hypoglykemier.allvarlig-forekomst-trafiken.beskrivning': 'Hur många sådana episoder och när inträffade de? ',
-
-        'ts-diabetes.label.hypoglykemier.egenkontroll-blodsocker': 'f) Genomför patienten egenkontroller av blodsocker?',
-
-        'ts-diabetes.label.hypoglykemier.allvarlig-forekomst-vaken-tid': 'g) Har patienten haft allvarlig hypoglykemi (som krävt hjälp av annan för att hävas) under vaken tid det senaste året?',
-
         'ts-diabetes.label.kontakt-info': 'Namnförtydligande, mottagningens adress och telefon',
 
         'ts-diabetes.label.bedomning-info-alt-1': 'Patienten uppfyller kraven enligt Transportstyrelsens föreskrifter och allmänna råd om medicinska krav för innehav av körkort m.m. (TSFS 2010:125, senast ändrade genom TSFS 2013:2) för:',
         'ts-diabetes.label.bedomning.kan-inte-ta-stallning': 'Kan inte ta ställning',
-        'ts-diabetes.label.bedomning-info-undersokas-med-specialkompetens': 'Patienten bör före ärendets avgörande undersökas av läkare med specialistkompetens i',
         'ts-diabetes.label.bedomning-info-ej-angivet': 'Ej angivet',
-        'ts-diabetes.label.bedomning.lamplighet-inneha': 'Om patienten söker behörighet C1, C1E, C, CE, D1, D1E, D, DE eller Taxi, är han eller hon lämplig att inneha sådan behörighet med hänsyn till de körningar och arbetsformer som är aktuella vid sådant innehav? (Se 6 kap. 16 §) ',
-
-        'ts-diabetes.helptext.synfunktioner.a-c': 'Då någon av a - c besvarats med JA krävs ett läkarintyg från ögonspecialist.',
-        'ts-diabetes.helptext.synfunktioner.8-dioptrier': 'Då högsta styrka i något av glasen överskrider 8 dioptrier besvarats med JA måste Intyg om korrektionsglasens styrka skickas in.',
-        'ts-diabetes.helptext.diabetes.behandling': 'Vid tablett- eller insulinbehandlad diabetes krävs det att ett läkarintyg gällande sjukdomen skickas in.',
-        'ts-diabetes.helptext.narkotika-lakemedel.provtagning': 'Om ja på ovanstående ska resultatet redovisas separat.',
 
         // Help texts
-        'ts-diabetes.helptext.lakaren-ska-uppmarksamma': 'Läkaren ska uppmärksamma Transportstyrelsens föreskrifter och allmänna råd om medicinska krav för innehav av körkort m.m. (TSFS 2010:125, senast ändrade genom TSFS 2013:2). Intyget ska vara utfärdat tidigast 2 månader före att ansökan kom in till Transportstyrelsen och i enlighet med vad som sägs i 17 kapitlet.<br/>Se: <a href="http://www.transportstyrelsen.se" target="_blank">http://www.transportstyrelsen.se</a>.',
-        'ts-diabetes.helptext.intyget-avser': '<span style="text-align:left">AM = moped klass I<br/>A1 = lätt motorcykel<br/>A2 = mellanstor motorcykel<br/>A = motorcykel<br/>B = personbil, lätt lastbil och enbart ett lätt släpfordon<br/>BE = personbil, lätt lastbil och ett eller flera släpfordon. Släpens totalvikt får inte överstiga 3,5 ton.<br/>C1 = medeltung lastbil och enbart ett lätt släpfordon<br/>C1E = medeltung lastbil och ett eller flera släpfordon oavsett vikt<br/>C = tung lastbil och enbart ett lätt släpfordon<br/>CE = tung lastbil och ett eller flera släpfordon oavsett vikt<br/>D1 = mellanstor buss och enbart ett lätt släpfordon<br/>D1E = mellanstor buss och ett eller flera släpfordon oavsett vikt<br/>D = buss och enbart ett lätt släpfordon<br/>DE = buss och ett eller flera släpfordon oavsett vikt<br/>Taxi = taxiförarlegitimation</span>',
 
         'ts-diabetes.helptext.identitet-styrkt-genom': 'ID-kort = SIS-märkt ID-kort, svenskt nationellt ID-kort eller ID-kort utfärdat av Skatteverket.<br/> Företagskort eller tjänstekort = SIS-märkt företagskort eller tjänstekort.<br/> Försäkran enligt 18 kap. 4 § = Försäkran enligt 18 kap 4 § i Transportstyrelsens föreskrifter (TSFS 2010:125, senast ändrade genom TSFS 2013:2): Identiteten får fastställas genom att en förälder, annan vårdnadshavare, make, maka eller sambo, registrerad partner eller myndigt barn skriftligen försäkrar att lämnade uppgifter om sökandens identitet är riktiga. Den som lämnar en sådan försäkran ska vara närvarande vid identitetskontrollen och kunna styrka sin egen identitet.<br/> Pass = Svenskt EU-pass, annat EU-pass utfärdade från och med den 1 september 2006, pass utfärdat av Island, Liechtenstein, Norge eller Schweiz från och med den 1 september 200<br/>',
 
