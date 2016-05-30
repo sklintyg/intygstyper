2dfd24c71128aa0778672f3cafa10178eb6dc3bd
Per Beijer <per.beijer@usify.se>
2016-05-30 11:12:21 +0200

    INTYG-2494: Dynamiska texter frontend för TS-bas

--- ts/ts-bas/src/main/resources/META-INF/resources/webjars/ts-bas/minaintyg/js/messages.js
+++ ts/ts-bas/src/main/resources/META-INF/resources/webjars/ts-bas/minaintyg/js/messages.js
@@ -22,54 +22,10 @@ var tsBasMessages = {
     'sv': {
         'ts-bas.label.intygavser': 'Avser behörighet för körkort',
         'ts-bas.label.identitet': 'Identiteten är styrkt genom',
-        'ts-bas.label.synfunktioner': '1. Synfunktioner',
-        'ts-bas.label.horselbalans': '2. Hörsel och balanssinne',
-        'ts-bas.label.funktionsnedsattning': '3. Rörelseorganens funktioner',
-        'ts-bas.label.hjartkarl': '4. Hjärt- och kärlsjukdomar',
-        'ts-bas.label.diabetes': '5. Diabetes',
-        'ts-bas.label.neurologi': '6. Neurologiska sjukdomar',
-        'ts-bas.label.medvetandestorning': '7. Epilepsi, epileptiskt anfall och annan medvetandestörning',
-        'ts-bas.label.njurar': '8. Njursjukdomar',
-        'ts-bas.label.kognitivt': '9. Demens och andra kognitiva störningar',
-        'ts-bas.label.somn-vakenhet': '10. Sömn- och vakenhetsstörningar',
-        'ts-bas.label.narkotika-lakemedel': '11. Alkohol, narkotika och läkemedel',
-        'ts-bas.label.psykiskt': '12. Psykiska sjukdomar och störningar',
-        'ts-bas.label.utvecklingsstorning': '13. ADHD, autismspektrumtillstånd och likartade tillstånd samt psykisk utvecklingsstörning',
-        'ts-bas.label.sjukhusvard': '14. Sjukhusvård',
-        'ts-bas.label.medicinering': '15. Övrig medicinering',
-        'ts-bas.label.ovrigkommentar': '16. Övrig kommentar',
-        'ts-bas.label.bedomning': 'Bedömning',
         'ts-bas.label.unit': 'Vårdenhet',
 
-        'ts-bas.label.korkort.c1': 'C1',
-        'ts-bas.label.korkort.c1e': 'C1E',
-        'ts-bas.label.korkort.c': 'C',
-        'ts-bas.label.korkort.ce': 'CE',
-        'ts-bas.label.korkort.d1': 'D1',
-        'ts-bas.label.korkort.d1e': 'D1E',
-        'ts-bas.label.korkort.d': 'D',
-        'ts-bas.label.korkort.de': 'DE',
-        'ts-bas.label.korkort.taxi': 'Taxi',
-        'ts-bas.label.korkort.annat': 'Annat',
-
-        // Identitet styrkt genom
-        'ts-bas.label.identitet.id_kort': 'ID-kort',
-        'ts-bas.label.identitet.foretag_eller_tjanstekort': 'Företagskort eller tjänstekort.',
-        'ts-bas.label.identitet.korkort': 'Svenskt körkort',
-        'ts-bas.label.identitet.pers_kannedom': 'Personlig kännedom',
-        'ts-bas.label.identitet.forsakran_kap18': 'Försäkran enligt 18 kap 4 §',
-        'ts-bas.label.identitet.pass': 'Pass',
 
         'ts-bas.label.syn.binokulart': 'Binokulärt',
-        'ts-bas.label.syn.8-dioptrier': 'Högsta styrka i något av glasen överskrider 8 dioptrier:',
-        'ts-bas.label.syn.synfaltsdefekter': 'a) Finns tecken på synfältsdefekter vid undersökning enligt Donders konfrontationsmetod?',
-        'ts-bas.label.syn.nattblindhet': 'b) Framkommer anamnestiska uppgifter om begränsning av seendet vid nedsatt belysning?',
-        'ts-bas.label.syn.progressivogonsjukdom': ' c) Har patienten någon progressiv ögonsjukdom?',
-        'ts-bas.label.syn.diplopi': 'd) Framkommer dubbelseende vid prövning av ögats rörlighet (prövningen ska göras i de åtta huvudmeridianerna)?',
-        'ts-bas.label.syn.nystagmus': 'e) Förekommer nystagmus?',
-        'ts-bas.label.syn.synvarden': 'f) Värden för synskärpa',
-
-        'ts-bas.label.syn.synskarpa': 'Värden för synskärpa',
         'ts-bas.label.syn.hogeroga': 'Höger öga',
         'ts-bas.label.syn.vansteroga': 'Vänster öga',
         'ts-bas.label.syn.utankorrektion': 'Utan korrektion',
@@ -77,56 +33,8 @@ var tsBasMessages = {
         'ts-bas.label.syn.kontaktlins': 'Kontaktlinser',
         'ts-bas.label.syn.korrektionsglasens-styrka': 'Korrektionsglasens styrka',
 
-        'ts-bas.label.horselbalans.balansrubbningar': 'a) Har patienten överraskande anfall av balansrubbningar eller yrsel?',
-        'ts-bas.label.horselbalans.samtal4meter': 'b) Har patienten svårt att uppfatta vanlig samtalsstämma på fyra meters avstånd (hörapparat får användas)?',
-
-        'ts-bas.label.funktionsnedsattning.funktionsnedsattning': 'a) Har patienten någon sjukdom eller funktionsnedsättning som påverkar rörligheten och som medför att fordon inte kan köras på ett trafiksäkert sätt?',
-        'ts-bas.label.funktionsnedsattning.beskrivning': 'Typ av nedsättning eller sjukdom:',
-        'ts-bas.label.funktionsnedsattning.otillracklig-rorelseformaga': 'b) Är rörelseförmågan otillräcklig för att kunna hjälpa passagerare in och ut ur fordonet samt med bilbälte?',
-
-        'ts-bas.label.hjartkarl.hjart-karlsjukdom': 'a) Föreligger hjärt- eller kärlsjukdom som kan medföra en påtaglig risk för att hjärnans funktioner akut försämras eller som i övrigt innebär en trafiksäkerhetsrisk?',
-        'ts-bas.label.hjartkarl.hjarnskada-efter-trauma': 'b) Finns tecken på hjärnskada eller trauma, stroke eller annan sjukdom i centrala nervsystemet?',
-        'ts-bas.label.hjartkarl.riskfaktorer-stroke': 'c) Föreligger viktiga riskfaktorer för stroke (tidigare stroke eller TIA, förhöjt blodtryck, förmaksflimmer eller kärlmissbildning)?',
-        'ts-bas.label.hjartkarl.riskfaktorer-stroke.beskrivning': 'Vilken typ av sjukdom:',
-
-        'ts-bas.label.diabetes.har-diabetes': 'Har patienten diabetes?',
-        'ts-bas.label.diabetes.diabetestyp': 'Diabetestyp',
         'ts-bas.label.diabetes.diabetestyp.diabetes_typ_1': 'Typ 1',
         'ts-bas.label.diabetes.diabetestyp.diabetes_typ_2': 'Typ 2',
-        'ts-bas.label.diabetes.behandling': 'Behandling:',
-        'ts-bas.label.diabetes.behandling.kost': 'Kost',
-        'ts-bas.label.diabetes.behandling.tabletter': 'Tabletter',
-        'ts-bas.label.diabetes.behandling.insulin': 'Insulin',
-
-        'ts-bas.label.neurologi.neurologisksjukdom': 'Finns tecken på neurologisk sjukdom?',
-
-        'ts-bas.label.medvetandestorning.medvetandestorning': 'Har eller har patienten haft epilepsi, epileptiskt anfall eller annan medvetandestörning?',
-        'ts-bas.label.medvetandestorning.beskrivning': 'Om frågan besvaras med ja, och det är frågan om annan medvetandestörning, ange när den inträffade och orsak:',
-
-        'ts-bas.label.njurar.nedsatt-njurfunktion': 'Föreligger allvarligt nedsatt njurfunktion som kan innebära en trafiksäkerhetsrisk?',
-
-        'ts-bas.label.kognitivt.sviktande-kognitiv-funktion': 'Finns tecken på sviktande kognitiv funktion?',
-
-        'ts-bas.label.somn-vakenhet.tecken-somnstorningar': 'Finns tecken på, eller anamnestiska uppgifter som talar för sömn- eller vakenhetsstörning?',
-
-        'ts-bas.label.narkotika-lakemedel.tecken-missbruk': 'a) Finns journaluppgifter, anamnestiska uppgifter, resultat av laboratorieprover eller andra tecken på missbruk eller beroende av alkohol, narkotika eller läkemedel?',
-        'ts-bas.label.narkotika-lakemedel.foremal-for-vardinsats': 'b) Har patienten vid något tillfälle varit föremål för vårdinsatser för missbruk eller beroende av alkohol, narkotika eller läkemedel?',
-        'ts-bas.label.narkotika-lakemedel.behov-provtagning': 'Behövs det provtagning avseende aktuellt bruk av alkohol eller narkotika?',
-        'ts-bas.label.narkotika-lakemedel.lakarordinerat-lakemedelsbruk': 'c) Pågår regelbundet läkarordinerat bruk av läkemedel som kan innebära en trafiksäkerhetsrisk?',
-        'ts-bas.label.narkotika-lakemedel.beskrivning': 'Om frågan besvaras med ja, ange läkemedel och ordinerad dos:',
-
-        'ts-bas.label.psykiskt.psykisksjukdom': 'Har eller har patienten haft psykisk sjukdom eller störning, till exempel schizofreni, annan psykos eller bipolär (manodepressiv) sjukdom?',
-
-        'ts-bas.label.utvecklingsstorning.psykisk-utvecklingsstorning': 'a) Har patienten någon psykisk utvecklingsstörning?',
-        'ts-bas.label.utvecklingsstorning.har-syndrom': 'b) Har patienten till exempel ADHD, ADD, DAMP, Aspergers syndrom eller Tourettes syndrom?',
-
-        'ts-bas.label.sjukhusvard.sjukhus-eller-lakarkontakt': 'Har patienten vårdats på sjukhus eller haft kontakt med läkare med anledning av fälten 1-13?',
-        'ts-bas.label.sjukhusvard.vardinrattning': 'Vårdinrättningens namn och klinik/er:',
-        'ts-bas.label.sjukhusvard.anledning': 'För vad?',
-        'ts-bas.label.sjukhusvard.tidpunkt': 'Om frågan besvarats med ja, när?',
-
-        'ts-bas.label.medicinering.stadigvarande-medicinering': 'Har patienten någon stadigvarande medicinering?',
-        'ts-bas.label.medicinering.beskrivning': 'Vilken eller vilka mediciner:',
 
         'ts-bas.label.true': 'JA',
         'ts-bas.label.false': 'NEJ',
@@ -190,19 +98,10 @@ var tsBasMessages = {
         'ts-bas.label.date': 'Datum',
         'ts-bas.label.contactinfo': 'Namnförtydligande, mottagningens adress och telefon',
 
-        'ts-bas.label.bedomning-info-alt-1': 'Patienten uppfyller kraven enligt Transportstyrelsens föreskrifter och allmänna råd om medicinska krav för innehav av körkort m.m. (TSFS 2010:125, senast ändrade genom TSFS 2013:2) för:',
-        'ts-bas.label.bedomning.kan-inte-ta-stallning': 'Kan inte ta ställning',
-        'ts-bas.label.bedomning-info-undersokas-med-specialkompetens': 'Patienten bör före ärendets avgörande undersökas av läkare med specialistkompetens i:',
         'ts-bas.label.bedomning-info-ej-angivet': 'Ej angivet',
         'ts-bas.helptext.readpdfhelp': '<b>Läsa en pdf-fil</b><br/>PDF är ett filformat som används för att ett dokument ska se likadant ut i olika datorer. För att kunna öppna pdf-filer behöver du en pdf-läsare exempelvis. <a href="http://get.adobe.com/se/reader/" target="_blank">Adobe Reader</a>.',
 
         //Helptexts
-
-        'ts-bas.helptext.synfunktioner.a-c': 'Då någon av a - c besvarats med JA krävs ett läkarintyg från ögonspecialist.',
-        'ts-bas.helptext.synfunktioner.8-dioptrier': 'Då högsta styrka i något av glasen överskrider 8 dioptrier besvarats med JA måste Intyg om korrektionsglasens styrka skickas in.',
-        'ts-bas.helptext.diabetes.behandling': 'Vid tablett- eller insulinbehandlad diabetes krävs det att ett läkarintyg gällande sjukdomen skickas in.',
-        'ts-bas.helptext.narkotika-lakemedel.provtagning': 'Om ja på ovanstående ska resultatet redovisas separat.',
-
         'ts-bas.inbox.complementaryinfo': 'Avser behörighet'
 
     },
