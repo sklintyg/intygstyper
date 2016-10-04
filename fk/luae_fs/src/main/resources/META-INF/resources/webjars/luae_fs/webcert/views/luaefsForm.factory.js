558c8256fc5942d57820c4085c78415dfc06b27b
Per Beijer <per.beijer@usify.se>
2016-10-04 14:12:32 +0200

    INTYG-2975: Id för FRG_1

--- fk/luae_fs/src/main/resources/META-INF/resources/webjars/luae_fs/webcert/views/luaefsForm.factory.js
+++ fk/luae_fs/src/main/resources/META-INF/resources/webjars/luae_fs/webcert/views/luaefsForm.factory.js
@@ -41,7 +41,7 @@ angular.module('luae_fs').factory('luae_fs.FormFactory',
                     templateOptions: {category: 1, categoryName: categoryNames[1]},
                     fieldGroup: [
                         //Fråga 1 -----
-                        {type: 'headline', templateOptions: {label: 'FRG_1', level:4}},
+                        {type: 'headline', templateOptions: {id:'FRG_1', label: 'FRG_1', level:4}},
                         {key: 'undersokningAvPatienten', type: 'date', templateOptions: {label: 'KV_FKMU_0001.UNDERSOKNING'}},
                         {key: 'journaluppgifter', type: 'date', templateOptions: {label: 'KV_FKMU_0001.JOURNALUPPGIFTER'}},
                         {
