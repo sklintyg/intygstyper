e949ccf28432ec2cf77d6b3b4a9e4fe64c4fe27e
Jesper Holmberg <jesper.holmberg@callistaenterprise.se>
2016-10-19 17:05:18 +0200

    DEVENV: konsolidera gruntfiler

--- gruntfiles/Gruntfile.js
+++ gruntfiles/Gruntfile.js
@@ -1,32 +1,5 @@
-/*
- * Copyright (C) 2016 Inera AB (http://www.inera.se)
- *
- * This file is part of sklintyg (https://github.com/sklintyg).
- *
- * sklintyg is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * sklintyg is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/* global module */
-function config(name) {
-    'use strict';
-    return require('./tasks/' + name);
-}
-
 module.exports = function(grunt) {
     'use strict';
-
-    require('time-grunt')(grunt);
 
     var npmDir = grunt.option('npmDir');
     var cwd = process.cwd();
@@ -41,23 +14,23 @@ module.exports = function(grunt) {
     process.chdir(cwd);
 
     var SRC_DIR = 'src/main/resources/META-INF/resources/';
+    var TEST_DIR = 'src/test/js/';
     var DEST_DIR = (grunt.option('outputDir') || 'build/') +  'resources/main/META-INF/resources/';
     var TEST_OUTPUT_DIR = (grunt.option('outputDir') || 'build/karma/');
     var SKIP_COVERAGE = grunt.option('skip-coverage') !== undefined ? grunt.option('skip-coverage') : true;
+    var MODULE = grunt.option('module');
 
-    var minaintyg = grunt.file.expand({cwd:SRC_DIR}, ['webjars/ts-diabetes/minaintyg/**/*.js', '!**/*.spec.js', '!**/module.js']).sort();
-    grunt.file.write(DEST_DIR + 'webjars/ts-diabetes/minaintyg/js/module-deps.json', JSON.stringify(minaintyg.
-        map(function(file){ return '/web/'+file; }).
-        concat('/web/webjars/ts-diabetes/minaintyg/templates.js'), null, 4));
-    minaintyg = [SRC_DIR + 'webjars/ts-diabetes/minaintyg/js/module.js', DEST_DIR + 'webjars/ts-diabetes/minaintyg/templates.js'].concat(minaintyg.map(function(file){
+    var minaintyg = grunt.file.expand({cwd:SRC_DIR}, ['webjars/' + MODULE + '/minaintyg/**/*.js', '!**/*.spec.js', '!**/module.js']).sort();
+    grunt.file.write(DEST_DIR + 'webjars/' + MODULE + '/minaintyg/js/module-deps.json',
+                     JSON.stringify(minaintyg.map(function(file){ return '/web/'+file; }).concat('/web/webjars/' + MODULE + '/minaintyg/templates.js'), null, 4));
+    minaintyg = [SRC_DIR + 'webjars/' + MODULE + '/minaintyg/js/module.js', DEST_DIR + 'webjars/' + MODULE + '/minaintyg/templates.js'].concat(minaintyg.map(function(file){
         return SRC_DIR + file;
     }));
 
-    var webcert = grunt.file.expand({cwd:SRC_DIR}, ['webjars/ts-diabetes/webcert/**/*.js', '!**/*.spec.js', '!**/*.test.js', '!**/module.js']).sort();
-    grunt.file.write(DEST_DIR + 'webjars/ts-diabetes/webcert/module-deps.json', JSON.stringify(webcert.
-        map(function(file){ return '/web/'+file; }).
-        concat('/web/webjars/ts-diabetes/webcert/templates.js'), null, 4));
-    webcert = [SRC_DIR + 'webjars/ts-diabetes/webcert/module.js', DEST_DIR + 'webjars/ts-diabetes/webcert/templates.js'].concat(webcert.map(function(file){
+    var webcert = grunt.file.expand({cwd:SRC_DIR}, ['webjars/' + MODULE + '/webcert/**/*.js', '!**/*.spec.js', '!**/*.test.js', '!**/module.js']).sort();
+    grunt.file.write(DEST_DIR + 'webjars/' + MODULE + '/webcert/module-deps.json',
+                     JSON.stringify(webcert.map(function(file){ return '/web/'+file; }).concat('/web/webjars/' + MODULE + '/webcert/templates.js'), null, 4));
+    webcert = [SRC_DIR + 'webjars/' + MODULE + '/webcert/module.js', DEST_DIR + 'webjars/' + MODULE + '/webcert/templates.js'].concat(webcert.map(function(file){
         return SRC_DIR + file;
     }));
 
@@ -89,15 +62,15 @@ module.exports = function(grunt) {
             },
             target: [SRC_DIR + '**/*.scss']
         },
-        
+
         concat: {
             minaintyg: {
                 src: minaintyg,
-                dest: DEST_DIR + 'webjars/ts-diabetes/minaintyg/js/module.min.js'
+                dest: DEST_DIR + 'webjars/' + MODULE + '/minaintyg/js/module.min.js'
             },
             webcert: {
                 src: webcert,
-                dest: DEST_DIR + 'webjars/ts-diabetes/webcert/module.min.js'
+                dest: DEST_DIR + 'webjars/' + MODULE + '/webcert/module.min.js'
             }
         },
 
@@ -109,10 +82,10 @@ module.exports = function(grunt) {
                 ignores: ['**/templates.js']
             },
             minaintyg: {
-                src: [ 'Gruntfile.js', SRC_DIR + 'webjars/ts-diabetes/minaintyg/**/*.js']
+                src: [ 'Gruntfile.js', SRC_DIR + 'webjars/' + MODULE + '/minaintyg/**/*.js', TEST_DIR + 'minaintyg/**/*.js' ]
             },
             webcert: {
-                src: [ 'Gruntfile.js', SRC_DIR + 'webjars/ts-diabetes/webcert/**/*.js']
+                src: [ 'Gruntfile.js', SRC_DIR + 'webjars/' + MODULE + '/webcert/**/*.js', TEST_DIR + 'webcert/**/*.js' ]
             }
         },
 
@@ -134,23 +107,24 @@ module.exports = function(grunt) {
                 }
             }
         },
-
-         sass: {
+        
+        // Compiles Sass to CSS
+        sass: {
             options: {
             },
             dist: {
                 files: [{
                     expand: true,
-                    cwd: SRC_DIR + 'webjars/ts-diabetes/webcert/css/',
+                    cwd: SRC_DIR + 'webjars/' + MODULE + '/webcert/css/',
                     src: ['*.scss'],
-                    dest: DEST_DIR + 'webjars/ts-diabetes/webcert/css',
+                    dest: DEST_DIR + 'webjars/' + MODULE + '/webcert/css',
                     ext: '.css'
                 }, 
                 {
                     expand: true,
-                    cwd: SRC_DIR + 'webjars/ts-diabetes/minaintyg/css/',
+                    cwd: SRC_DIR + 'webjars/' + MODULE + '/minaintyg/css/',
                     src: ['*.scss'],
-                    dest: DEST_DIR + 'webjars/ts-diabetes/minaintyg/css',
+                    dest: DEST_DIR + 'webjars/' + MODULE + '/minaintyg/css',
                     ext: '.css'
                 }]
             }
@@ -161,12 +135,12 @@ module.exports = function(grunt) {
                 singleQuotes: true
             },
             minaintyg: {
-                src: DEST_DIR + 'webjars/ts-diabetes/minaintyg/js/module.min.js',
-                dest: DEST_DIR + 'webjars/ts-diabetes/minaintyg/js/module.min.js'
+                src: DEST_DIR + 'webjars/' + MODULE + '/minaintyg/js/module.min.js',
+                dest: DEST_DIR + 'webjars/' + MODULE + '/minaintyg/js/module.min.js'
             },
             webcert: {
-                src: DEST_DIR + 'webjars/ts-diabetes/webcert/module.min.js',
-                dest: DEST_DIR + 'webjars/ts-diabetes/webcert/module.min.js'
+                src: DEST_DIR + 'webjars/' + MODULE + '/webcert/module.min.js',
+                dest: DEST_DIR + 'webjars/' + MODULE + '/webcert/module.min.js'
             }
         },
 
@@ -175,27 +149,50 @@ module.exports = function(grunt) {
                 mangle: false
             },
             minaintyg: {
-                src: DEST_DIR + 'webjars/ts-diabetes/minaintyg/js/module.min.js',
-                dest: DEST_DIR + 'webjars/ts-diabetes/minaintyg/js/module.min.js'
+                src: DEST_DIR + 'webjars/' + MODULE + '/minaintyg/js/module.min.js',
+                dest: DEST_DIR + 'webjars/' + MODULE + '/minaintyg/js/module.min.js'
             },
             webcert: {
-                src: DEST_DIR + 'webjars/ts-diabetes/webcert/module.min.js',
-                dest: DEST_DIR + 'webjars/ts-diabetes/webcert/module.min.js'
+                src: DEST_DIR + 'webjars/' + MODULE + '/webcert/module.min.js',
+                dest: DEST_DIR + 'webjars/' + MODULE + '/webcert/module.min.js'
             }
         },
-
-        ngtemplates: config('ngtemplates'),
 
         lcovMerge: {
             options: {
                 outputFile: TEST_OUTPUT_DIR + 'merged_lcov.info'
             },
             src: [TEST_OUTPUT_DIR + 'webcert/*.info', TEST_OUTPUT_DIR +'minaintyg/*.info']
+        },
+
+        ngtemplates : {
+            webcert: {
+                cwd: 'src/main/resources/META-INF/resources/webjars/' + MODULE + '/webcert',
+                src: ['**/*.html'],
+                dest: 'build/resources/main/META-INF/resources/webjars/' + MODULE + '/webcert/templates.js',
+                options:{
+                    module: MODULE,
+                    url: function(url) {
+                        return '/web/webjars/' + MODULE + '/webcert/' + url;
+                    }
+                }
+            },
+            minaintyg: {
+                cwd: 'src/main/resources/META-INF/resources/webjars/' + MODULE + '/minaintyg',
+                src: ['**/*.html'],
+                dest: 'build/resources/main/META-INF/resources/webjars/' + MODULE + '/minaintyg/templates.js',
+                options:{
+                    module: MODULE,
+                    url: function(url) {
+                        return '/web/webjars/' + MODULE + '/minaintyg/' + url;
+                    }
+                }
+            }
         }
     });
 
     grunt.registerTask('default', [ 'bower', 'ngtemplates', 'concat', 'ngAnnotate', 'uglify', 'sass', 'jshint' ]);
-    grunt.registerTask('lint-minaintyg', [ 'jshint:minaintyg' ] );
+    grunt.registerTask('lint-minaintyg', [ 'jshint:minaintyg' ]);
     grunt.registerTask('lint-webcert', [ 'jshint:webcert' ]);
     grunt.registerTask('lint', [ 'jshint', 'sasslint' ]);
     grunt.registerTask('test-minaintyg', [ 'wiredep:minaintyg', 'karma:minaintyg' ]);
