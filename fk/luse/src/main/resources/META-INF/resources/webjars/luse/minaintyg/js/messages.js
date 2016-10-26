a1d327d689b457b4fd1269a8de63527f4dd4944e
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2016-10-26 13:44:09 +0200

    INTYG-2926: Luse PDF: Aktiverar PDF utskrift i MI

--- fk/luse/src/main/resources/META-INF/resources/webjars/luse/minaintyg/js/messages.js
+++ fk/luse/src/main/resources/META-INF/resources/webjars/luse/minaintyg/js/messages.js
@@ -39,7 +39,6 @@ angular.module('luse').constant('luse.messages', {
         'luse.button.sendtofk': 'Skicka till Försäkringskassan',
         'luse.button.send.certificate': 'Skicka',
         'luse.button.send.certificate.title': 'Skicka detta intyg till en eller flera mottagare.',
-        'luse.button.save.as.pdf.title': 'Läkarutlåtande för sjukersättning går inte att ladda ner som PDF.',
 
         'luse.label.latestevent': 'Senaste händelse',
         'luse.label.latestevent.noevents': 'Inga händelser',
