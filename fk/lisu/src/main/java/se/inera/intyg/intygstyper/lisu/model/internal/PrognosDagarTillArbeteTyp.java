752f8fa1dd97355c94aaeaf33f30b25f9f9adf87
Mikaela Nöteberg <mnoteberg@gmail.com>
2016-10-26 12:16:35 +0200

    INTYG-3139: Uppdateringar i KV_FKMU_0007

--- fk/lisu/src/main/java/se/inera/intyg/intygstyper/lisu/model/internal/PrognosDagarTillArbeteTyp.java
+++ fk/lisu/src/main/java/se/inera/intyg/intygstyper/lisu/model/internal/PrognosDagarTillArbeteTyp.java
@@ -19,9 +19,7 @@
 
 package se.inera.intyg.intygstyper.lisu.model.internal;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.annotation.*;
 
 /**
  * Implements KV_FKMU_0007.
@@ -30,19 +28,23 @@ public enum PrognosDagarTillArbeteTyp {
     /**
      * 30 dagarm, id 1.
      */
-    DAGAR_30(1, "30_DAGAR", "30 dagar"),
+    DAGAR_30(1, "TRETTIO_DGR", "30 dagar"),
     /**
      * 60 dagar, id 2.
      */
-    DAGAR_60(2, "60_DAGAR", "60 dagar"),
+    DAGAR_60(2, "SEXTIO_DGR", "60 dagar"),
     /**
      * 90 dagar, id 3.
      */
-    DAGAR_90(3, "90_DAGAR", "90 dagar"),
+    DAGAR_90(3, "NITTIO_DGR", "90 dagar"),
     /**
      * 180 dagar, id 4.
      */
-    DAGAR_180(4, "180_DAGAR", "180 dagar");
+    DAGAR_180(4, "HUNDRAATTIO_DAGAR", "180 dagar"),
+    /**
+     * 365 dagar, id 5.
+     */
+    DAGAR_365(5, "TREHUNDRASEXTIOFEM_DAGAR", "365 dagar");
 
     private final int id;
     private final String transportId;
